455e82a135c064639f72df0f3a309374087336da
Jacques tatiana <tatout08090@gmail.com>
2015-10-05 16:34:18 +0200

    Actualite Article Photos
    Ajout d’un padding pour sur le slide pour qu’il ne soit pas collé au
    texte :  .article_photo

---
+++ _html/article_photos.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <!-- //// META \\\\ -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+
+    <!-- //// TITLE \\\\ -->
+    <title>Festival de Cannes 2016 - HTML</title>
+
+    <!-- //// CSS \\\\ -->
+    <link href='js/bower_components/chocolat/src/css/chocolat.css' rel="stylesheet">
+    <link href='css/styles.css' rel="stylesheet">
+  
+  </head>
+
+  <body>
+    <?php include('header.html'); ?>
+
+    <div id="main" class="single-article">
+      <div class="container small-container">
+        <a href="#" class="nav prev"></a>
+        <a href="#" class="nav next"></a>
+        <div class="info">
+          <a href="#" class="category">Cinéma de la plage</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+        </div>
+        <h2 class="title-article">The Lobster : Premières images</h2>
+        <div class="buttons">
+          <a id="share-article" href="#" class="button">Partager</a>
+          <a href="#" class="button">Imprimer</a>
+        </div>
+        
+        <div class="slideshow article_photo">
+          <div class="slideshow-img">
+            <div class="images">
+              <div class="img active">
+                <a class="chocolat-image" href="img/slide001.jpg" title='<h2>Sur le tournage de "Deephan" de Jacques Audiard</h2><p>Crédit Image : VALERY HACHE / AFP</p>'>
+                  <img src="img/slide001.jpg" alt="" />
+                </a>
+              </div>
+              <div class="img">
+                <a class="chocolat-image" href="http://cdn-parismatch.ladmedia.fr/var/news/storage/images/paris-match/culture/cinema/festival-de-cannes-2013-audrey-tautou-maitresse-de-ceremonie-508580/4577783-1-fre-FR/Festival-de-Cannes-2013-Audrey-Tautou-maitresse-de-ceremonie.jpg"><img src="img/slide002.jpg" alt="" /></a>
+              </div>
+              <div class="img"><img src="img/slide001.jpg" alt="" /></div>
+              <div class="img"><img src="img/slide001.jpg" alt="" /></div>
+              <div class="img"><img src="img/slide001.jpg" alt="" /></div>
+              <div class="img"><img src="img/slide001.jpg" alt="" /></div>
+              <div class="img"><img src="img/slide001.jpg" alt="" /></div>
+              <div class="img"><img src="img/slide001.jpg" alt="" /></div>
+            </div>
+            <div class="owl-carousel thumbnails">
+              <div class="thumb active">
+                <img src="img/thumb01.jpg" />
+              </div>
+              <div class="thumb">
+                <img src="img/thumb01.jpg" />
+              </div>
+              <div class="thumb">
+                <img src="img/thumb01.jpg" />
+              </div>
+              <div class="thumb">
+                <img src="img/thumb01.jpg" />
+              </div>
+              <div class="thumb">
+                <img src="img/thumb01.jpg" />
+              </div>
+              <div class="thumb">
+                <img src="img/thumb01.jpg" />
+              </div>
+              <div class="thumb">
+                <img src="img/thumb01.jpg" />
+              </div>
+              <div class="thumb">
+                <img src="img/thumb01.jpg" />
+              </div>
+              <div class="thumb">
+                <img src="img/thumb01.jpg" />
+              </div>
+            </div>
+          </div>
+          <p class="caption"><strong>Équipe du film - Photocall - The Lobster</strong> © FDC / Théophile Delange</p>
+        </div>
+      </div>
+      <div class="share">
+        <p>Partagez l'article</p>
+        <div class="buttons square">
+          <a href="#" class="button">f</a>
+          <a href="#" class="button">t</a>
+          <a href="#" class="button">l</a>
+          <a href="#" class="button">e</a>
+          <a href="#" class="button">p</a>
+        </div>
+      </div>
+      <div class="film">
+        <div class="info-film">
+
+        </div>
+        <div class="programmation-film">
+
+        </div>
+      </div>
+      <div class="container">
+        <div class="focus">
+          <h2 class="title">Focus</h2>
+          <div class="articles">
+            <article class="article" data-format="article" data-theme="competition">
+              <div class="image" style="background-image: url('img/articles/03.jpg')">
+                <a href="article.php"></a>
+                <span class="picto"></span>
+              </div>
+              <div class="info">
+                <a href="#" class="category">Compétition</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                <h2><a href="article.php">Stéphane Brizé interroge la loi du marché</a></h2>
+              </div>
+            </article>
+            <article class="video" data-format="video" data-theme="photocall">
+              <div class="image" style="background-image: url('img/articles/03.jpg')">
+                <a href="article.php"></a>
+                <span class="picto"></span>
+              </div>
+              <div class="info">
+                <a href="#" class="category">Photocall</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                <h2><a href="article.php">Z de Costa-Gravas</a></h2>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div class="same-day">
+          <h2 class="title">Le même jour</h2>
+          <div class="articles center">
+            <article class="article" data-format="article" data-theme="competition">
+              <div class="image" style="background-image: url('img/articles/03.jpg')">
+                <a href="article.php"></a>
+                <span class="picto"></span>
+              </div>
+              <div class="info">
+                <a href="#" class="category">Compétition</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                <h2><a href="article.php">Stéphane Brizé interroge la loi du marché</a></h2>
+              </div>
+            </article>
+            <article class="video" data-format="video" data-theme="photocall">
+              <div class="image" style="background-image: url('img/articles/03.jpg')">
+                <a href="article.php"></a>
+                <span class="picto"></span>
+              </div>
+              <div class="info">
+                <a href="#" class="category">Photocall</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                <h2><a href="article.php">Z de Costa-Gravas</a></h2>
+              </div>
+            </article>
+            <article class="photo" data-format="photo" data-theme="photograph">
+              <div class="image" style="background-image: url('img/articles/03.jpg')">
+                <a href="article.php"></a>
+                <span class="picto"></span>
+              </div>
+              <div class="info">
+                <a href="#" class="category">L'oeil du photographe</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                <h2><a href="article.php">Renaud, réalisateur indépendant canadien © Balint Porneczi / supported by [NEUS]</a></h2>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include('footer.html'); ?>
+
+    <!-- //// SCRIPTS \\\\ -->
+    <script src="js/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="js/bower_components/owl.carousel/src/js/owl.carousel.js"></script>
+    <script src="js/bower_components/chocolat/src/js/jquery.chocolat.js"></script>
+    <script src="js/bower_components/wavesurfer.js/dist/wavesurfer.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
