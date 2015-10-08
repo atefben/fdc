ceccbe6a7f97860fd7413e97574419b8b17ac09a
Jacques tatiana <tatout08090@gmail.com>
2015-10-08 15:42:11 +0200

    Plan du site - base

---
+++ _html/plan-du-site.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <!-- //// META \\\\ -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+
+    <!-- //// TITLE \\\\ -->
+    <title>Festival de Cannes 2016 - HTML</title>
+
+    <!-- //// CSS \\\\ -->
+    <link href='js/bower_components/chocolat/src/css/chocolat.css' rel="stylesheet">
+    <link href='css/styles.css' rel="stylesheet">
+  <style type="text/css">
+        
+  </style>
+  </head>
+
+  <body>
+    <?php include('header.html'); ?>
+
+    <div id="main" class="sitemap">
+    <h2 class="title">Plan du site</h2>
+    <section class="legal-notice-container">
+        <article class="legal-notice-item">
+            <h3 class="legal-notice-item-title">L'acutalité</h3>
+            <ul>
+              <li><a href="#">Jour après jour</a></li>
+              <li><a href="#">Articles</a></li>
+              <li><a href="#">Photos</a></li>
+              <li><a href="#">Vidéos</a></li>
+              <li><a href="#">Audios</a></li>
+            </ul>
+        </article>
+        <article class="legal-notice-item">
+            <h3 class="legal-notice-item-title">Web tv</h3>
+            <ul>
+              <li><a href="#">En direct</a></li>
+              <li><a href="#">Chaînes</a></li>
+              <li><a href="#">Bandes annonces</a></li>  
+            </ul>
+        </article>
+        <article class="legal-notice-item">
+            <h3 class="legal-notice-item-title">Les films</h3>
+            <ul>
+              <li><a href="#">Séléection officielle</a></li>
+              <li><a href="#">Cannes classics</a></li>
+              <li><a href="#">Cinéma de la plage</a></li>
+              <li><a href="#">Caméra d'or</a></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>  
+            </ul>
+        </article>
+        <article class="legal-notice-item">
+            <h3 class="legal-notice-item-title">Jurys</h3>
+            <ul>
+              <li><a href="#">Longs métrages</a></li>
+              <li><a href="#">Cinéfondation & court metrage</a></li>
+              <li><a href="#">Un certain regard</a></li>
+              <li><a href="#">Caméra d'or</a></li>  
+            </ul>
+        </article>
+        <article class="legal-notice-item">
+            <h3 class="legal-notice-item-title">Le palmarès</h3>
+            <ul>
+               <li><a href="#">Compétition</a></li>
+               <li><a href="#">Un certain regard</a></li>
+               <li><a href="#">Cinéfondation</a></li>
+               <li><a href="#">Caméra d'or</a></li>
+               <li><a href="#">Tout le palmarès</a></li>
+            </ul>
+        </article>
+        <article class="legal-notice-item">
+            <h3 class="legal-notice-item-title">Les évènements</h3>
+            <ul>
+               <li><a href="#">Lecçon</a></li>
+               <li><a href="#">Expositions</a></li>
+               <li><a href="#">Rendez-vous eutropéen</a></li>
+               <li><a href="#">Leçon de musique</a></li>
+               <li><a href="#">Leçon de cinéma</a></li>
+               <li><a href="#">Leçon d'acteur</a></li>
+               <li><a href="#">Hommages</a></li>
+               <li><a href="#">Village international</a></li> 
+            </ul>
+        </article>
+        <article class="legal-notice-item">
+            <h3 class="legal-notice-item-title">Participer</h3>
+            <ul>
+                <li>
+                    <a href="#">Préparer son séjour</a>
+                </li>
+                <li>
+                    <a href="#">Festival mode d'emploi</a>
+                </li>
+                <li>
+                    <a href="#">Accès aus projections</a>
+                </li>
+                <li>
+                    <a href="#">Partenaires</a>
+                </li>
+                <li>
+                    <a href="#">Fournisseurs</a>
+                </li>
+            </ul>
+        </article>
+        <article class="legal-notice-item">
+            <h3 class="legal-notice-item-title">Espace presse</h3>
+            <ul>
+                <li>
+                    <a href="#">Accueil</a>
+                </li>
+                <li>
+                    <a href="#">L'info / communiqués</a>
+                </li>
+                <li>
+                    <a href="#">Participer</a>
+                </li>
+                <li>
+                    <a href="#">S'accréditer</a>
+                </li>
+                <li>
+                    <a href="#">Programmation</a>
+                </li>
+                <li>
+                    <a href="#">Médiathèsuqe</a>
+                </li>
+                <li>
+                    <a href="#">À télécharger</a>
+                </li>
+            </ul>
+        </article>
+    </section>
+    </div>
+    <?php include('footer.html'); ?>
+
+    <!-- //// SCRIPTS \\\\ -->
+    <script src="js/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="js/bower_components/owl.carousel/src/js/owl.carousel.js"></script>
+    <script src="js/bower_components/chocolat/src/js/jquery.chocolat.js"></script>
+    <script src="js/bower_components/wavesurfer.js/dist/wavesurfer.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
