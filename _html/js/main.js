fe4544abbf1bf85b3c5e102710a612df7a04c57c
Clement Grellier <clementgrellier@gmail.com>
2015-10-13 12:17:09 +0200

    newsletter form

--- _html/js/main.js
+++ _html/js/main.js
@@ -20,6 +20,7 @@
 // 18. Contact
 // 19. Webtv
 // 20. FAQ
+// 21. Newsletter
 // =========================
 
 
@@ -1396,7 +1397,7 @@ $(document).ready(function() {
   }
 
     
-  // 19. FAQ
+  // 20. FAQ
   // =========================
     $(".faq-article-active").find("i.fa").removeClass("fa-plus").addClass("fa-minus");
     $(".faq-container article").click(function(){
@@ -1410,5 +1411,51 @@ $(document).ready(function() {
             i.removeClass("fa-plus").addClass("fa-minus");
         }
     });
+
+
+  // 21. Newsletter
+  // =========================
+
+  $('.newsletter #email').on('focus', function() {
+    if($(this).val() == "L'adresse e-mail n'est pas valide" || $(this).val() == "Veuillez saisir une adresse e-mail valide") {
+      $(this).val('');
+      $(this).removeClass('error');
+    }
+  });
+
+  // on submit : check if there are errors in the form
+  $('.newsletter form').on('submit', function() {
+
+    var input = $('.newsletter #email');
+    var empty = false;
+
+    if($('#email').val() == '') {
+      empty = true;
+      input.addClass("error").val("Veuillez saisir une adresse e-mail valide");
+    } else {
+
+      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+      var is_email=re.test(input.val());
+      if(is_email){
+        input.removeClass("error");
+      }
+      else {
+        input.addClass("error").val("L'adresse e-mail n'est pas valide");
+      }
+    }
+
+    if($('.newsletter .error').length || empty) {
+      return false;
+    } else {
+      // ajax call newsletter
+
+      // show confirmation 
+      $('.newsletter form').addClass('hide');
+      $('#confirmation span').html($('#email').val());
+      $('#confirmation').addClass('show');
+
+      return false;
+    }
+  });
 
 });
