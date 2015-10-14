48c5d08168e2e843fb025a9026443bc772ad1465
Clement Grellier <clementgrellier@gmail.com>
2015-10-14 17:50:35 +0200

    articles

--- _html/js/main.js
+++ _html/js/main.js
@@ -1403,14 +1403,15 @@ $(document).ready(function() {
               img = $(e.target).find('img').attr('src'),
               category = $(e.target).find('.category').text(),
               date = $(e.target).find('.date').text(),
-              text = $(e.target).find('p').text();
+              text = $(e.target).find('p').text(),
+              $popinAudio = $('.popin-audio');
             
-          $('.popin-audio').attr('data-sound', s);
-          $('.popin-audio').find('.image').css('background-image', 'url(' + img + ')');
-          $('.popin-audio').find('.category').text(category);
-          $('.popin-audio').find('.date').text(date);
-          $('.popin-audio').find('p').text(text);
-          $('.popin-audio').addClass('audio-player show');
+          $popinAudio.attr('data-sound', s);
+          $popinAudio.find('.image').css('background-image', 'url(' + img + ')');
+          $popinAudio.find('.category').text(category);
+          $popinAudio.find('.date').text(date);
+          $popinAudio.find('p').text(text);
+          $popinAudio.addClass('audio-player show');
 
           initAudioPlayers();
           $('.ov').addClass('show');
