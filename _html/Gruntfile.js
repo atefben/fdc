61cd09eef08dcb65284264ba41aa433ceaf3917c
Clement Grellier <clementgrellier@gmail.com>
2015-10-16 19:18:16 +0200

    animation + started grunt

--- _html/Gruntfile.js
+++ _html/Gruntfile.js
@@ -9,8 +9,7 @@ module.exports = function(grunt) {
       }
     },
     // COMPASS
-    compass :
-    {
+    compass : {
       dev : {
         options : {
           config    : 'config.rb',
@@ -26,11 +25,64 @@ module.exports = function(grunt) {
         files: '**/*.scss',
         tasks: ['compile']
       }
+    },
+    concat: {
+        generated: {
+            options: {
+                separator: ";"
+            }
+        }
+    },
+    tags: {
+      app: {
+          options: {
+              scriptTemplate: '<script src="{{ path }}"></script>',
+              openTag: '<!-- festival-cannes tags start -->',
+              closeTag: '<!-- festival-cannes tags end -->'
+          },
+          src: [
+            'js/*.js',
+
+            // Exclusions
+            '!js/**/app-mobile.module.js'
+          ],
+          dest: './scripts.inc.php.tmp'
+      }
+    },
+
+    useminPrepare: {
+      scripts: 'inc/scripts.inc.php',
+      options: {
+          root: './',
+          dest: './'
+      }
+    },
+
+    usemin: {
+      js: {
+        scripts: 'inc/scripts.dist.inc.php'
+      },
     }
   });
+
+  
+  grunt.loadNpmTasks('grunt-script-link-tags');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-usemin');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   
   grunt.registerTask('compile', ['compass:dev']);
+
+  /**
+   * Build JavaScript only
+   **/
+  grunt.registerTask('build:js', [
+      'tags:app',
+      'useminPrepare',
+      'concat:generated',
+      'usemin:js',
+  ]);
 }
