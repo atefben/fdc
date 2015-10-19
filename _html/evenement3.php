b45428f7e149c5576b15648d2d6cf3b942556870
Jacques tatiana <tatout08090@gmail.com>
2015-10-19 18:53:29 +0200

    event detail

---
+++ _html/evenement3.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <!-- //// META \\\\ -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+
+    <!-- //// TITLE \\\\ -->
+    <title>Festival de Cannes 2016 - HTML</title>
+
+    <!-- //// CSS \\\\ -->
+    <link href='js/bower_components/chocolat/src/css/chocolat.css' rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/47cc6bed-5912-4140-bc5c-4caa2425b61d.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700,400italic' rel='stylesheet' type='text/css'>
+    <link href='css/styles.css' rel="stylesheet">
+  
+  </head>
+
+  <body>
+    <?php include('header.html'); ?>
+
+    <div id="main" class="single-article single-evenement">
+      <div class="content-article">
+        <div class="container">
+          <a href="article2.php" class="nav prev"></a>
+          <a href="article3.php" class="nav next"></a>
+          <div class="info">
+            <a href="#" class="category">Rendez-vous Européen</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+            <span class="update">Mise à jour :</span><span class="date">18.05.15</span> . <span class="hour">10:00</span>
+          </div>
+          <h2 class="title-article">Le Rendez-vous Européen</h2>
+          <div class="buttons">
+            <a id="share-article" href="#" class="button">Partager</a>
+            <a href="#" class="button print">Imprimer</a>
+          </div>
+          <div class="single-photo">
+            <div class="photo-container">
+              <img src="img/evenements/evenement3.jpg" alt="" />
+              <p class="caption">Équipe du film - Photocall - The Lobster © FDC / Théophile Delange</p>
+            </div>
+          </div>
+          <p class="chapeau">Parce que le FESTIVAL DE CANNES présente des films prestigieux, témoins de la créativité du cinéma mondial et notamment des cinéastes européens, la Commission européenne s’est toujours attachée à célébrer son amour pour le cinéma à cette occasion.</p>
+          <div class="text">
+            <p class="paragraph">
+            Cette année, Androulla VASSILIOU, Commissaire européenne en charge de la culture, vient encore une fois illustrer cet engagement pour le cinéma au cœur du 67e Festival de Cannes, qui accueille pas moins de 21 films soutenus par le programme EUROPE CREATIVE de la Commission européenne, dans toutes ses sélections. <br>
+
+            Samedi 17 mai, elle remettra le Prix MEDIA de l’Union Européenne au cinéaste bosniaque Danis Tanović, pour le projet What are you looking at ? qu’il partage avec son co-auteur Predrag Kojovic et sa productrice, Amra Baksic Camo (SCCA/pro.ba, Sarajevo). <br>
+
+            Ce prix, qui récompense un projet au fort potentiel européen, a distingué en 2012 Le Passé, d’Asghar Farhadi et en 2013 Kollektivet (The Commune) de Thomas Vinterberg. Il illustre les actions de l’Union Européenne en faveur de la création et notamment le nouveau programme EUROPE CREATIVE qui prend le relais des programmes MEDIA et Culture à partir de cette année. <br>
+
+            Doté d’un budget de 1.5 milliard d’euros pour les sept prochaines années et d’un nouveau fond de garantie destiné à faciliter l’accès au financement pour les secteurs culturels et créatifs d’un montant de 750 millions d’euros, EUROPE CREATIVE permet ainsi d’assurer aujourd’hui la continuité de ce qui a été entrepris depuis plus de 20 ans : renforcer l'offre culturelle européenne dans toute sa diversité et soutenir l'industrie en lui offrant de nouvelles perspectives de développement. <br>
+
+            « A l’image des 21 films soutenus par l’Europe et présentés cette année au Festival, il contribuera à la réalisation et la distribution de films éblouissants dont les histoires, parfois ancrées dans des territoires bien précis, dépassent leurs frontières pour véhiculer au public du monde entier des valeurs universels et lui offrir un cinéma de qualité. » a déclaré 
+            Androulla VASSILIOU. <br>
+
+            Cette année Androulla VASSILIOU invite également, au cours d’une conférence, les professionnels européens du film à discuter des défis auxquels ils sont confrontés et à aborder l’impératif toujours plus pressant dans un contexte de profond changement technologique, d'éveiller et de maintenir la curiosité et l'intérêt des publics pour le cinéma, et en particulier l'intérêt des plus jeunes. Impératif placé au cœur de l'action du nouveau programme EUROPE CREATIVE.
+            </p>
+        </div>
+        </div>
+        <div class="share">
+          <p>Partagez l'article</p>
+          <div class="buttons square">
+            <a href="#" class="button facebook"></a>
+            <a href="#" class="button twitter"></a>
+            <a href="#" class="button link"></a>
+            <a href="#" class="button email"></a>
+            <a href="#" class="button print"></a>
+          </div>
+        </div>
+        <div class="film">
+          <div class="programmation-film">
+            <div class="wrapper">
+              <div class="vCenter">
+                <div class="vCenterKid">
+                  <span class="category">Programme</span>
+                  <ul>
+                    <li>
+                      <span class="type">Projection</span><span>15/07/2015</span><span>9h</span>
+                      <span class="location">Salle Debussy</span>
+                    </li>
+                    <li>
+                      <span class="type">Projection</span><span>15/07/2015</span><span>10h</span>
+                      <span class="location">Cinéma de la plage</span>
+                    </li>
+                    <li>
+                      <span class="type">Projection</span><span>15/07/2015</span><span>9h</span>
+                      <span class="location">Salle du Soixantième</span>
+                    </li>
+                    <li>
+                      <span class="type">Projection</span><span>15/07/2015</span><span>9h</span>
+                      <span class="location">Grand Théâtre Lumière</span>
+                    </li>
+                  </ul>
+                  <a href="#" class="button">Toute la programmation</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="focus">
+            <h2 class="title">Focus</h2>
+            <div class="articles">
+              <article class="article" data-format="article" data-theme="competition">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">Compétition</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Stéphane Brizé interroge la loi du marché</a></h2>
+                </div>
+              </article>
+              <article class="video" data-format="video" data-theme="photocall">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">Photocall</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Z de Costa-Gravas</a></h2>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class="same-day">
+            <h2 class="title">Le même jour</h2>
+            <div class="articles center">
+              <article class="article" data-format="article" data-theme="competition">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">Compétition</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Stéphane Brizé interroge la loi du marché</a></h2>
+                </div>
+              </article>
+              <article class="video" data-format="video" data-theme="photocall">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">Photocall</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Z de Costa-Gravas</a></h2>
+                </div>
+              </article>
+              <article class="photo" data-format="photo" data-theme="photograph">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">L'oeil du photographe</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Renaud, réalisateur indépendant canadien © Balint Porneczi / supported by [NEUS]</a></h2>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+      <span class="anim"></span>
+    </div>
+
+    <?php include('footer.html'); ?>
+
+    <!-- //// SCRIPTS \\\\ -->
+    <?php include('scripts.inc.php'); ?>
+  </body>
+</html>
--- _html/evenements.php
+++ _html/evenements.php
@@ -65,10 +65,10 @@
         </div>
         <div class="item theme1 date1 date theme article evenement">
           <article class="article evenement">
-            <div class="img"><span class="picto"></span><a href="evenement.php"><img src='img/evenements/evenement1.jpg' /></a><a href="#" class="read-later"><span>Enragés, polar hybride d'Éric Hannezo</span></a></div>
+            <div class="img"><span class="picto"></span><a href="evenement3.php"><img src='img/evenements/evenement3.jpg' /></a><a href="#" class="read-later"><span>Le Rendez-vous Européen</span></a></div>
             <div class="info">
-              <a href="#" class="category">Expositions</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
-              <h2><a href="evenement.php">Enragés, polar hybride d'Eric Hannezo</a></h2>
+              <a href="#" class="category">Rendez-vous européen</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+              <h2><a href="evenement.php">Rendez-vous européen</a></h2>
             </div>
           </article>
         </div>
