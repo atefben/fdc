725d54948491b530b7f5e69530b28780a94ece99
Jacques tatiana <tatout08090@gmail.com>
2015-10-19 18:08:44 +0200

    evenement detail

---
+++ _html/evenement2.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <!-- //// META \\\\ -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+
+    <!-- //// TITLE \\\\ -->
+    <title>Festival de Cannes 2016 - HTML</title>
+
+    <!-- //// CSS \\\\ -->
+    <link href='js/bower_components/chocolat/src/css/chocolat.css' rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/47cc6bed-5912-4140-bc5c-4caa2425b61d.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700,400italic' rel='stylesheet' type='text/css'>
+    <link href='css/styles.css' rel="stylesheet">
+  
+  </head>
+
+  <body>
+    <?php include('header.html'); ?>
+
+    <div id="main" class="single-article single-evenement">
+      <div class="content-article">
+        <div class="container small-container">
+          <a href="article2.php" class="nav prev"></a>
+          <a href="article3.php" class="nav next"></a>
+          <div class="info">
+            <a href="#" class="category">Leçon de cinéma</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+            <span class="update">Mise à jour :</span><span class="date">18.05.15</span> . <span class="hour">10:00</span>
+          </div>
+          <h2 class="title-article">Marco Bellocchio</h2>
+          <div class="buttons">
+            <a id="share-article" href="#" class="button">Partager</a>
+            <a href="#" class="button print">Imprimer</a>
+          </div>
+          <div class="single-photo">
+            <div class="photo-container">
+              <img src="img/evenements/evenement2.jpg" alt="" />
+              <p class="caption">Équipe du film - Photocall - The Lobster © FDC / Théophile Delange</p>
+            </div>
+          </div>
+          <div class="quote">
+            <blockquote>
+              Au cinéma comme en amour, il faut savoir s’abandonner. Le réalisateur ne doit pas tout expliquer.
+            </blockquote>
+          </div>
+          <div class="text">
+            <p class="text-quote">“…Une leçon de cinéma n’a de sens, à mes yeux, que si elle est pratique, le travail sur le plateau, les prises, diriger l’équipe, les acteurs, commander, fais ceci, fais cela... Sur le plateau, la démocratie, l’égalité n’existent pas, il faut de la rigueur, de l’intérêt, de l’affection, du respect... Je ne crois pas en ces réalisateurs qui, pour faire pleurer, pensent être en droit de gifler une belle fille ou de l’insulter, etc. (on raconte souvent l’histoire de ce réalisateur de la fin du néo-réalisme qui, pour faire pleurer une toute jeune actrice, la frappait sur les jambes avec une cravache tandis qu’il la filmait en gros plan.)… Mais dans la mesure où je ne peux ici apporter qu’un témoignage oral, je dis que la chose la plus précieuse qu’un réalisateur puisse enseigner à ceux qui veulent faire ce métier (un métier, entre autres, extrêmement compliqué et je suis toujours étonné qu’il séduise tant de jeunes) c’est le travail avec les acteurs et les actrices… Parce que ce n’est pas la même chose de diriger un acteur ou une actrice lorsqu’il y a nécessité de convaincre, de “séduire” (et la “séduction” uniquement “artistique” d’une actrice est totalement différente de la “séduction” d’un acteur), voire d’être “séduits”, ce qui ne signifie pas nécessairement être fragiles ou passifs… Tout le monde peut enseigner la technique mais la manière de faire interpréter un personnage que vous avez imaginé par un être humain vivant est un don de la nature. On peut toutefois l’apprendre, en partie, d’un metteur en scène qui ne se soustrait pas au risque d’un échec…
+Il est impossible de garantir une jolie fin à toute relation humaine…”</p>
+<p>
+<p class="paragraph">
+Marco Bellocchio, après des études d’art dramatique et de cinéma, réalise en 1965 un premier long métrage remarqué par la critique. Ses oeuvres engagées dénoncent les symboles du conformisme italien : après le film culte <a href="#" title="les poings dans les poches">Les Poings dans les poches </a>( 1966), manifeste d’une jeunesse en révolte, il dénonce la religion dans <a href="#" title='au nom du père'>Au nom du père</a> (1971) ou l’armée dans <a href="#" alt="La marche trimphale">La Marche triomphale </a>(1976). Avec Michel Piccoli et Anouk Aimée, il remporte à Cannes deux prix d’interprétation pour <a href="#" title="Le saut dans le vide">Le Saut dans le vide</a> (1980). 
+</p>
+<p class="paragraph">
+
+Il réalise aussi des adaptations littéraires, comme Diable au corps qui fait scandale à Cannes en 1986, ou <a href="#" title="La nourrice">La Nourrice</a> (1990) d’après Pirandello. Marco Bellocchio dérange à nouveau le Vatican avec <a href="#" title="Le Sourire de ma mère">Le Sourire de ma mère</a> (Compétition Festival de Cannes - 2002). Il sera le premier à évoquer l’activisme des brigades rouges dans Buongiorno, notte présenté à la Mostra en 2004. En 2009, il présente Vincere en Compétition à Cannes, salué par la critique internationale.</p>
+          </div>
+        <h2 class="title">Revivez la Leçon de Cinéma de Marco Bellochio en photos</h2>
+                  <div class="slideshow">
+            <div class="slideshow-img">
+              <div class="images">
+                <div class="img active">
+                  <a id="photo1" class="chocolat-image" href="img/slide001.jpg" title='<h2>Sur le tournage de "Deephan" de Jacques Audiard</h2><p>Crédit Image : VALERY HACHE / AFP</p>'>
+                    <img src="img/evenements/slide-evenement.jpg" alt="" />
+                  </a>
+                </div>
+                <div class="img">
+                  <a id="photo2" class="chocolat-image" href="http://cdn-parismatch.ladmedia.fr/var/news/storage/images/paris-match/culture/cinema/festival-de-cannes-2013-audrey-tautou-maitresse-de-ceremonie-508580/4577783-1-fre-FR/Festival-de-Cannes-2013-Audrey-Tautou-maitresse-de-ceremonie.jpg"><img src="img/evenements/slide-evenement.jpg" alt="" /></a>
+                </div>
+                <div class="img"><img src="img/evenements/slide-evenement.jpg" alt="" /></div>
+                <div class="img"><img src="img/evenements/slide-evenement.jpg" alt="" /></div>
+                <div class="img"><img src="img/evenements/slide-evenement.jpg" alt="" /></div>
+                <div class="img"><img src="img/evenements/slide-evenement.jpg" alt="" /></div>
+                <div class="img"><img src="img/evenements/slide-evenement.jpg" alt="" /></div>
+                <div class="img"><img src="img/evenements/slide-evenement.jpg" alt="" /></div>
+                                <div class="img"><img src="img/evenements/slide-evenement.jpg" alt="" /></div>
+                                                <div class="img"><img src="img/evenements/slide-evenement.jpg" alt="" /></div>
+              </div>
+              <div class="owl-carousel thumbnails">
+                <div data-id="photo1" class="thumb active">
+                  <img src="img/evenements/evenement-slide-min2.jpg" />
+                </div>
+                <div data-id="photo2" class="thumb">
+                  <img src="img/evenements/evenement-slide-min1.jpg" />
+                </div>
+                <div data-id="photo3" class="thumb">
+                  <img src="img/evenements/evenement-slide-min2.jpg" />
+                </div>
+                <div class="thumb">
+                  <img src="img/evenements/evenement-slide-min2.jpg" />
+                </div>
+                <div class="thumb">
+                  <img src="img/evenements/evenement-slide-min2.jpg" />
+                </div>
+                <div class="thumb">
+                  <img src="img/evenements/evenement-slide-min2.jpg" />
+                </div>
+                <div class="thumb">
+                  <img src="img/evenements/evenement-slide-min2.jpg" />
+                </div>
+                <div class="thumb">
+                  <img src="img/evenements/evenement-slide-min2.jpg" />
+                </div>
+                <div class="thumb">
+                  <img src="img/evenements/evenement-slide-min2.jpg" />
+                </div>
+              </div>
+            </div>
+            <p class="caption"><strong>Équipe du film - Photocall - The Lobster</strong> © FDC / Théophile Delange</p>
+          </div>
+          <h2 class="title">Revivez la Leçon de Cinéma de Marco Bellocchio en audio</h2>
+           <div class="audio-player audio" data-sound="img/article/sound.mp3">
+            <div class="image" style="background-image: url(img/slide001.jpg);"></div>
+            <a href="#" class="playpause"></a>
+            <div class="off">
+              <div class="vCenter">
+                <div class="vCenterKid">
+                  <div class="picto"></div>
+                  <div class="info">
+                    <a href="#" class="category">Leçon de cinéma</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                    <p>Marco Bellocchio</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="on">
+              <div class="vCenter">
+                <div class="vCenterKid">
+                  <div class="duration">
+                    <span class="curr">0:00</span>/<span class="total">0:00</span>
+                  </div>
+                  <div class="wave-container"></div>
+                  <div class="volume">
+                    <span></span>
+                  </div>
+                  <a href="#" class="fullscreen"></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="share">
+          <p>Partagez l'article</p>
+          <div class="buttons square">
+            <a href="#" class="button facebook"></a>
+            <a href="#" class="button twitter"></a>
+            <a href="#" class="button link"></a>
+            <a href="#" class="button email"></a>
+            <a href="#" class="button print"></a>
+          </div>
+        </div>
+        <div class="film">
+          <div class="info-film">
+            <div class="wrapper">
+              <div class="vCenter">
+                <div class="vCenterKid">
+                  <div class="poster">
+                    <img src="img/article/007.jpg" alt="" />
+                  </div>
+                  <div class="text-container">
+                    <span class="category">Un certain regard</span>
+                    <h3>Enragé</h3>
+                    <p class="director">Eric HANNEZO<p>
+                    <p>France - 2014</p>
+                    <p>1h35</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="programmation-film">
+            <div class="wrapper">
+              <div class="vCenter">
+                <div class="vCenterKid">
+                  <span class="category">Programmation</span>
+                  <ul>
+                    <li>
+                      <span class="type">Projection</span><span>15/07/2015</span><span>9h</span>
+                      <span class="location">Salle Debussy</span>
+                    </li>
+                    <li>
+                      <span class="type">Projection</span><span>15/07/2015</span><span>10h</span>
+                      <span class="location">Cinéma de la plage</span>
+                    </li>
+                    <li>
+                      <span class="type">Projection</span><span>15/07/2015</span><span>9h</span>
+                      <span class="location">Salle du Soixantième</span>
+                    </li>
+                    <li>
+                      <span class="type">Projection</span><span>15/07/2015</span><span>9h</span>
+                      <span class="location">Grand Théâtre Lumière</span>
+                    </li>
+                  </ul>
+                  <a href="#" class="button">Toute la programmation</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="focus">
+            <h2 class="title">Focus</h2>
+            <div class="articles">
+              <article class="article" data-format="article" data-theme="competition">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">Compétition</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Stéphane Brizé interroge la loi du marché</a></h2>
+                </div>
+              </article>
+              <article class="video" data-format="video" data-theme="photocall">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">Photocall</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Z de Costa-Gravas</a></h2>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div class="same-day">
+            <h2 class="title">Le même jour</h2>
+            <div class="articles center">
+              <article class="article" data-format="article" data-theme="competition">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">Compétition</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Stéphane Brizé interroge la loi du marché</a></h2>
+                </div>
+              </article>
+              <article class="video" data-format="video" data-theme="photocall">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">Photocall</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Z de Costa-Gravas</a></h2>
+                </div>
+              </article>
+              <article class="photo" data-format="photo" data-theme="photograph">
+                <div class="image">
+                  <div class="image-wrapper">
+                    <img src="img/articles/03.jpg" alt="" />
+                  </div>
+                  <a href="article.php" class="linkImage"></a>
+                  <span class="picto"></span>
+                </div>
+                <div class="info">
+                  <a href="#" class="category">L'oeil du photographe</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+                  <h2><a href="article.php">Renaud, réalisateur indépendant canadien © Balint Porneczi / supported by [NEUS]</a></h2>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+      <span class="anim"></span>
+    </div>
+
+    <?php include('footer.html'); ?>
+
+    <!-- //// SCRIPTS \\\\ -->
+    <?php include('scripts.inc.php'); ?>
+  </body>
+</html>
--- _html/evenements.php
+++ _html/evenements.php
@@ -56,10 +56,10 @@
         </div>
          <div class="item theme1 date1 date theme article evenement">
           <article class="article evenement">
-            <div class="img"><span class="picto"></span><a href="evenement.php"><img src='img/evenements/evenement1.jpg' /></a><a href="#" class="read-later"><span>Enragés, polar hybride d'Éric Hannezo</span></a></div>
+            <div class="img"><span class="picto"></span><a href="evenement2.php"><img src='img/evenements/evenement2.jpg' /></a><a href="#" class="read-later"><span>Marco Bellocchio</span></a></div>
             <div class="info">
-              <a href="#" class="category">Expositions</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
-              <h2><a href="evenement.php">Enragés, polar hybride d'Eric Hannezo</a></h2>
+              <a href="#" class="category">Leçon de cinéma</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+              <h2><a href="evenement.php">Marco Bellocchio</a></h2>
             </div>
           </article>
         </div>
