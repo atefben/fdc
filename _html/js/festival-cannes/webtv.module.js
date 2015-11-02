55391795d00f4daf336f779a89f98dd87455c4da
Clement Grellier <clementgrellier@gmail.com>
2015-11-02 18:03:19 +0100

    webtv animation + header search border

--- _html/js/festival-cannes/webtv.module.js
+++ _html/js/festival-cannes/webtv.module.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
      $('#live .play').on('click', function(e) {
        e.preventDefault();
        $('#live').addClass('on');
-       $('#live .img').addClass('rePosition');
-       $('#live').data('height', $('#live').height()).height($(window).height() - 91).css('padding-top', '91px');
-       $('body').css('padding-top', '91px');
+       //$('#live .img').addClass('rePosition');
+       $('#live').data('height', $('#live').height()).height($(window).height() - 91);
+       $('#main').css('padding-top', '91px');
        setTimeout(function() {
           $('header').addClass('sticky');
         }, 800);
