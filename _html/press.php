69e00d606765e29fffbed234fdb7b4779e422658
Clement Grellier <clementgrellier@gmail.com>
2015-11-13 17:34:54 +0100

    start press

---
+++ _html/press.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <!-- //// META \\\\ -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+
+    <!-- //// TITLE \\\\ -->
+    <title>Festival de Cannes 2016 - HTML</title>
+
+    <!-- //// CSS \\\\ -->
+    <link href='js/bower_components/chocolat/src/css/chocolat.css' rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/47cc6bed-5912-4140-bc5c-4caa2425b61d.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700,400italic' rel='stylesheet' type='text/css'>
+    <link href='css/styles.css' rel="stylesheet">
+  
+  </head>
+
+  <body>
+    <?php include('header.html'); ?>
+
+    <div id="main" class="press loading">
+      <div class="header-press">
+        <div class="head">
+          <span>Espace presse</span>
+        </div>
+        <h2>Accueil</h2>
+        <p>
+          <span class="vCenter">
+            <span class="vCenterKid">L'espace presse met également à la disposition du grand public des contenus en libre accès. Journalistes, pour visualiser les contenus et services qui vous sont exclusivement réservés, nous vous invitons à saisir le code qui vous a été délivré par le service de presse.</span>
+          </span>
+        </p>
+      </div>
+      <div class="section participate">
+        <div class="img"></div>
+        <div class="txt vCenter">
+          <div class="vCenterKid">
+            <h2 class="title">Participer</h2>
+            <p class="link"><a href="#"><img src="img/svg/arrow-right-gold.svg" />Accéder</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="section accrediter">
+        <div class="img"></div>
+        <div class="txt vCenter">
+          <div class="vCenterKid">
+            <h2 class="title">S'accréditer</h2>
+            <p class="link"><a href="#"><img src="img/svg/arrow-right-gold.svg" />Accéder</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="contact-press">
+        <h2 class="title">Contactez-nous</h2>
+        <div class="container">
+          <div class="col">
+            <h3>Festival de Cannes - Service de Presse</h3>
+            <p></p>
+          </div>
+          <div class="col">
+            <a href="contact.php" class="button">formulaire de contact</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include('footer.html'); ?>
+
+    <!-- //// SCRIPTS \\\\ -->
+    <?php include('scripts.inc.php'); ?>
+  </body>
+</html>
