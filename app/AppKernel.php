f4f2ea9db8c598dfaa27518eb180ae9024da25d7
Antoine Mineau <am2@ohwee.fr>
2015-11-30 18:54:37 +0100

    added press bundle, session in database

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -60,6 +60,7 @@ class AppKernel extends Kernel
             new Base\ApiBundle\BaseApiBundle(),
             new FDC\CorporateBundle\FDCCorporateBundle(),
             new FDC\EventBundle\FDCEventBundle(),
+            new FDC\PressBundle\FDCPressBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
