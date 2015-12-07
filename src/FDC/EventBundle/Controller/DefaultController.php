589150b3069f6d3ce7051f2fff5beeab41de126b
lrocher <lrocher@webqam.fr>
2015-12-07 18:28:40 +0100

    implement sass with assetic / start header

--- src/FDC/EventBundle/Controller/DefaultController.php
+++ src/FDC/EventBundle/Controller/DefaultController.php
@@ -17,6 +17,64 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array();
+
+        $mainNav = array(
+            array(
+                'href' => '/',
+                'title' => 'Festival 2016'
+            ),
+            array(
+                'href' => 'http://www.festival-cannes.com/fr.html',
+                'title' => 'Festival de Cannes'
+            ),
+            array(
+                'href' => 'http://www.cinefondation.com/fr/',
+                'title' => 'Cinéfondation'
+            ),
+            array(
+                'href' => 'http://www.cannescourtmetrage.com/fr/',
+                'title' => 'Cannes Court Métrage'
+            ),
+            array(
+                'href' => 'http://www.marchedufilm.com/fr/',
+                'title' => 'Marché du film'
+            )
+        );
+        $userNav = array(
+            array(
+                'href' => '#',
+                'title' => 'Ma Sélection'
+            ),
+            array(
+                'href' => 'http://sub.festival-cannes.fr/',
+                'title' => '>Mon compte'
+            )
+        );
+
+        $langNav = array(
+            array(
+                'current' => true,
+                'iso' => 'FR'
+            ),
+            array(
+                'current' => false,
+                'iso' => 'EN'
+            ),
+            array(
+                'current' => false,
+                'iso' => 'ES'
+            ),
+            array(
+                'current' => false,
+                'iso' => 'ZH'
+            ),
+        );
+
+        return array(
+            'mainNav' => $mainNav,
+            'userNav' => $userNav,
+            'langNav' => $langNav,
+            );
+
     }
 }
