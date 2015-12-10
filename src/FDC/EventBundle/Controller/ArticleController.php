43d06aa6ae8a1b76aa2ecaebc451f8f7a8fb2d96
lrocher <lrocher@webqam.fr>
2015-12-10 17:34:58 +0100

    remove selections method

--- src/FDC/EventBundle/Controller/ArticleController.php
+++ src/FDC/EventBundle/Controller/ArticleController.php
@@ -2,198 +2,98 @@
 
 namespace FDC\EventBundle\Controller;
 
+use Guzzle\Http\Message\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
 
 class ArticleController extends Controller
 {
 
     /**
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/suggestion", options={"expose"=true})
      */
-    public function filtersAction()
+    public function suggestionAction(Request $request)
     {
-        $filters = array(
-            'all',
-            'articles',
-            'videos',
-            'audios'
-        );
+        $articles = array();
 
-        $nbArticle = 15;
+        if($request->isXmlHttpRequest()) {
 
-        return $this->render(
-            "FDCEventBundle:Article:article.filter.html.twig",
-            array(
-                'filters' => $filters,
-                'nbArticle' => $nbArticle,
-            )
-        );
+            $queryArticle = array(
+                array(
+                    'title' => 'test',
+                    'content' => 'Contenu de l\'article',
+                    'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                    'filter' => 'suggestion',
+                    'link'   => '/article/article1.php',
+                    'date'   => '18.05.15',
+                    'time'   => '09:00',
+                    'category' => 'presse'
+                ),
+                array(
+                    'title' => 'test',
+                    'content' => 'Contenu de l\'article',
+                    'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                    'filter' => 'suggestion',
+                    'link'   => '/article/article1.php',
+                    'date'   => '18.05.15',
+                    'time'   => '09:00',
+                    'category' => 'presse'
+                ),
+                array(
+                    'title' => 'test',
+                    'content' => 'Contenu de l\'article',
+                    'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                    'filter' => 'suggestion',
+                    'link'   => '/article/article1.php',
+                    'date'   => '18.05.15',
+                    'time'   => '09:00',
+                    'category' => 'presse'
+                ),
+                array(
+                    'title' => 'test',
+                    'content' => 'Contenu de l\'article',
+                    'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                    'filter' => 'suggestion',
+                    'link'   => '/article/article1.php',
+                    'date'   => '18.05.15',
+                    'time'   => '09:00',
+                    'category' => 'presse'
+                ),
+                array(
+                    'title' => 'test',
+                    'content' => 'Contenu de l\'article',
+                    'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                    'filter' => 'suggestion',
+                    'link'   => '/article/article1.php',
+                    'date'   => '18.05.15',
+                    'time'   => '09:00',
+                    'category' => 'presse'
+                ),
+                array(
+                    'title' => 'test',
+                    'content' => 'Contenu de l\'article',
+                    'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                    'filter' => 'suggestion',
+                    'link'   => '/article/article1.php',
+                    'date'   => '18.05.15',
+                    'time'   => '09:00',
+                    'category' => 'presse'
+                ),
+                array(
+                    'title' => 'test',
+                    'content' => 'Contenu de l\'article',
+                    'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                    'filter' => 'suggestion',
+                    'link'   => '/article/article1.php',
+                    'date'   => '18.05.15',
+                    'time'   => '09:00',
+                    'category' => 'presse'
+                )
+            );
+            $articles = array_slice($queryArticle, 0, 8);
 
-    }
-
-    /**
-     * @param $filter
-     * @return array
-     */
-    public function selectionAction($filter)
-    {
-
-        //Todo Grab user articles from $filter category
-
-        $articles = array(
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => $filter,
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => $filter,
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => $filter,
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => $filter,
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => $filter,
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => $filter,
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            )
-        );
-
-        return $this->render(
-            "FDCEventBundle:Article:article.selection.$filter.html.twig",
-            array('articles' => $articles)
-        );
-
-    }
-
-    /**
-     * @param int $max
-     * @return array
-     */
-    public function suggestionAction($max)
-    {
-
-        $queryArticle = array(
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => 'suggestion',
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => 'suggestion',
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => 'suggestion',
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => 'suggestion',
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => 'suggestion',
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => 'suggestion',
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Contenu de l\'article',
-                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
-                'filter' => 'suggestion',
-                'link'   => '/article/article1.php',
-                'date'   => '18.05.15',
-                'time'   => '09:00',
-                'category' => 'presse'
-            )
-        );
-
-        $articles = array_slice($queryArticle, 0, $max);
+        }
 
         return $this->render(
             'FDCEventBundle:Article:article.suggestion.html.twig',
