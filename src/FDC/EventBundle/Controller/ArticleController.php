39d8aebabde8b76cd9699ddb1f780a42cfb5f9c4
lrocher <lrocher@webqam.fr>
2015-12-10 15:19:01 +0100

    ticket #476

--- src/FDC/EventBundle/Controller/ArticleController.php
+++ src/FDC/EventBundle/Controller/ArticleController.php
@@ -11,73 +11,192 @@ class ArticleController extends Controller
 {
 
     /**
-     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function filtersAction()
+    {
+        $filters = array(
+            'all',
+            'articles',
+            'videos',
+            'audios'
+        );
+
+        $nbArticle = 15;
+
+        return $this->render(
+            "FDCEventBundle:Article:article.filter.html.twig",
+            array(
+                'filters' => $filters,
+                'nbArticle' => $nbArticle,
+            )
+        );
+
+    }
+
+    /**
+     * @param $filter
+     * @return array
+     */
+    public function selectionAction($filter)
+    {
+
+        //Todo Grab user articles from $filter category
+
+        $articles = array(
+            array(
+                'title' => 'test',
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => $filter,
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
+            ),
+            array(
+                'title' => 'test',
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => $filter,
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
+            ),
+            array(
+                'title' => 'test',
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => $filter,
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
+            ),
+            array(
+                'title' => 'test',
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => $filter,
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
+            ),
+            array(
+                'title' => 'test',
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => $filter,
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
+            ),
+            array(
+                'title' => 'test',
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => $filter,
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
+            )
+        );
+
+        return $this->render(
+            "FDCEventBundle:Article:article.selection.$filter.html.twig",
+            array('articles' => $articles)
+        );
+
+    }
+
+    /**
      * @param int $max
      * @return array
-     *
      */
-    public function selectionAction($max)
+    public function suggestionAction($max)
     {
 
         $queryArticle = array(
             array(
                 'title' => 'test',
-                'content' => 'Double test'
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => 'suggestion',
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
             ),
             array(
                 'title' => 'test',
-                'content' => 'Double test'
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => 'suggestion',
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
             ),
             array(
                 'title' => 'test',
-                'content' => 'Double test'
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => 'suggestion',
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
             ),
             array(
                 'title' => 'test',
-                'content' => 'Double test'
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => 'suggestion',
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
             ),
             array(
                 'title' => 'test',
-                'content' => 'Double test'
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => 'suggestion',
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
             ),
             array(
                 'title' => 'test',
-                'content' => 'Double test'
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => 'suggestion',
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
             ),
             array(
                 'title' => 'test',
-                'content' => 'Double test'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Double test'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Double test'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Double test'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Double test'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Double test'
-            ),
-            array(
-                'title' => 'test',
-                'content' => 'Double test'
-            ),
+                'content' => 'Contenu de l\'article',
+                'thumbnail' => '//lorempixel.com/212/133/sports/1/Dummy-Text/',
+                'filter' => 'suggestion',
+                'link'   => '/article/article1.php',
+                'date'   => '18.05.15',
+                'time'   => '09:00',
+                'category' => 'presse'
+            )
         );
 
         $articles = array_slice($queryArticle, 0, $max);
 
         return $this->render(
-            'FDCEventBundle:Article:article.selection.html.twig',
+            'FDCEventBundle:Article:article.suggestion.html.twig',
             array('articles' => $articles)
         );
 
