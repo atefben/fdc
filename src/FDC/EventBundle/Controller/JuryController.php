1f963d5f0314e186edd521d1e58f424fe64f4314
lrocher <lrocher@webqam.fr>
2015-12-14 14:58:21 +0100

    page juries

--- src/FDC/EventBundle/Controller/JuryController.php
+++ src/FDC/EventBundle/Controller/JuryController.php
@@ -9,11 +9,94 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class JuryController extends Controller
 {
     /**
-     * @Route("/juries")
-     * @Template()
+     * @Route("/juries-{section}")
+     *
+     * @param section
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function getJury($section)
     {
-        return array();
+        $president= array();
+        $members= array();
+
+        $allPresident = array(
+            'id' => 0,
+            'img' => 'img.jpg',
+            'name' => 'Jane Campion',
+            'description' => 'lorem ipsum',
+            'functions' => 'Réalisatrice, Scénariste, Productrice'
+        );
+
+        $allMembers = array(
+            array(
+                'id' => 0,
+                'img' => 'img.jpg',
+                'name' => 'Jane Campion',
+                'functions' => 'Réalisatrice, Scénariste, Productrice'
+            ),
+            array(
+                'id' => 0,
+                'img' => 'img.jpg',
+                'name' => 'Jane Campion',
+                'functions' => 'Réalisatrice, Scénariste, Productrice'
+            ),
+            array(
+                'id' => 0,
+                'img' => 'img.jpg',
+                'name' => 'Jane Campion',
+                'functions' => 'Réalisatrice, Scénariste, Productrice'
+            ),
+            array(
+                'id' => 0,
+                'img' => 'img.jpg',
+                'name' => 'Jane Campion',
+                'functions' => 'Réalisatrice, Scénariste, Productrice'
+            ),
+            array(
+                'id' => 0,
+                'img' => 'img.jpg',
+                'name' => 'Jane Campion',
+                'functions' => 'Réalisatrice, Scénariste, Productrice'
+            )
+        );
+
+        switch ($section) {
+            case 'longs-metrages':
+                $members = $allMembers;
+                $president = $allPresident;
+                break;
+            case 'cinefondation':
+                $members = $allMembers;
+                $president = $allPresident;
+                break;
+            case 'certain-regard':
+                $members = $allMembers;
+                $president = $allPresident;
+                break;
+            case 'camera-dor':
+                $members = $allMembers;
+                $president = $allPresident;
+                break;
+        }
+
+        return $this->render(
+            "FDCEventBundle:Jury:jury.section.html.twig",
+            array(
+                'members' => $members,
+                'president' => $president,
+            )
+        );
+
+    }
+
+    /**
+     * @Route("/artiste-{id}")
+     *
+     * @param id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getArtiste($id)
+    {
+
     }
 }
