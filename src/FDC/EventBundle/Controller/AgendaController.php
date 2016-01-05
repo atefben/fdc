6f3b7b0c8b64877822c35acd92247f1c61ae833d
lrocher <lrocher@webqam.fr>
2016-01-05 14:35:04 +0100

    agenda controller, programmation & agenda page

---
+++ src/FDC/EventBundle/Controller/AgendaController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace FDC\EventBundle\Controller;
+
+use Guzzle\Http\Message\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+/**
+ * @Route("/programmation")
+ * Class ArtistController
+ * @package FDC\EventBundle\Controller
+ */
+class AgendaController extends Controller
+{
+
+    /**
+     * @Route("/")
+     * @Template("FDCEventBundle:Agenda:scheduling.html.twig")
+     *
+     */
+    public function schedulingAction()
+    {
+        $schedulingDays = array(
+            array(
+                'date' => new \DateTime(),
+            ),
+            array(
+                'date' => new \DateTime(),
+            ),
+            array(
+                'date' => new \DateTime(),
+            ),
+            array(
+                'date' => new \DateTime(),
+            ),
+            array(
+                'date' => new \DateTime(),
+            ),
+            array(
+                'date' => new \DateTime(),
+            ),
+            array(
+                'date' => new \DateTime(),
+            ),
+            array(
+                'date' => new \DateTime(),
+            )
+        );
+
+        $selectionFilters = array(
+            array(
+                'name' => 'Toutes',
+                'slug' => 'all'
+            ),
+            array(
+                'name' => 'Compétitions',
+                'slug' => 'competition'
+            ),
+            array(
+                'name' => 'Hors compétitions',
+                'slug' => 'hors-competition'
+            ),
+            array(
+                'name' => 'Un certain regard',
+                'slug' => 'certain-regard'
+            )
+        );
+
+        $typeFilters = array(
+            array(
+                'name' => 'Tout',
+                'slug' => 'all'
+            ),
+            array(
+                'name' => 'Séances',
+                'slug' => 'seances'
+            ),
+            array(
+                'name' => 'Evenements',
+                'slug' => 'evenements'
+            ),
+        );
+
+        return array(
+            'schedulingDays' => $schedulingDays,
+            'typeFilters' => $typeFilters,
+            'selectionFilters' => $selectionFilters,
+        );
+
+    }
+
+    /**
+     * @Route("/agenda")
+     * @Template("FDCEventBundle:Agenda:agenda.html.twig")
+     *
+     */
+    public function getAction()
+    {
+        $agenda = array();
+
+        return array(
+            'agenda' => $agenda
+        );
+
+    }
+}
