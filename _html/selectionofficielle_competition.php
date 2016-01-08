2f210a5d083c033b0664b88116942b476534ce22
Jacques tatiana <tatout08090@gmail.com>
2016-01-08 18:38:32 +0100

    fix selection offiiciel + single trailer

--- _html/selectionofficielle_competition.php
+++ _html/selectionofficielle_competition.php
@@ -192,7 +192,7 @@
       <div class="bandeau-list  bandeau-list-footer push-footer vCenter">
            <div class="bandeau-list-img vCenterKid" style="background-image:url(img/films/selection-officielle/push.jpg);">
                <h2 class="title title-list-header">Un certain regard</h2>
-               <a href="selectionofficielle_uncertainregard.php" class="bandeau-lien"> <img src="img/svg/arrow-right-gold.svg" alt="Découvrir la rubrique" class="svg-arrow"> Découvrir la rubrique</a>
+               <a href="selectionofficielle_uncertainregard.php" class="bandeau-lien"><i class="icon icon_fleche-right"></i> Découvrir la rubrique</a>
            </div>
       </div>
     </div>
