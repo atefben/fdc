ecbab96454a4cea1ee74fb06411153f4501cd09b
lrocher <lrocher@webqam.fr>
2016-01-11 14:47:54 +0100

    espace presse a telecharger template

--- src/FDC/PressBundle/Controller/MediaController.php
+++ src/FDC/PressBundle/Controller/MediaController.php
@@ -10,7 +10,7 @@ use FDC\PressBundle\Form\Type\LockedContentType;
 class MediaController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/media")
      * @Template("FDCPressBundle:Media:main.html.twig")
      * @return array
      */
@@ -1798,6 +1798,307 @@ class MediaController extends Controller
         return array(
             'headerInfo' => $headerInfo,
             'mediaSection' => $mediaSection,
+            'pressMovieDownloads' => $movieDownload,
+            'lockedForm' => $lockedContentForm->createView(),
+        );
+    }
+
+    /**
+     * @Route("/download")
+     * @Template("FDCPressBundle:Media:download.html.twig")
+     * @return array
+     */
+    public function downloadAction()
+    {
+        $headerInfo = array(
+            'title' => 'À télécharger',
+            'description' => 'Ces élements visuels sont à usage exclusif de la presse et des médias qui couvrent
+                              le Festival de Cannes. Aucune utilisation commerciale ou promotionnelle de ces visuels
+                              n’est autorisée.'
+        );
+        $translator = $this->get('translator');
+        $movieDownload = array(
+            'description' => 'Le Festival de Cannes met à disposition de la presse accréditée les bandes-annonces et
+                                  extraits de films fournis par les productions. Ces contenus sont mis à jour tout au
+                                  long du Festival. Ce service est fourni pour faciliter le traitement des films sur
+                                  vos propres médias. Nous vous prions instamment de ne pas les publier sur les réseaux
+                                  sociaux ou les portails de partage de type Youtube ou Dailymotion sans l’accord des
+                                  ayants-droits du film.<br><strong>Chaque fois que possible, les fichiers mis à disposition
+                                  seront en HD 1080p. Le Festival reste tributaire de la qualité du matériel qui lui est
+                                  adressé. 1 à 3 fichiers sont mis à disposition en fonction du fichier source reçu :</strong>',
+            'movie' => array(
+                'title' => 'Il racconto dei racconti',
+                'author' => array(
+                    'fullName' => 'Matteo GARRONE'
+                ),
+                'image' => array(
+                    'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/media/img6.jpg'
+                ),
+                'trailers' => array(
+                    array(
+                        'title' => 'Lorem ipsum',
+                        'description' => 'Lorem ipsum',
+                        'path' => '#'
+                    ),
+                    array(
+                        'title' => 'Lorem ipsum',
+                        'description' => 'Lorem ipsum',
+                        'path' => '#'
+                    ),
+                    array(
+                        'title' => 'Lorem ipsum',
+                        'description' => 'Lorem ipsum',
+                        'path' => '#'
+                    ),
+                    array(
+                        'title' => 'Lorem ipsum',
+                        'description' => 'Lorem ipsum',
+                        'path' => '#'
+                    )
+                )
+            ),
+        );
+        $lockedContentForm = $this->createForm( new LockedContentType($translator) );
+
+        $download = array(
+            'section' => array(
+                array(
+                    'title' => 'Affiche Officielle',
+                    'slug'  => 'affiche-officielle',
+                    'poster' => array(
+                        array(
+                            'image' => array(
+                                'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/press-portrait.jpg',
+                                'caption' => '© FDC / Lagency / Taste (Paris) / Ingrid Bergman © David Seymour / Estate of
+                                              David Seymour - Magnum Photos',
+                            ),
+                            'format' => array(
+                                'name' => 'Format Portrait',
+                                'slug' => 'portrait',
+                                'info' => array(
+                                    array(
+                                        'title' => 'JPG 72 DPI',
+                                        'link'  => '#'
+                                    ),
+                                    array(
+                                        'title' => 'JPG 300 DPI',
+                                        'link'  => '#'
+                                    )
+                                )
+                            )
+                        ),
+                        array(
+                            'image' => array(
+                                'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/press-landscape.jpg',
+                                'caption' => '© FDC / Lagency / Taste (Paris) / Ingrid Bergman © David Seymour / Estate of
+                                              David Seymour - Magnum Photos',
+                            ),
+                            'format' => array(
+                                'name' => 'Format Paysage',
+                                'slug' => 'landscape',
+                                'info' => array(
+                                    array(
+                                        'title' => 'JPG 72 DPI',
+                                        'link'  => '#'
+                                    ),
+                                    array(
+                                        'title' => 'JPG 300 DPI',
+                                        'link'  => '#'
+                                    )
+                                )
+                            )
+                        )
+                    )
+                ),
+                array(
+                    'title' => 'Signatures',
+                    'slug'  => 'signature',
+                    'signature' => array(
+                        array(
+                            'title' => 'Signature blanche',
+                            'image' => array(
+                                'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/img1.png',
+                                'caption' => '© FDC / Théophile Delange',
+                            ),
+                            'info' => array(
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                )
+                            ),
+                            'link' => '#'
+                        ),
+                        array(
+                            'title' => 'Signature bleu',
+                            'image' => array(
+                                'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/image2.png',
+                                'caption' => '© FDC / Théophile Delange',
+                            ),
+                            'info' => array(
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                )
+                            ),
+                            'link' => '#'
+                        ),
+                        array(
+                            'title' => 'Signature bleu #00427C',
+                            'image' => array(
+                                'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/img3.png',
+                                'caption' => '© FDC / Théophile Delange',
+                            ),
+                            'info' => array(
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                ),
+                                array(
+                                    'content' => 'Bannière carrée 400x400 px'
+                                )
+                            ),
+                            'link' => '#'
+
+                        ),
+                    )
+                ),
+                array(
+                    'title' => 'Animation',
+                    'slug'  => 'animation',
+                    'video' => array(
+                        'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/player.jpg',
+                        'caption' => '© FDC / Lagency / Taste (Paris) / Ingrid Bergman © David Seymour / Estate of
+                                      David Seymour - Magnum Photos<br>Film d\'animation avec la collaboration de Sonia
+                                      Tout Court sur un remix du thème musical du Festival, Le Carnaval des animaux de
+                                      Camille Saint-Saëns, avec un arrangement imaginé par deux musiciens suédois,
+                                      Patrik Andersson et Andreas Söderström.',
+                        'format' => array(
+                            array(
+                                'name' => '.MOV',
+                                'slug' => 'mov',
+                                'link' => '#',
+                            ),
+                            array(
+                                'name' => '.GIF',
+                                'slug' => 'gif',
+                                'link' => '#',
+                            ),
+                        )
+                    )
+                ),
+                array(
+                    'title' => 'Photos Institutionnelles',
+                    'slug' => 'photos-institutionnelles',
+                    'link' => '#',
+                    'photo' => array(
+                        array(
+                            'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/im-grid.jpg',
+                            'large' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/img-large.jpg',
+                            'thumb' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/media/img2.jpg',
+                            'title' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, minima.',
+                            'credit' => 'Crédit Image : VALERY HACHE / AFP',
+                        ),
+                        array(
+                            'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/im-grid.jpg',
+                            'large' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/img-large.jpg',
+                            'thumb' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/media/img2.jpg',
+                            'title' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, minima.',
+                            'credit' => 'Crédit Image : VALERY HACHE / AFP',
+                        ),
+                        array(
+                            'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/im-grid.jpg',
+                            'large' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/img-large.jpg',
+                            'thumb' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/media/img2.jpg',
+                            'title' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, minima.',
+                            'credit' => 'Crédit Image : VALERY HACHE / AFP',
+                        ),
+                        array(
+                            'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/im-grid.jpg',
+                            'large' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/img-large.jpg',
+                            'thumb' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/media/img2.jpg',
+                            'title' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, minima.',
+                            'credit' => 'Crédit Image : VALERY HACHE / AFP',
+                        ),
+                        array(
+                            'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/im-grid.jpg',
+                            'large' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/img-large.jpg',
+                            'thumb' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/media/img2.jpg',
+                            'title' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, minima.',
+                            'credit' => 'Crédit Image : VALERY HACHE / AFP',
+                        ),
+                        array(
+                            'path' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/im-grid.jpg',
+                            'large' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/downloading/img-large.jpg',
+                            'thumb' => '//html.festival-cannes-2016.com.ohwee.fr/img/press/media/img2.jpg',
+                            'title' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, minima.',
+                            'credit' => 'Crédit Image : VALERY HACHE / AFP',
+                        ),
+                    )
+                ),
+                array(
+                    'title' => 'Dossier de presse',
+                    'slug' => 'dossier-presse',
+                    'category' => array(
+                        array(
+                            'updatedAt' => new \DateTime(),
+                            'file' => array(
+                                array(
+                                'title' => 'Dossier de presse',
+                                'link' => '#',
+                                )
+                            )
+                        ),
+                        array(
+                            'updatedAt' => new \DateTime(),
+                            'file' => array(
+                                array(
+                                    'title' => 'Liste des attachés de presse',
+                                    'link' => '#',
+                                )
+                            )
+                        ),
+                        array(
+                            'updatedAt' => new \DateTime(),
+                            'file' => array(
+                                array(
+                                    'title' => 'Horaire des projections (presse)',
+                                    'link' => '#',
+                                ),
+                                array(
+                                    'title' => 'Horaire des projections (grand public)',
+                                    'link' => '#',
+                                ),
+                            )
+                        ),
+                    )
+                )
+            )
+        );
+
+        return array(
+            'headerInfo' => $headerInfo,
+            'pressDownloads' => $download,
             'pressMovieDownloads' => $movieDownload,
             'lockedForm' => $lockedContentForm->createView(),
         );
