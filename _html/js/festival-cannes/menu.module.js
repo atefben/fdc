ee53d0da0b489791e460869980720a8748777ae3
Clement Grellier <clementgrellier@gmail.com>
2016-01-22 16:10:09 +0100

    menu tablet #2

--- _html/js/festival-cannes/menu.module.js
+++ _html/js/festival-cannes/menu.module.js
@@ -19,16 +19,15 @@ $(document).ready(function() {
     });
   }
 
-  if($('body').hasClass('mob')) {
-    $('.main>li>a, .user>li>a').on('click', function(e) {
-      alert('testtablet');
+  $('.main>li>a, .user>li>a').on('click', function(e) {
+    if($('body').hasClass('mob')) {
       if($(this).parent().find('ul').length != 0) {
         e.preventDefault();
         $('#main, footer').addClass('overlay');
         $('.main>li').not($(this).parent()).addClass('fade');
         return false;
       }
-    });
-  }
+    }
+  });
 
 });
