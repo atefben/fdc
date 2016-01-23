c9e57215b50b34c4edebc320f280ae5e52bcff9b
benjamin giboudaud <benjamin.giboudaud@gmail.com>
2016-01-23 22:20:45 +0100

    add video player to videos.html

--- _html_mobile/js/videos.js
+++ _html_mobile/js/videos.js
@@ -1,29 +1,47 @@
-// var playerInstance = jwplayer("player1");
-//         playerInstance.setup({
-//         file: "/videos/sample.mp4",
-//         width: "100%",
-//         aspectratio: "16:9",
-//         displaytitle: false
-//         });
-
-
-$(document).ready(function() {
-	$('.list .item').on("click",function(){
-		console.log("coucou ", $(this).data("data-video"));
-		var playerInstance = jwplayer("player1");
+var playerInstance = jwplayer("player1");
         playerInstance.setup({
         file: "/videos/sample.mp4",
         width: "100%",
         aspectratio: "16:9",
-        displaytitle: false,
-        mediaid: '123456'
+        displaytitle: false
         });
-		playerInstance.getContainer().querySelector('video').setAttribute('id','videoPlayer');
-        var player = $('#player1').find('video');
-			player.addEventListener('webkitendfullscreen', function() {
-		   //desired "done button" behavior defined here
-		   console.log('coucou');
-		}, false);
+
+
+$(document).ready(function() {
+	$('.list .item').on("click",function(){
+		console.log('coucou',$(this).data('video'));
+		$('.fullscreenplayer').addClass('show');
+		setTimeout(function() {
+	      
+$('body').addClass('allow-landscape');
+	      
+	    }, 900);
+		
+		var playerInstance = jwplayer("player1");
+        playerInstance.setup({
+        file: $(this).data('video'),
+        width: "100%",
+        aspectratio: "16:9",
+        displaytitle: false,
+        mediaid: '123456',
+        skin: {
+		  name: "five"
+		 }
+        });
+		
 	});
+
+	$('body').on('click', '.close-button', function(e){
+    $('body').removeClass('allow-landscape');
+    //document.body.removeEventListener('touchmove', listener,false);
+    
+
+    setTimeout(function() {
+      $('.fullscreenplayer').removeClass('show');
+
+      
+    }, 900);
+  });
+
 	
 });
