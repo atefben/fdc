f8edb3a261368a279f8085679ebb7b6da35367f5
bengib <benjamin.giboudaud@gmail.com>
2016-01-26 15:48:49 +0100

    home page slideshow

--- _html_mobile/js/home.js
+++ _html_mobile/js/home.js
@@ -106,8 +106,8 @@ $(document).ready(function() {
 
 	  // init slideshow
 	  
-  var slideshows = [];
-  // init slideshow chocolat
+  	var slideshows = [];
+  	// init slideshow chocolat
     if($('.slideshow').length) {
 
       var slideshow = $('.slideshow .images').Chocolat({
@@ -130,59 +130,61 @@ $(document).ready(function() {
     }
 
 
-  // close slideshow on click
-  $('body').on('click', '.close-button', function(e){
-    $('body').removeClass('allow-landscape');
-    document.body.removeEventListener('touchmove', listener,false);
+	  // close slideshow on click
+	  $('body').off('click', '.close-button');
+	  $('body').on('click', '.close-button', function(e){
+	    $('body').removeClass('allow-landscape');
+	    document.body.removeEventListener('touchmove', listener,false);
 
-    setTimeout(function() {
-      $('.chocolat-wrapper').removeClass('show');
-    }, 900);
-  });
+	    setTimeout(function() {
+	      $('.chocolat-wrapper').removeClass('show');
+	    }, 900);
+	  });
 
- $('body').on('click', '.share', function(e){
-     e.preventDefault();
-      setTimeout(function() {
-        $('.buttons.square').toggleClass('show');
-      }, 200);
-     
- });
+	 $('body').on('click', '.share', function(e){
+	     e.preventDefault();
+	      setTimeout(function() {
+	        $('.buttons.square').toggleClass('show');
+	      }, 200);
+	     
+	 });
 
 
-  var listener = function (event) {
-    event.preventDefault();
-  };
+	  var listener = function (event) {
+	    event.preventDefault();
+	  };
 
-  // zoom
-  $('body').on('click', '.chocolat-image', function() {
-    var $that = $(this);
-    $('body').addClass('allow-landscape');
-    document.body.addEventListener('touchmove', listener,false);
-    
-    $('.chocolat-top').html('<div class="close-button"><i class="icon icon_close"></i></div>');
-    $('<a href="#" class="share"><i class="icon icon_share"></i></a>').insertAfter('.chocolat-wrapper .chocolat-pagination');
-    $('<div class="buttons square"><a href="#" class="button facebook"><i class="icon icon_facebook"></i></a><a href="#" class="button twitter"><i class="icon icon_twitter"></i></a><a href="#" class="button link"><i class="icon icon_link"></i></a><a href="#" class="button email"><i class="icon icon_lettre"></i></a></div>').appendTo('.chocolat-bottom');
+	  // zoom
+	  $('body').off('click', '.chocolat-image');
+	  $('body').on('click', '.chocolat-image', function() {
+	    var $that = $(this);
+	    $('body').addClass('allow-landscape');
+	    document.body.addEventListener('touchmove', listener,false);
+	    
+	    $('.chocolat-top').html('<div class="close-button"><i class="icon icon_close"></i></div>');
+	    $('<a href="#" class="share"><i class="icon icon_share"></i></a>').insertAfter('.chocolat-wrapper .chocolat-pagination');
+	    $('<div class="buttons square"><a href="#" class="button facebook"><i class="icon icon_facebook"></i></a><a href="#" class="button twitter"><i class="icon icon_twitter"></i></a><a href="#" class="button link"><i class="icon icon_link"></i></a><a href="#" class="button email"><i class="icon icon_lettre"></i></a></div>').appendTo('.chocolat-bottom');
 
-    window.location.hash = $that.attr('id');
+	    window.location.hash = $that.attr('id');
 
-    setTimeout(function() {
-      $('.chocolat-bottom').addClass('show');
-      $('.chocolat-wrapper').addClass('show');
-    }, 200);
+	    setTimeout(function() {
+	      $('.chocolat-bottom').addClass('show');
+	      $('.chocolat-wrapper').addClass('show');
+	    }, 200);
 
-    
+	    
 
-    // ADD SWIPE TO NAVIGATE THROUGH PHOTOS
-    var myElement = document.getElementById('chocolat-content-1');
-    var hammertime = new Hammer(myElement);
-    hammertime.on('swipeleft', function(ev) {
-        slideshow.api().next();
-    });
-    hammertime.on('swiperight', function(ev) {
-        slideshow.api().prev();
-    });
+	    // ADD SWIPE TO NAVIGATE THROUGH PHOTOS
+	    var myElement = document.getElementById('chocolat-content-1');
+	    var hammertime = new Hammer(myElement);
+	    hammertime.on('swipeleft', function(ev) {
+	        slideshow.api().next();
+	    });
+	    hammertime.on('swiperight', function(ev) {
+	        slideshow.api().prev();
+	    });
 
-  });
+	  });
 
   
 
