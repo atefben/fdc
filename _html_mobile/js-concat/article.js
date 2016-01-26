30f2352c4506689058097429dc97cbec8eb070a9
Louise OBÉ <louiseobe@free.fr>
2016-01-26 15:46:34 +0100

    fix chocolat android bug

--- _html_mobile/js-concat/article.js
+++ _html_mobile/js-concat/article.js
@@ -793,39 +793,13 @@ $(document).ready(function() {
 
   // close slideshow on click
   $('body').on('click', '.close-button', function(e){
-    $('body').removeClass('allow-landscape');
+    $('body').removeClass('allow-landscape chocolat-open chocolat-cover');
     document.body.removeEventListener('touchmove', listener,false);
-    // $('.chocolat-img').css('transition', 'all 0.9s ease').addClass('close');
 
-    // $('.chocolat-bottom').css('opacity', 0);
-    // $('.chocolat-close').css('opacity', 0);
 
     setTimeout(function() {
       $('.chocolat-wrapper').removeClass('show');
 
-      // $('body').removeClass('fixed');
-
-      // if($('.slideshow').length) {
-      //   $('html, body').animate({
-      //     scrollTop: $(window.location.hash).parents('.slideshow').offset().top - 300
-      //   }, 0);
-      // }
-
-      // setTimeout(function() {
-      //   for(var i=0; i<slideshows.length; i++) {
-
-      //       slideshows[i].api().close();
-      //       slideshows[i].api().destroy();
-      //       history.pushState("", document.title, window.location.pathname);
-
-      //   }
-      // }, 1000);
-
-      // setTimeout(function() {
-      //   slideshows = [];
-      //   $('.chocolat-wrapper').remove();
-      //   initSlideshows();
-      // }, 1400);
     }, 900);
   });
 
@@ -839,7 +813,7 @@ $(document).ready(function() {
 
 
   var listener = function (event) {
-    event.preventDefault();
+    // event.preventDefault();
   };
 
   // zoom
@@ -847,13 +821,10 @@ $(document).ready(function() {
     var $that = $(this);
     $('body').addClass('allow-landscape');
     document.body.addEventListener('touchmove', listener,false);
-    // $('.chocolat-left, .chocolat-right').appendTo('.chocolat-bottom');
-    // $('.chocolat-left').html('<i class="icon icon_flecheGauche"></i>');
-    // $('.chocolat-right').html('<i class="icon icon_fleche-right"></i>');
     $('.chocolat-top').html('<div class="close-button"><i class="icon icon_close"></i></div>');
     $('<a href="#" class="share"><i class="icon icon_share"></i></a>').insertAfter('.chocolat-wrapper .chocolat-pagination');
     $('<div class="buttons square"><a href="#" class="button facebook"><i class="icon icon_facebook"></i></a><a href="#" class="button twitter"><i class="icon icon_twitter"></i></a><a href="#" class="button link"><i class="icon icon_link"></i></a><a href="#" class="button email"><i class="icon icon_lettre"></i></a></div>').appendTo('.chocolat-bottom');
-    // $('<div class="credit">' + $that.data('credit') + '</div>').insertAfter('.chocolat-description');
+
 
     window.location.hash = $that.attr('id');
 
@@ -862,13 +833,6 @@ $(document).ready(function() {
       $('.chocolat-wrapper').addClass('show');
     }, 200);
 
-    // setTimeout(function() {
-    //   // $('.chocolat-wrapper .chocolat-img').css('transition', 'all 900ms cubic-bezier(0.165, 0.435, 0.000, 0.935)');
-    //   // change url hash
-    //   window.location.hash = $that.attr('id');
-
-
-    // }, 2500);
 
     // ADD SWIPE TO NAVIGATE THROUGH PHOTOS
     var myElement = document.getElementById('chocolat-content-1');
