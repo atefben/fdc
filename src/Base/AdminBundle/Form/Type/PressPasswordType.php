2c91a7874b1da071327e0b2c0e798b2df6dad70e
Louis <louis.rocher@gmail.com>
2016-01-27 01:16:25 +0100

    change pressPassword in BO

---
+++ src/Base/AdminBundle/Form/Type/PressPasswordType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Base\AdminBundle\Form\Type;
+
+use Doctrine\ORM\EntityRepository;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PressPasswordType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+            ->add('save', 'submit', array(
+                'label' => 'form.label_save'
+            ));
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FOS\UserBundle\Entity\User',
+            'translation_domain' => 'BaseAdminBundle'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'base_admin_press_password';
+    }
+}
