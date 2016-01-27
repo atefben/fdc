0b2f63e0701b407c12d0088825b2b69c9f97e032
benjamin giboudaud <benjamin.giboudaud@gmail.com>
2016-01-28 00:13:01 +0100

    filters on search page

--- _html_mobile/js-concat/searchpage.js
+++ _html_mobile/js-concat/searchpage.js
@@ -155,7 +155,7 @@ var GLOBALS = {
 };
 
 $(document).ready(function() {
- 
+  var dataForFilter = "";
     var menu = $("#horizontal-menu").owlCarousel({
 		  nav: false,
 		  dots: false,
@@ -350,6 +350,7 @@ if($('.searchpage').length) {
                             if($this.hasClass('artists') || $this.hasClass('events') || $this.hasClass('films') || $this.hasClass('participate')) {
                           
                         } else {
+                          dataForFilter = data;
                           var filters = $(".filters-slider").owlCarousel({
                                   nav: false,
                                   dots: false,
@@ -419,6 +420,7 @@ if($('.searchpage').length) {
                     if($this.hasClass('artists') || $this.hasClass('events') || $this.hasClass('films') || $this.hasClass('participate')) {
                       
                     } else {
+                      dataForFilter = data;
                       var filters = $(".filters-slider").owlCarousel({
                                   nav: false,
                                   dots: false,
@@ -455,6 +457,46 @@ if($('.searchpage').length) {
       
        
 }
+});
+$('body').off('click', '#filters span');
+$('body').on('click', '#filters span', function() {
+  console.log("test");
+  var id = $('#filters').data('id'),
+          i = $(this).index();
+
+      $('#' + id + ' .select span').removeClass('active');
+      $('#' + id + ' .select span').eq(i).addClass('active');
+
+
+
+      // $(".list .item:not(."+$(this).data('filter')+")").css('display','none');
+      // $(".list .item."+$(this).data('filter')).css('display','block');
+
+
+      $(".list .item").css("display","block");
+  $('#filteredContent').html(dataForFilter);
+  $('.filter').each(function() {
+    var $that = $(this);
+    var a = $that.find('.select span.active').data('filter');
+    console.log("test2",a);
+    if(a !== 'all'){
+      $(".list .item:not(."+a+")").remove();
+      console.log("test3");
+    }
+     
+  });
+  var sliderArticles = $(" #filteredContent .landscape-carousel").owlCarousel({ 
+      nav: false,
+      dots: false,
+      smartSpeed: 500,
+      fluidSpeed: 500,
+      loop: false,
+      margin: 20,
+      autoWidth: true,
+      dragEndSpeed: 600,
+      items:1
+    });
+   sliderArticles.owlCarousel();
 });
     // test : remove once on server
     if($('.searchpage #inputSearch').val() == 'Léonardo Di Caprio') { //TODO a revoir//
@@ -514,49 +556,23 @@ if($('.searchpage').length) {
 function filter() {
 
   // var filters = [];
+  
+    $('.filter').each(function() {
+      var $that = $(this);
+      var a = $that.find('.select span.active').data('filter');
 
-  $('.filter').each(function() {
-    var $that = $(this);
-    var a = $that.find('.select span.active').data('filter');
+      if(a !== 'all'){
+        $(".list .item:not(."+a+")").css('display','none');
+      }
+       
+    });
 
-    if(a !== 'all'){
-      $(".list .item:not(."+a+")").css('display','none');
+    if($('.grid').length !== 0){
+      $('.grid').isotope();
     }
-     
-  });
-
-  if($('.grid').length !== 0){
-    $('.grid').isotope();
-  }
-  if($('#filteredContent').length){
-    console.log('refreshed');
+    
       
-    var $owl = $(" #filteredContent .landscape-carousel").owlCarousel({ 
-                          nav: false,
-                          dots: false,
-                          smartSpeed: 500,
-                          fluidSpeed: 500,
-                          loop: false,
-                          margin: 20,
-                          autoWidth: true,
-                          dragEndSpeed: 600,
-                          items:1
-                        });
-    $owl.trigger('destroy.owl.carousel');
-    $owl.html($owl.find('.owl-stage-outer').html()).removeClass('owl-loaded');
-    $owl = $(" #filteredContent .landscape-carousel").owlCarousel({ 
-                          nav: false,
-                          dots: false,
-                          smartSpeed: 500,
-                          fluidSpeed: 500,
-                          loop: false,
-                          margin: 20,
-                          autoWidth: true,
-                          dragEndSpeed: 600,
-                          items:1
-                        });
-    $owl.owlCarousel();
-  }
+  
   
 
 }
