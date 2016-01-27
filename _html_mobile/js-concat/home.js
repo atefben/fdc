9b88a42d14210e7b6969b65ca5507bb9ea6644b3
bengib <benjamin.giboudaud@gmail.com>
2016-01-27 11:10:12 +0100

    search css

--- _html_mobile/js-concat/home.js
+++ _html_mobile/js-concat/home.js
@@ -1002,11 +1002,11 @@ function closeSearch() {
         $("#header").removeClass('fixfixed');
         openedKeyboard = false;
         $("#searchContainer").css({
-                    'top':''
-                  });
-                  $("#header").css({
-                    'top':''
-                  });
+            'top':''
+          });
+          $("#header").css({
+            'top':''
+          });
     });
 
   $('.suggestSearch').on('input', function(e) {
