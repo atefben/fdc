3d649186a4701830a01906d8a19783dc6b48fea0
bengib <benjamin.giboudaud@gmail.com>
2016-02-01 14:47:00 +0100

    add footer newsletter errors

--- _html_mobile/js-concat/movie.js
+++ _html_mobile/js-concat/movie.js
@@ -855,6 +855,53 @@ $(document).ready(function() {
 
 
 });
+// Newsletter
+// =========================
+
+$(document).ready(function() {
+
+  $('.newsletter #email').on('focus', function() {
+    if($(this).val() == GLOBALS.texts.newsletter.errorsNotValide || $(this).val() == GLOBALS.texts.newsletter.errorsMailEmpty) {
+      $(this).val('');
+      $(this).removeClass('error');
+    }
+  });
+
+  // on submit : check if there are errors in the form
+  $('.newsletter form').on('submit', function() {
+
+    var input = $('.newsletter #email');
+    var empty = false;
+
+    if($('#email').val() == '') {
+      empty = true;
+      input.addClass("error").val(GLOBALS.texts.newsletter.errorsMailEmpty);
+    } else {
+
+      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+      var is_email=re.test(input.val());
+      if(is_email){
+        input.removeClass("error");
+      }
+      else {
+        input.addClass("error").val(GLOBALS.texts.newsletter.errorsNotValide);
+      }
+    }
+
+    if($('.newsletter .error').length || empty) {
+      return false;
+    } else {
+      // ajax call newsletter
+
+      // show confirmation 
+      $('.newsletter form').addClass('hide');
+      $('#confirmation span').html($('#email').val());
+      $('#confirmation').addClass('show');
+
+      return false;
+    }
+  });
+});
 var GLOBALS = {
   "locale" : "fr",
   "defaultDate" : "2016-05-12",
@@ -898,7 +945,7 @@ var GLOBALS = {
     },
     "newsletter" : {
       "errorsNotValide" : "L'adresse e-mail n'est pas valide",
-      "errorsMailEmpty" : "Veuillez saisir une adresse e-mail valide"
+      "errorsMailEmpty" : "Veuillez saisir une adresse e-mail"
     },
     'agenda' : {
       'delete' : "Supprimer de votre agenda"
