dc65637470057afa926b304f81b76880edcb6bfc
lrocher <lrocher@webqam.fr>
2016-02-02 11:19:30 +0100

    press guide BO

--- src/Base/AdminBundle/Admin/PressGuideAdmin.php
+++ src/Base/AdminBundle/Admin/PressGuideAdmin.php
@@ -7,6 +7,10 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Base\CoreBundle\Entity\PressGuideTranslation;
+use Base\CoreBundle\Entity\PressGuide;
 
 class PressGuideAdmin extends Admin
 {
@@ -37,6 +41,30 @@ class PressGuideAdmin extends Admin
             ->add('id')
             ->add('createdAt')
             ->add('updatedAt')
+            ->add('status', 'doctrine_orm_callback', array(
+                'callback' => function($queryBuilder, $alias, $field, $value) {
+                    if (!$value['value']) {
+                        return;
+                    }
+                    $queryBuilder->join("{$alias}.translations", 't');
+                    $queryBuilder->where('t.locale = :locale');
+                    $queryBuilder->setParameter('locale', 'fr');
+                    $queryBuilder->andWhere('t.status = :status');
+                    $queryBuilder->setParameter('status', $value['value']);
+
+                    return true;
+                },
+                'field_type' => 'choice',
+                'field_options' => array(
+                    'choices' => PressGuideTranslation::getStatuses(),
+                    'choice_translation_domain' => 'BaseAdminBundle'
+                ),
+            ))
+            ->add('translate', new CheckboxType() , array(
+                'label'    => 'Traduction',
+                'required' => false,
+            ))
+
         ;
     }
 
@@ -49,6 +77,16 @@ class PressGuideAdmin extends Admin
             ->add('id')
             ->add('createdAt')
             ->add('updatedAt')
+            ->add('status', null, array('template' => 'BaseAdminBundle:News:list_status.html.twig'))
+            ->add('priorityStatus', 'choice', array(
+                'choices' => PressGuide::getPriorityStatusesList(),
+                'catalogue' => 'BaseAdminBundle'
+            ))
+            ->add('statusMain', 'choice', array(
+                'choices' => PressGuideTranslation::getStatuses(),
+                'catalogue' => 'BaseAdminBundle'
+            ))
+
         ;
     }
 
@@ -69,6 +107,42 @@ class PressGuideAdmin extends Admin
                     'updatedAt' => array(
                         'display' => false
                     ),
+                    'arriveTitle' => array(
+                        'label' => 'form.label_title',
+                        'translation_domain' => 'BaseAdminBundle',
+                        'locale_options' => array(
+                            'fr' => array(
+                                'required' => true
+                            )
+                        )
+                    ),
+                    'meetingTitle' => array(
+                        'label' => 'form.label_title',
+                        'translation_domain' => 'BaseAdminBundle',
+                        'locale_options' => array(
+                            'fr' => array(
+                                'required' => true
+                            )
+                        )
+                    ),
+                    'informationTitle' => array(
+                        'label' => 'form.label_title',
+                        'translation_domain' => 'BaseAdminBundle',
+                        'locale_options' => array(
+                            'fr' => array(
+                                'required' => true
+                            )
+                        )
+                    ),
+                    'serviceTitle' => array(
+                        'label' => 'form.label_title',
+                        'translation_domain' => 'BaseAdminBundle',
+                        'locale_options' => array(
+                            'fr' => array(
+                                'required' => true
+                            )
+                        )
+                    ),
                     'seoTitle' => array(
                         'attr' => array(
                             'placeholder' => 'form.placeholder_seo_title'
@@ -87,10 +161,23 @@ class PressGuideAdmin extends Admin
                         'translation_domain' => 'BaseAdminBundle',
                         'required' => false
 
-                    )
+                    ),
+                    'status' => array(
+                        'label' => 'form.label_status',
+                        'translation_domain' => 'BaseAdminBundle',
+                        'field_type' => 'choice',
+                        'choices' => PressGuideTranslation::getStatuses(),
+                        'choice_translation_domain' => 'BaseAdminBundle'
+                    ),
                 )
             ))
-            ->add('widgets', 'infinite_form_polycollection', array(
+            ->add('translate', 'checkbox' , array())
+
+            ->add('priorityStatus', 'choice', array(
+                'choices' => PressGuide::getPriorityStatuses(),
+                'choice_translation_domain' => 'BaseAdminBundle'
+            ))
+            ->add('arriveWidgets', 'infinite_form_polycollection', array(
                 'label' => false,
                 'types' => array(
                     'press_guide_widget_image_type',
@@ -101,6 +188,74 @@ class PressGuideAdmin extends Admin
                 'allow_delete' => true,
                 'prototype' => true,
                 'by_reference' => false,
+            ))
+            ->add('meetingWidgets', 'infinite_form_polycollection', array(
+                'label' => false,
+                'types' => array(
+                    'press_guide_widget_image_type',
+                    'press_guide_widget_column_type',
+                    'press_guide_widget_picto_type',
+                ),
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+                'by_reference' => false,
+            ))
+            ->add('serviceWidgets', 'infinite_form_polycollection', array(
+                'label' => false,
+                'types' => array(
+                    'press_guide_widget_image_type',
+                    'press_guide_widget_column_type',
+                    'press_guide_widget_picto_type',
+                ),
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+                'by_reference' => false,
+            ))
+            ->add('informationWidgets', 'infinite_form_polycollection', array(
+                'label' => false,
+                'types' => array(
+                    'press_guide_widget_image_type',
+                    'press_guide_widget_column_type',
+                    'press_guide_widget_picto_type',
+                ),
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+                'by_reference' => false,
+            ))
+            ->add('arriveIcon', new ChoiceType(), array(
+                'choices' => array(
+                    'icon-article' => 'Icone article',
+                    'icon-communique' => 'Icone communique',
+                    'icon-video' => 'Icone video',
+                ),
+                'choice_translation_domain' => 'BaseAdminBundle'
+            ))
+            ->add('serviceIcon', new ChoiceType(), array(
+                'choices' => array(
+                    'icon-article' => 'Icone article',
+                    'icon-communique' => 'Icone communique',
+                    'icon-video' => 'Icone video',
+                ),
+                'choice_translation_domain' => 'BaseAdminBundle'
+            ))
+            ->add('meetingIcon', new ChoiceType(), array(
+                'choices' => array(
+                    'icon-article' => 'Icone article',
+                    'icon-communique' => 'Icone communique',
+                    'icon-video' => 'Icone video',
+                ),
+                'choice_translation_domain' => 'BaseAdminBundle'
+            ))
+            ->add('informationIcon', new ChoiceType(), array(
+                'choices' => array(
+                    'icon-article' => 'Icone article',
+                    'icon-communique' => 'Icone communique',
+                    'icon-video' => 'Icone video',
+                ),
+                'choice_translation_domain' => 'BaseAdminBundle'
             ))
 
             ->end()
--- src/Base/CoreBundle/Entity/PressGuideTranslation.php
+++ src/Base/CoreBundle/Entity/PressGuideTranslation.php
@@ -6,6 +6,8 @@ use A2lix\I18nDoctrineBundle\Doctrine\ORM\Util\Translation;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Base\CoreBundle\Util\Seo;
+use Base\CoreBundle\Util\TranslateChild;
+use Base\CoreBundle\Interfaces\TranslateChildInterface;
 
 use Base\CoreBundle\Util\Time;
 /**
@@ -15,8 +17,9 @@ use Base\CoreBundle\Util\Time;
  * @ORM\Entity
  * @ORM\HasLifecycleCallbacks()
  */
-class PressGuideTranslation
+class PressGuideTranslation implements TranslateChildInterface
 {
+    use TranslateChild;
     use Time;
     use Translation;
     use Seo;
@@ -24,31 +27,121 @@ class PressGuideTranslation
     /**
      * @var string
      *
-     * @ORM\Column(name="procedure_title", type="string", length=255)
+     * @ORM\Column(name="guide_arrive_title", type="string", length=122)
      */
-    protected $procedureTitle;
+    protected $arriveTitle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="guide_meeting_title", type="string", length=122)
+     */
+    protected $meetingTitle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="guide_information_title", type="string", length=122)
+     */
+    protected $informationTitle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="guide_service_title", type="string", length=122)
+     */
+    protected $serviceTitle;
 
 
     /**
-     * Set procedureTitle
+     * Set arriveTitle
      *
-     * @param string $procedureTitle
+     * @param string $arriveTitle
      * @return PressGuideTranslation
      */
-    public function setProcedureTitle($procedureTitle)
+    public function setArriveTitle($arriveTitle)
     {
-        $this->procedureTitle = $procedureTitle;
+        $this->arriveTitle = $arriveTitle;
 
         return $this;
     }
 
     /**
-     * Get procedureTitle
+     * Get arriveTitle
      *
      * @return string 
      */
-    public function getProcedureTitle()
+    public function getArriveTitle()
     {
-        return $this->procedureTitle;
+        return $this->arriveTitle;
+    }
+
+    /**
+     * Set meetingTitle
+     *
+     * @param string $meetingTitle
+     * @return PressGuideTranslation
+     */
+    public function setMeetingTitle($meetingTitle)
+    {
+        $this->meetingTitle = $meetingTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get meetingTitle
+     *
+     * @return string 
+     */
+    public function getMeetingTitle()
+    {
+        return $this->meetingTitle;
+    }
+
+    /**
+     * Set informationTitle
+     *
+     * @param string $informationTitle
+     * @return PressGuideTranslation
+     */
+    public function setInformationTitle($informationTitle)
+    {
+        $this->informationTitle = $informationTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get informationTitle
+     *
+     * @return string 
+     */
+    public function getInformationTitle()
+    {
+        return $this->informationTitle;
+    }
+
+    /**
+     * Set serviceTitle
+     *
+     * @param string $serviceTitle
+     * @return PressGuideTranslation
+     */
+    public function setServiceTitle($serviceTitle)
+    {
+        $this->serviceTitle = $serviceTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get serviceTitle
+     *
+     * @return string 
+     */
+    public function getServiceTitle()
+    {
+        return $this->serviceTitle;
     }
 }
