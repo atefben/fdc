731e0e95cac04c1d76325cd4acdb177223ee3a2e
Louise OBÉ <louiseobe@free.fr>
2016-02-05 16:14:22 +0100

    #871

--- _html_mobile/js-concat/audios.js
+++ _html_mobile/js-concat/audios.js
@@ -470,7 +470,6 @@ var waves = [];
 var inter = null;
 $.initAudioPlayers = function(autoplay) {
 
-
     var duration = null;
 
   $('.audio-player').each(function(i) {
@@ -518,6 +517,7 @@ $.initAudioPlayers = function(autoplay) {
     waves.push(wave);
     // on click on play/pause
     $(this).find('.playpause').on('click', function(e) {
+      e.preventDefault();
 
     	if ($(this).find(".icon").hasClass('icon_audio')){
     		$(this).find(".icon").removeClass('icon_audio');
@@ -528,7 +528,7 @@ $.initAudioPlayers = function(autoplay) {
     	}
     	
 
-      e.preventDefault();
+      
 
       if(inter) {
         clearInterval(inter);
