a98ba4a82a7bbf723c808d9bbeda3cb518f9054e
lrocher <lrocher@webqam.fr>
2016-02-08 10:08:26 +0100

    contact page in front

--- src/FDC/PressBundle/Controller/NewsController.php
+++ src/FDC/PressBundle/Controller/NewsController.php
@@ -415,7 +415,6 @@ class NewsController extends Controller
 
                 $years[] = $statement->getPublishedAt()->format('Y');
             }
-            dump(!in_array($statement->getTheme()->getSlug(), $themes));
             if (!in_array($statement->getTheme()->getSlug(), $themes)) {
                 $filters['themes'][$i]['slug'] = $statement->getTheme()->getSlug();
                 $filters['themes'][$i]['content'] = $statement->getTheme()->getName();
