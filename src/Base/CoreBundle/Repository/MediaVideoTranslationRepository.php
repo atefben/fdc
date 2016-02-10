3b32f983192602eddf94a2b09578c42b9b3f8aba
Antoine Mineau <am2@ohwee.fr>
2016-02-10 14:53:24 +0100

    added property imageamazonurl to save the video preview

---
+++ src/Base/CoreBundle/Repository/MediaVideoTranslationRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Base\CoreBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+
+/**
+ * MediaVideoTranslationRepository class.
+ *
+ * \@extends EntityRepository
+ * @author   Antoine Mineau
+ * \@company Ohwee
+ */
+class MediaVideoTranslationRepository extends EntityRepository
+{
+}
