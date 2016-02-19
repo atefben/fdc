9b293206f31698265f5166a6a9b462f392cc9e1b
Pierre BELLENGER <p.bellenger@ohwee.fr>
2016-02-19 17:20:00 +0100

    filters film

--- src/Base/AdminBundle/Admin/FilmFilmAdmin.php
+++ src/Base/AdminBundle/Admin/FilmFilmAdmin.php
@@ -15,19 +15,66 @@ class FilmFilmAdmin extends SoifAdmin
      */
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
+        $em = $this->getConfigurationPool()->getContainer()->get('doctrine.orm.entity_manager');
+        $years = $em->getRepository('BaseCoreBundle:FilmFestival')->findAll();
+
+
         $datagridMapper
-            ->add('id')
-            ->add('directorFirst')
-            ->add('restored')
-            ->add('titleVO')
-            ->add('titleVOAlphabet')
-            ->add('productionYear')
-            ->add('duration')
-            ->add('castingCommentary')
-            ->add('website')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('soifUpdatedAt')
+            ->add('title', 'doctrine_orm_callback', array(
+                'callback' => function($queryBuilder, $alias, $field, $value) {
+                    if (!$value['value']) {
+                        return;
+                    }
+                    $queryBuilder->join("{$alias}.translations", 't');
+                    $queryBuilder->where('t.locale = :locale');
+                    $queryBuilder->setParameter('locale', 'fr');
+                    $queryBuilder->andWhere('t.title LIKE :title');
+                    $queryBuilder->setParameter('title', '%'. $value['value']. '%');
+
+                    return true;
+                },
+                'field_type' => 'text'
+            ))
+            ->add('realisator', 'doctrine_orm_callback', array(
+                'callback' => function($queryBuilder, $alias, $field, $value) {
+                    if (!$value['value']) {
+                        return;
+                    }
+                },
+                'field_type' => 'text'
+            ))
+            ->add('selection', 'doctrine_orm_callback', array(
+                'callback' => function($queryBuilder, $alias, $field, $value) {
+                    if (!$value['value']) {
+                        return;
+                    }
+                    $queryBuilder->join("{$alias}.selectionSection", 'ts');
+                    $queryBuilder->join("ts.translations", 'tst');
+                    $queryBuilder->where('tst.locale = :locale');
+                    $queryBuilder->setParameter('locale', 'fr');
+                    $queryBuilder->andWhere('tst.name LIKE :name');
+                    $queryBuilder->setParameter('name', '%'. $value['value']. '%');
+                    return true;
+                },
+                'field_type' => 'text'
+            ))
+            ->add('festival-year', 'doctrine_orm_callback', array(
+                'callback' => function($queryBuilder, $alias, $field, $value) {
+                    $em = $this->getConfigurationPool()->getContainer()->get('doctrine.orm.entity_manager');
+                    $years = $em->getRepository('BaseCoreBundle:FilmFestival')->findAll();
+                    if (!$value['value']) {
+                        return;
+                    }
+                    $queryBuilder->join("{$alias}.festival", 'fs');
+                    $queryBuilder->where('fs.year LIKE :year');
+                    $queryBuilder->setParameter('year', '%'. $years[$value['value']]. '%');
+                    return true;
+                },
+                'field_type' => 'choice',
+                'field_options' => array(
+                    'choices' => $years
+                )
+            ))
         ;
     }
 
