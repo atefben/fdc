6413c9ef36b7dac53c2db3cd46fdf62fa74f7d65
lrocher <lrocher@webqam.fr>
2016-03-02 11:38:56 +0100

    PressGuide Front

--- src/Base/CoreBundle/Entity/PressAccreditTranslation.php
+++ src/Base/CoreBundle/Entity/PressAccreditTranslation.php
@@ -28,23 +28,44 @@ class PressAccreditTranslation implements TranslateChildInterface
     /**
      * @var string
      *
-     * @ORM\Column(name="common_title", type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $commonTitle;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="common_content", type="text", nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      */
     protected $commonContent;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="procedure_main_title", type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $procedureMainTitle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $btnLabel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $btnLink;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $btnText;
 
 
     /**
@@ -114,5 +135,74 @@ class PressAccreditTranslation implements TranslateChildInterface
     public function getProcedureMainTitle()
     {
         return $this->procedureMainTitle;
+    }
+
+    /**
+     * Set btnLabel
+     *
+     * @param string $btnLabel
+     * @return PressAccreditTranslation
+     */
+    public function setBtnLabel($btnLabel)
+    {
+        $this->btnLabel = $btnLabel;
+
+        return $this;
+    }
+
+    /**
+     * Get btnLabel
+     *
+     * @return string 
+     */
+    public function getBtnLabel()
+    {
+        return $this->btnLabel;
+    }
+
+    /**
+     * Set btnLink
+     *
+     * @param string $btnLink
+     * @return PressAccreditTranslation
+     */
+    public function setBtnLink($btnLink)
+    {
+        $this->btnLink = $btnLink;
+
+        return $this;
+    }
+
+    /**
+     * Get btnLink
+     *
+     * @return string 
+     */
+    public function getBtnLink()
+    {
+        return $this->btnLink;
+    }
+
+    /**
+     * Set btnText
+     *
+     * @param string $btnText
+     * @return PressAccreditTranslation
+     */
+    public function setBtnText($btnText)
+    {
+        $this->btnText = $btnText;
+
+        return $this;
+    }
+
+    /**
+     * Get btnText
+     *
+     * @return string 
+     */
+    public function getBtnText()
+    {
+        return $this->btnText;
     }
 }
--- src/FDC/PressBundle/Controller/AccreditController.php
+++ src/FDC/PressBundle/Controller/AccreditController.php
@@ -32,10 +32,7 @@ class AccreditController extends Controller
         }
 
         // GET ACCREDIT PAGE
-        $accredit = $em->getRepository('BaseCoreBundle:PressAccredit')->findOneBy(array(
-            'festival' => $settings->getFestival()->getId()
-        ));
-
+        $accredit = $em->getRepository('BaseCoreBundle:PressAccredit')->findOneById($this->getParameter('admin_press_accredit_id'));
         if ($accredit === null) {
             throw new NotFoundHttpException();
         }
@@ -48,140 +45,9 @@ class AccreditController extends Controller
         );
 
 
-        $sectionContent = array(
-            'section' => array(
-                array(
-                    'title' => 'Presse écrite et digitale',
-                    'slug' => 'presse',
-                    'content' => '<h4>Agence de presse</h4> <p>Merci d’adresser au service de presse les éléments nécessaires à
-                         votre demande d\'accréditation :</p><ul> <li> <strong>- pour une première demande
-                         d’accréditation :</strong> <p>Une lettre officielle à l\'en-tête de votre média, signée du
-                         rédacteur en chef ou du directeur, vous mandatant pour couvrir le Festival. Merci de nous
-                         donner des informations relatives à l\'importance de l\'agence que vous représentez
-                         (zone géographique de diffusion, listes des médias abonnés…) ainsi que la couverture que le
-                         média compte faire du Festival. Si votre média souhaite mandater plusieurs journalistes, merci
-                         de nous préciser leurs noms et fonctions. Si votre agence de presse fournit du contenu à des
-                         médias web, merci de nous préciser si vous mandatez une équipe technique (vidéastes, techniciens).
-                         Pour un photographe, veuillez vous référer à la rubrique « Agence photo ». - vos coordonnées
-                         (ligne directe, portable, adresse e-mail) - la photocopie de votre carte professionnelle
-                         (si le journaliste n’en possède pas, merci de l’indiquer sur la lettre officielle) - 3 dépêches
-                         et/ou reportages récents signés de vous ou de l\'agence</p></li><li> <strong>- si vous avez été
-                         accrédité(e) l’an dernier :</strong> <p>Toutes les dépêches et/ou reportages, signés de vous ou
-                         de l\'agence, consacrés au Festival de l\'année précédente ainsi que vos éventuelles
-                         interventions sur d’autres médias (presse écrite, web, radio, TV). </p></li></ul> <h5>Envoi de
-                         votre couverture presse :</h5> <p> Les dépêches et/ou reportages au format PDF et/ou tout autre
-                         fichier numérique peuvent nous être envoyés :<br>- par e-mail à l\'adresse
-                         <a href="mailto:mediareport@festival-cannes.fr">mediareport@festival-cannes.fr</a><br>-
-                         par serveur FTP<br>- par courrier (envoi de CD, DVD, clef USB)<br>Les liens Internet adressés
-                         par email ne seront pas pris en compte.<br></p>',
-                ),
-                array(
-                    'title' => 'Presse écrite et digitale',
-                    'slug' => 'test',
-                    'content' => '<h4>Agence de presse</h4> <p>Merci d’adresser au service de presse les éléments nécessaires à
-                         votre demande d\'accréditation :</p><ul> <li> <strong>- pour une première demande
-                         d’accréditation :</strong> <p>Une lettre officielle à l\'en-tête de votre média, signée du
-                         rédacteur en chef ou du directeur, vous mandatant pour couvrir le Festival. Merci de nous
-                         donner des informations relatives à l\'importance de l\'agence que vous représentez
-                         (zone géographique de diffusion, listes des médias abonnés…) ainsi que la couverture que le
-                         média compte faire du Festival. Si votre média souhaite mandater plusieurs journalistes, merci
-                         de nous préciser leurs noms et fonctions. Si votre agence de presse fournit du contenu à des
-                         médias web, merci de nous préciser si vous mandatez une équipe technique (vidéastes, techniciens).
-                         Pour un photographe, veuillez vous référer à la rubrique « Agence photo ». - vos coordonnées
-                         (ligne directe, portable, adresse e-mail) - la photocopie de votre carte professionnelle
-                         (si le journaliste n’en possède pas, merci de l’indiquer sur la lettre officielle) - 3 dépêches
-                         et/ou reportages récents signés de vous ou de l\'agence</p></li><li> <strong>- si vous avez été
-                         accrédité(e) l’an dernier :</strong> <p>Toutes les dépêches et/ou reportages, signés de vous ou
-                         de l\'agence, consacrés au Festival de l\'année précédente ainsi que vos éventuelles
-                         interventions sur d’autres médias (presse écrite, web, radio, TV). </p></li></ul> <h5>Envoi de
-                         votre couverture presse :</h5> <p> Les dépêches et/ou reportages au format PDF et/ou tout autre
-                         fichier numérique peuvent nous être envoyés :<br>- par e-mail à l\'adresse
-                         <a href="mailto:mediareport@festival-cannes.fr">mediareport@festival-cannes.fr</a><br>-
-                         par serveur FTP<br>- par courrier (envoi de CD, DVD, clef USB)<br>Les liens Internet adressés
-                         par email ne seront pas pris en compte.<br></p>',
-                ),
-                array(
-                    'title' => 'Presse écrite et digitale',
-                    'slug' => 'testt',
-                    'content' => '<h4>Agence de presse</h4> <p>Merci d’adresser au service de presse les éléments nécessaires à
-                         votre demande d\'accréditation :</p><ul> <li> <strong>- pour une première demande
-                         d’accréditation :</strong> <p>Une lettre officielle à l\'en-tête de votre média, signée du
-                         rédacteur en chef ou du directeur, vous mandatant pour couvrir le Festival. Merci de nous
-                         donner des informations relatives à l\'importance de l\'agence que vous représentez
-                         (zone géographique de diffusion, listes des médias abonnés…) ainsi que la couverture que le
-                         média compte faire du Festival. Si votre média souhaite mandater plusieurs journalistes, merci
-                         de nous préciser leurs noms et fonctions. Si votre agence de presse fournit du contenu à des
-                         médias web, merci de nous préciser si vous mandatez une équipe technique (vidéastes, techniciens).
-                         Pour un photographe, veuillez vous référer à la rubrique « Agence photo ». - vos coordonnées
-                         (ligne directe, portable, adresse e-mail) - la photocopie de votre carte professionnelle
-                         (si le journaliste n’en possède pas, merci de l’indiquer sur la lettre officielle) - 3 dépêches
-                         et/ou reportages récents signés de vous ou de l\'agence</p></li><li> <strong>- si vous avez été
-                         accrédité(e) l’an dernier :</strong> <p>Toutes les dépêches et/ou reportages, signés de vous ou
-                         de l\'agence, consacrés au Festival de l\'année précédente ainsi que vos éventuelles
-                         interventions sur d’autres médias (presse écrite, web, radio, TV). </p></li></ul> <h5>Envoi de
-                         votre couverture presse :</h5> <p> Les dépêches et/ou reportages au format PDF et/ou tout autre
-                         fichier numérique peuvent nous être envoyés :<br>- par e-mail à l\'adresse
-                         <a href="mailto:mediareport@festival-cannes.fr">mediareport@festival-cannes.fr</a><br>-
-                         par serveur FTP<br>- par courrier (envoi de CD, DVD, clef USB)<br>Les liens Internet adressés
-                         par email ne seront pas pris en compte.<br></p>',
-                ),
-                array(
-                    'title' => 'Presse écrite et digitale',
-                    'slug' => 'testtt',
-                    'content' => '<h4>Agence de presse</h4> <p>Merci d’adresser au service de presse les éléments nécessaires à
-                         votre demande d\'accréditation :</p><ul> <li> <strong>- pour une première demande
-                         d’accréditation :</strong> <p>Une lettre officielle à l\'en-tête de votre média, signée du
-                         rédacteur en chef ou du directeur, vous mandatant pour couvrir le Festival. Merci de nous
-                         donner des informations relatives à l\'importance de l\'agence que vous représentez
-                         (zone géographique de diffusion, listes des médias abonnés…) ainsi que la couverture que le
-                         média compte faire du Festival. Si votre média souhaite mandater plusieurs journalistes, merci
-                         de nous préciser leurs noms et fonctions. Si votre agence de presse fournit du contenu à des
-                         médias web, merci de nous préciser si vous mandatez une équipe technique (vidéastes, techniciens).
-                         Pour un photographe, veuillez vous référer à la rubrique « Agence photo ». - vos coordonnées
-                         (ligne directe, portable, adresse e-mail) - la photocopie de votre carte professionnelle
-                         (si le journaliste n’en possède pas, merci de l’indiquer sur la lettre officielle) - 3 dépêches
-                         et/ou reportages récents signés de vous ou de l\'agence</p></li><li> <strong>- si vous avez été
-                         accrédité(e) l’an dernier :</strong> <p>Toutes les dépêches et/ou reportages, signés de vous ou
-                         de l\'agence, consacrés au Festival de l\'année précédente ainsi que vos éventuelles
-                         interventions sur d’autres médias (presse écrite, web, radio, TV). </p></li></ul> <h5>Envoi de
-                         votre couverture presse :</h5> <p> Les dépêches et/ou reportages au format PDF et/ou tout autre
-                         fichier numérique peuvent nous être envoyés :<br>- par e-mail à l\'adresse
-                         <a href="mailto:mediareport@festival-cannes.fr">mediareport@festival-cannes.fr</a><br>-
-                         par serveur FTP<br>- par courrier (envoi de CD, DVD, clef USB)<br>Les liens Internet adressés
-                         par email ne seront pas pris en compte.<br></p>',
-                ),
-                array(
-                    'title' => 'Presse écrite et digitale',
-                    'slug' => 'testttt',
-                    'content' => '<h4>Agence de presse</h4> <p>Merci d’adresser au service de presse les éléments nécessaires à
-                         votre demande d\'accréditation :</p><ul> <li> <strong>- pour une première demande
-                         d’accréditation :</strong> <p>Une lettre officielle à l\'en-tête de votre média, signée du
-                         rédacteur en chef ou du directeur, vous mandatant pour couvrir le Festival. Merci de nous
-                         donner des informations relatives à l\'importance de l\'agence que vous représentez
-                         (zone géographique de diffusion, listes des médias abonnés…) ainsi que la couverture que le
-                         média compte faire du Festival. Si votre média souhaite mandater plusieurs journalistes, merci
-                         de nous préciser leurs noms et fonctions. Si votre agence de presse fournit du contenu à des
-                         médias web, merci de nous préciser si vous mandatez une équipe technique (vidéastes, techniciens).
-                         Pour un photographe, veuillez vous référer à la rubrique « Agence photo ». - vos coordonnées
-                         (ligne directe, portable, adresse e-mail) - la photocopie de votre carte professionnelle
-                         (si le journaliste n’en possède pas, merci de l’indiquer sur la lettre officielle) - 3 dépêches
-                         et/ou reportages récents signés de vous ou de l\'agence</p></li><li> <strong>- si vous avez été
-                         accrédité(e) l’an dernier :</strong> <p>Toutes les dépêches et/ou reportages, signés de vous ou
-                         de l\'agence, consacrés au Festival de l\'année précédente ainsi que vos éventuelles
-                         interventions sur d’autres médias (presse écrite, web, radio, TV). </p></li></ul> <h5>Envoi de
-                         votre couverture presse :</h5> <p> Les dépêches et/ou reportages au format PDF et/ou tout autre
-                         fichier numérique peuvent nous être envoyés :<br>- par e-mail à l\'adresse
-                         <a href="mailto:mediareport@festival-cannes.fr">mediareport@festival-cannes.fr</a><br>-
-                         par serveur FTP<br>- par courrier (envoi de CD, DVD, clef USB)<br>Les liens Internet adressés
-                         par email ne seront pas pris en compte.<br></p>',
-                )
-            )
-        );
-
         return array(
             'headerInfo' => $headerInfo,
             'accredit' => $accredit,
-            'sectionContent' => $sectionContent
         );
 
     }
