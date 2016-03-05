220cf02f2ca2e635640d4db4b615d0f3b62a950a
Louis <louis.rocher@gmail.com>
2016-03-05 12:01:08 +0100

    pressdownloadsection list lock

--- src/Base/CoreBundle/Entity/PressDownloadSection.php
+++ src/Base/CoreBundle/Entity/PressDownloadSection.php
@@ -58,7 +58,7 @@ class PressDownloadSection implements TranslateMainInterface
 
     public function __toString()
     {
-        return (string) $this->getCurrentTranslation()->getTitle();
+        return (string) $this->findTranslationByLocale('fr')->getTitle();
     }
 
     /**
