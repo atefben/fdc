63b34eb45e959fac4fb48be2e2497bdf0b3e57ce
Louis <louis.rocher@gmail.com>
2016-03-07 00:12:05 +0100

    fix pressdownload section remove

--- src/Base/AdminBundle/Admin/PressDownloadAdmin.php
+++ src/Base/AdminBundle/Admin/PressDownloadAdmin.php
@@ -99,14 +99,7 @@ class PressDownloadAdmin extends Admin
             ->add('downloadSection', 'sonata_type_collection',
                 array(
                     'type_options' => array(
-                        'delete' => false,
-                        'delete_options' => array(
-                            'type'         => 'hidden',
-                            'type_options' => array(
-                                'mapped'   => false,
-                                'required' => false,
-                            )
-                        )
+                        'delete' => true,
                     ),
                     'cascade_validation' => true,
                     'by_reference' => false,
