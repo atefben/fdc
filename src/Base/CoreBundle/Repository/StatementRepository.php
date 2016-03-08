eeee4f837594ebd7b65545f4f47fdd7c638c4c5a
Louis <louis.rocher@gmail.com>
2016-03-08 13:59:18 +0100

    fix #1441 - Statement & Info

--- src/Base/CoreBundle/Repository/StatementRepository.php
+++ src/Base/CoreBundle/Repository/StatementRepository.php
@@ -431,4 +431,59 @@ class StatementRepository extends EntityRepository
 
         return $qb;
     }
+
+    public function getAllStatement($locale, $festival)
+    {
+        $qb = $this
+            ->createQueryBuilder('n')
+            ->select('n')
+            ->join('n.sites', 's')
+            ->leftjoin('Base\CoreBundle\Entity\StatementArticle', 'na1', 'WITH', 'na1.id = n.id')
+            ->leftjoin('Base\CoreBundle\Entity\StatementAudio', 'na2', 'WITH', 'na2.id = n.id')
+            ->leftjoin('Base\CoreBundle\Entity\StatementImage', 'na3', 'WITH', 'na3.id = n.id')
+            ->leftjoin('Base\CoreBundle\Entity\StatementVideo', 'na4', 'WITH', 'na4.id = n.id')
+            ->leftjoin('na1.translations', 'na1t')
+            ->leftjoin('na2.translations', 'na2t')
+            ->leftjoin('na3.translations', 'na3t')
+            ->leftjoin('na4.translations', 'na4t')
+            ->where('s.slug = :site_slug')
+            ->andWhere('n.festival = :festival');
+
+        $qb = $qb
+            ->andWhere(
+                '(na1t.locale = :locale_fr AND na1t.status = :status) OR
+                    (na2t.locale = :locale_fr AND na2t.status = :status) OR
+                    (na3t.locale = :locale_fr AND na3t.status = :status) OR
+                    (na4t.locale = :locale_fr AND na4t.status = :status)'
+            )
+            ->setParameter('locale_fr', 'fr')
+            ->setParameter('status', StatementArticleTranslation::STATUS_PUBLISHED);
+
+        if ($locale != 'fr') {
+            $qb = $qb
+                ->leftjoin('na1.translations', 'na5t')
+                ->leftjoin('na2.translations', 'na6t')
+                ->leftjoin('na3.translations', 'na7t')
+                ->leftjoin('na4.translations', 'na8t')
+                ->andWhere(
+                    '(na5t.locale = :locale AND na5t.status = :status_translated) OR
+                    (na6t.locale = :locale AND na6t.status = :status_translated) OR
+                    (na7t.locale = :locale AND na7t.status = :status_translated) OR
+                    (na8t.locale = :locale AND na8t.status = :status_translated)'
+                )
+                ->setParameter('status_translated', StatementArticleTranslation::STATUS_TRANSLATED)
+                ->setParameter('locale', $locale);
+        }
+
+        $qb = $qb
+            ->orderBy('n.publishedAt', 'DESC')
+            ->setParameter('festival', $festival)
+            ->setParameter('site_slug', 'site-press');
+
+        $qb = $qb
+            ->getQuery()
+            ->getResult();
+
+        return $qb;
+    }
 }
