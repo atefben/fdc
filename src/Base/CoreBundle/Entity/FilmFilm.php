bb0a0b13996ce6cdcdb13f82f833ffaae35d8bc8
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-03-09 10:07:10 +0100

    test

--- src/Base/CoreBundle/Entity/FilmFilm.php
+++ src/Base/CoreBundle/Entity/FilmFilm.php
@@ -173,7 +173,6 @@ class FilmFilm implements FilmFilmInterface, TranslateMainInterface
      * @ORM\Column(type="integer", nullable=true)
      *
      * @Groups({
-     *  "film_list", "film_show",
      *  "trailer_list", "trailer_show",
      *  "award_list", "award_show",
      *  "projection_list", "projection_show"
@@ -187,7 +186,6 @@ class FilmFilm implements FilmFilmInterface, TranslateMainInterface
      * @ORM\Column(type="string", length=255, nullable=true)
      *
      * @Groups({
-     *  "film_list", "film_show",
      *  "trailer_list", "trailer_show",
      *  "award_list", "award_show",
      *  "projection_list", "projection_show"
