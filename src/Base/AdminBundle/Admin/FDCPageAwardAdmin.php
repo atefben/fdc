5865fb44647091427b32ce7a186382e18fb73766
Jean-Luc Petit <webpetit@gmail.com>
2016-03-15 11:38:59 +0100

    Add missing files.

---
+++ src/Base/AdminBundle/Admin/FDCPageAwardAdmin.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Base\AdminBundle\Admin;
+
+use Base\CoreBundle\Entity\FDCPageAward;
+use Base\CoreBundle\Entity\FDCPageAwardTranslation;
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class FDCPageAwardAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('translations', 'a2lix_translations', array(
+                'label'              => false,
+                'translation_domain' => 'BaseAdminBundle',
+                'required_locales'   => array('fr'),
+                'fields'             => array(
+                    'status'         => array(
+                        'label'                     => 'form.label_status',
+                        'translation_domain'        => 'BaseAdminBundle',
+                        'field_type'                => 'choice',
+                        'choices'                   => FDCPageAwardTranslation::getStatuses(),
+                        'choice_translation_domain' => 'BaseAdminBundle'
+                    ),
+                    'name'       => array(
+                        'label'              => 'form.fdc_page_award.label_override_name',
+                        'translation_domain' => 'BaseAdminBundle',
+                        'required'           => true,
+                    ),
+                    'seoTitle'       => array(
+                        'attr'               => array(
+                            'placeholder' => 'form.fdc_page_award.placeholder_seo_title'
+                        ),
+                        'label'              => 'form.label_seo_title',
+                        'sonata_help'        => 'form.news.helper_seo_title',
+                        'translation_domain' => 'BaseAdminBundle',
+                        'required'           => false,
+                    ),
+                    'seoDescription' => array(
+                        'attr'               => array(
+                            'placeholder' => 'form.fdc_page_award.placeholder_seo_description'
+                        ),
+                        'label'              => 'form.label_seo_description',
+                        'sonata_help'        => 'form.news.helper_description',
+                        'translation_domain' => 'BaseAdminBundle',
+                        'required'           => false,
+                    ),
+                    'createdAt'      => array(
+                        'display' => false
+                    ),
+                    'updatedAt'      => array(
+                        'display' => false
+                    ),
+                )
+            ))
+            ->add('image', 'sonata_type_model_list', array(
+                'label'    => 'form.fdc_page_award.image',
+                'help'     => 'form.fdc_page_award.helper_image',
+                'required' => false,
+            ))
+            ->add('juryType', 'sonata_type_model_list', array(
+                'label'    => 'form.fdc_page_award.label_jury_type',
+                'required' => false,
+                'btn_add' => false,
+            ))
+            ->add('seoFile', 'sonata_media_type', array(
+                'provider' => 'sonata.media.provider.image',
+                'context'  => 'seo_file',
+                'help'     => 'form.seo.helper_file',
+                'required' => false
+            ))
+            ->add('translate')
+            ->add('translateOptions', 'choice', array(
+                'choices' => FDCPageAward::getAvailableTranslateOptions(),
+                'translation_domain' => 'BaseAdminBundle',
+                'multiple' => true,
+                'expanded' => true
+            ))
+            ->add('priorityStatus', 'choice', array(
+                'choices'                   => FDCPageAward::getPriorityStatuses(),
+                'choice_translation_domain' => 'BaseAdminBundle',
+            ))
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+        ;
+    }
+}
