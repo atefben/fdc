228d24303523a5323a30f9495cd009596f88ca6b
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-03-25 19:13:02 +0100

    test check

--- src/Base/CoreBundle/Manager/AmazonRemoteFileManager.php
+++ src/Base/CoreBundle/Manager/AmazonRemoteFileManager.php
@@ -31,19 +31,23 @@ class AmazonRemoteFileManager
     {
         $i = 0;
         foreach ($files as $key => $file) {
-            $amazonRemoteFile = new AmazonRemoteFile();
-            $amazonRemoteFile
-                ->setId($file['id'])
-                ->setName($file['name'])
-                ->setUrl($file['url'])
-            ;
-            ++$i;
-            $this->getDoctrineManager()->persist($amazonRemoteFile);
-            if ($i === 99) {
-                $i = 0;
-                $this->getDoctrineManager()->flush();
-                $this->getDoctrineManager()->clear();
-            }
+			$entity = $em->getRepository('AmazonRemoteFile')->findOneBy(array('id' => $file['id']));
+			if ($entity == null)
+			{
+	            $amazonRemoteFile = new AmazonRemoteFile();
+	            $amazonRemoteFile
+	                ->setId($file['id'])
+	                ->setName($file['name'])
+	                ->setUrl($file['url'])
+	            ;
+	            ++$i;
+	            $this->getDoctrineManager()->persist($amazonRemoteFile);
+	            if ($i === 99) {
+	                $i = 0;
+	                $this->getDoctrineManager()->flush();
+	                $this->getDoctrineManager()->clear();
+	            }
+			}
         }
         if ($i) {
             $this->getDoctrineManager()->flush();
