99ce918ce89414cb034480b21e54b4f73aa36f46
Pierre BELLENGER <p.bellenger@ohwee.fr>
2016-03-25 11:18:33 +0100

    bo #522

---
+++ src/Base/AdminBundle/Admin/FDCCannesClassicsWidgetImageDualAlignDummyAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Base\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class FDCCannesClassicsWidgetImageDualAlignDummyAdmin extends Admin
+{
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('gallery', 'sonata_type_model_list',array(
+                'btn_delete' => true
+            ))
+        ;
+    }
+}
---
+++ src/Base/CoreBundle/Entity/FDCCannesClassicsWidgetQuote.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Base\CoreBundle\Entity;
+
+use A2lix\I18nDoctrineBundle\Doctrine\ORM\Util\Translatable;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+use JMS\Serializer\Annotation\Groups;
+use JMS\Serializer\Annotation\Since;
+
+/**
+ * FDCCannesClassicsWidgetQuote
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ */
+class FDCCannesClassicsWidgetQuote extends FDCCannesClassicsWidget
+{
+    use Translatable;
+
+    /**
+     * @var ArrayCollection
+     **/
+    protected $translations;
+
+    public function __construct()
+    {
+        $this->translations = new ArrayCollection();
+    }
+}
