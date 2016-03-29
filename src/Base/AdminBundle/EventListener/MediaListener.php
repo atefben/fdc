217e203eb615778664094c694141fcf643a783cf
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-03-29 19:08:23 +0200

    test

--- src/Base/AdminBundle/EventListener/MediaListener.php
+++ src/Base/AdminBundle/EventListener/MediaListener.php
@@ -212,13 +212,7 @@ class MediaListener
         $path_video_input = $file_path['0'] . '/';
         $path_video_output = 'media_video_encoded' . '/direct_encoded/';
         //$mediaVideo->getAmazonRemoteFile()->getId();
-        $s3 = S3Client::factory(array(
-            'credentials' => array(
-                'key'    => 'AKIAJHXD67GEPPA2F4TQ',
-                'secret' => '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',
-            ),
-            'region'      => 'eu-west-1',
-        ));
+        $s3 = \Aws\S3\S3Client::factory(array('secret' => '8TtlhHgQEIPwQBQiDqCzG7h5Eq856H2jst1PtER6', 'key' => 'AKIAJHXD67GEPPA2F4TQ'));
 		$info = $s3->getObjectInfo('use', $path_video_output .  str_replace('.mov', '.mp4', $file_name));
 		if ($info)
 		{
