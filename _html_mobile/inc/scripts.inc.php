3e5d355e5cf133eca00f03e86a7d46348671a9ca
Johann Pinson <johann.pinson@gmail.com>
2016-03-30 16:50:35 +0200

    update Gruntfile-prod

---
+++ _html_mobile/inc/scripts.inc.php
@@ -0,0 +1,56 @@
+<!-- build:js js/vendor.min.js -->
+<!-- fdc-vendor tags start -->
+<script src="../js/vendors/classie.js"></script>
+<script src="../js/vendors/codebird.js"></script>
+<script src="../js/vendors/hammer.min.js"></script>
+<script src="../js/vendors/isotope.pkgd.min.js"></script>
+<script src="../js/vendors/jquery-1.12.0.min.js"></script>
+<script src="../js/vendors/jquery-ui.custom.min.js"></script>
+<script src="../js/vendors/jquery.chocolat.js"></script>
+<script src="../js/vendors/jquery.cookie.js"></script>
+<script src="../js/vendors/jwplayer.js"></script>
+<script src="../js/vendors/modernizr.custom.js"></script>
+<script src="../js/vendors/moment.min.js"></script>
+<script src="../js/vendors/owl.carousel.min.js"></script>
+<script src="../js/vendors/packery-mode.pkgd.min.js"></script>
+<script src="../js/vendors/wavesurfer.min.js"></script>
+<!-- fdc-vendor tags end -->
+<!-- endbuild -->
+
+<!-- build:js js/app.min.js -->
+<!-- fdc-app tags start -->
+<script src="../js/accordeon.js"></script>
+<script src="../js/add-to-selection.js"></script>
+<script src="../js/article.js"></script>
+<script src="../js/artist.js"></script>
+<script src="../js/audioplayer.js"></script>
+<script src="../js/communiques.js"></script>
+<script src="../js/contact.js"></script>
+<script src="../js/filters.js"></script>
+<script src="../js/fullscreenplayer.js"></script>
+<script src="../js/googlemap.js"></script>
+<script src="../js/grid-isotope.js"></script>
+<script src="../js/home.js"></script>
+<script src="../js/horizontal-filters.js"></script>
+<script src="../js/horizontal-menu.js"></script>
+<script src="../js/main.js"></script>
+<script src="../js/movie.js"></script>
+<script src="../js/my-calendar.js"></script>
+<script src="../js/palmares.js"></script>
+<script src="../js/press.js"></script>
+<script src="../js/press_accreditation.js"></script>
+<script src="../js/press_download.js"></script>
+<script src="../js/press_maps.js"></script>
+<script src="../js/press_medias.js"></script>
+<script src="../js/programmation.js"></script>
+<script src="../js/search.js"></script>
+<script src="../js/searchpage.js"></script>
+<script src="../js/selection.js"></script>
+<script src="../js/slidervideos.js"></script>
+<script src="../js/slideshows-chocolat.js"></script>
+<script src="../js/webtv.js"></script>
+<script src="../js/include/footer.js"></script>
+<script src="../js/include/menu.js"></script>
+<script src="../js/include/myselection.js"></script>
+<!-- fdc-app tags end -->
+<!-- endbuild -->
