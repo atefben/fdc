776f53226a5009ddc1f3b912ac304a97379202aa
Jacques tatiana <tatout08090@gmail.com>
2016-04-15 17:34:40 +0200

    Add lot3

---
+++ _html_lot3/app/assets/js/files/scroll.js
@@ -0,0 +1,22 @@
+var initHeaderSticky = function() {
+
+  $(window).on('scroll', function() {
+    var $header = $('header');
+    var lastScrollTop = 0;
+    var s = $(this).scrollTop();
+    scrollTarget = s;
+
+    // STICKY HEADER
+    if (s > lastScrollTop){
+      if(($('#prehome-container').length == 0 && s > 30)) {
+        $header.addClass('sticky');
+        $('body').css('margin-top', '91px');
+      }
+    } else {
+      if(($('#prehome-container').length == 0 && s < 30)) {
+        $header.removeClass('sticky');
+        $('body').css('margin-top', '0');
+      }
+    }
+  });
+}
---
+++ _html_lot3/build/js/app.js
@@ -0,0 +1,117 @@
+var initHeaderSticky = function() {
+
+  $(window).on('scroll', function() {
+    var $header = $('header');
+    var lastScrollTop = 0;
+    var s = $(this).scrollTop();
+    scrollTarget = s;
+
+    // STICKY HEADER
+    if (s > lastScrollTop){
+      if(($('#prehome-container').length == 0 && s > 30)) {
+        $header.addClass('sticky');
+        $('body').css('margin-top', '91px');
+      }
+    } else {
+      if(($('#prehome-container').length == 0 && s < 30)) {
+        $header.removeClass('sticky');
+        $('body').css('margin-top', '0');
+      }
+    }
+  });
+}
+
+/*------------------------------------------------------------------------------
+    JS Document (https://developer.mozilla.org/en/JavaScript)
+
+    project:    Festival de Cannes
+    Author:     OHWEE
+    created:    2016-24-03
+
+    summary:    SLIDER HOME
+----------------------------------------------------------------------------- */
+
+
+var owInitSlider = function(sliderName) {
+  /* SLIDER HOME
+  ----------------------------------------------------------------------------- */
+  if(sliderName == 'home') {
+
+    var slide = $('.slider-carousel').owlCarousel({
+      navigation          :  true,
+      items               :  1,
+      autoWidth           :  true,
+      autoplay            :  true,
+      autoplayTimeout     :  4000,
+      autoplayHoverPause  :  true,
+      loop                :  true,
+      smartSpeed          :  700
+    });
+
+    slide.on('changed.owl.carousel', function(event) {
+      setTimeout(function(){
+        var $item   = $('.owl-item.active').find('.item');
+        var number  = $item.data('item');
+        var $active = $(".container-images .item.active");
+
+        $active.removeClass("fadeInRight").addClass('fadeOut');
+
+        setTimeout(function(){
+          $(".container-images .item[data-item="+number+"]").removeClass('fadeOut').addClass('active fadeInRight');
+          $active.removeClass('active');
+        }, 500);
+      }, 200);
+    });
+  }
+
+
+  /* SLIDER 01
+  ----------------------------------------------------------------------------- */
+  if(sliderName == 'slider-01') {
+    var slide01 = $('.slider-01').owlCarousel({
+      navigation          : false,
+      items               : 1,
+      autoWidth           : true,
+      smartSpeed          : 700,
+      center              : true
+    });
+
+    // Custom Navigation Events
+    $(document).on('click', '.slider-01 .owl-item', function(){
+      var number = $(this).index();
+
+      $('.slider-01 .center').removeClass('center');
+      $(this).addClass('center');
+      slide01.trigger('to.owl.carousel', number);
+    });
+  }
+};
+
+/*------------------------------------------------------------------------------
+    JS Document (https://developer.mozilla.org/en/JavaScript)
+
+    project:    Festival de Cannes
+    Author:     OHWEE
+    created:    2016-24-03
+
+    summary:    CONSTANTES
+                UTILITIES
+                WINDOW.ONLOAD
+                MENU
+                VIDEOS
+                PLAY
+----------------------------------------------------------------------------- */
+
+/*  =INIT
+----------------------------------------------------------------------------- */
+
+$(document).ready(function() {
+
+ initHeaderSticky();
+
+  if($('.home').length){
+    owInitSlider('home');
+    owInitSlider('slider-01');
+  }
+
+});
