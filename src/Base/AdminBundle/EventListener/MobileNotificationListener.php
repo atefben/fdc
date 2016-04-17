6b0ed2d65ba81ea44d64e0862fb0404eba8466a5
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-04-17 21:36:39 +0200

    add notifications

--- src/Base/AdminBundle/EventListener/MobileNotificationListener.php
+++ src/Base/AdminBundle/EventListener/MobileNotificationListener.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jeapet
- * Date: 11/04/2016
- * Time: 17:05
- */
 
 namespace Base\AdminBundle\EventListener;
 
@@ -12,6 +6,10 @@ use Base\CoreBundle\Entity\MobileNotification;
 use Base\CoreBundle\Entity\MobileNotificationTranslation;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+require __DIR__.'/../ApnsPHP/Autoload.php';
 
 /**
  * Class MobileNotificationListener
@@ -19,6 +17,16 @@ use Doctrine\ORM\Event\PreUpdateEventArgs;
  */
 class MobileNotificationListener
 {
+	
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+	
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
 
     /**
      * @param LifecycleEventArgs $args
@@ -65,15 +73,45 @@ class MobileNotificationListener
 
     public function sendTestMobileNotification(MobileNotification $object)
     {
-        $token = $object->getToken();
-
         foreach ($object->getTranslations() as $translation) {
             $published = $translation->getStatus() === MobileNotificationTranslation::STATUS_PUBLISHED;
             $published = $published || $translation->getStatus() === MobileNotificationTranslation::STATUS_TRANSLATION_VALIDATING;
-            $title = $translation->getTitle();
-            $message = $translation->getDescription();
-			//TODO CODE NOTIFICATION ICI
-        }
+			
+			$conn = $this->getApnsConnection();
+			$conn->add($this->setNewMessage($object->getToken(), $translation->getDescription() , 1));
+			$this->sendPush($conn);
+		}
     }
+	
+	private function getApnsConnection()
+	{
+		$env = \ApnsPHP_Abstract::ENVIRONMENT_PRODUCTION;
+		if ($this->container->getParameter('instancemobile') == 'preprod') {
+			$env = \ApnsPHP_Abstract::ENVIRONMENT_SANDBOX;
+		}
+		
+		$conn = new \ApnsPHP_Push($env, $this->container->getParameter('apns'));
+		$conn->setRootCertificationAuthority($this->container->getParameter('app'));
+		$conn->connect();
+		
+		return $conn;
+	}
+	
+	private function sendPush($conn)
+	{
+		$conn->send();
+		$conn->disconnect();
+	}
+	
+	private function setNewMessage($token, $text, $badges)
+	{	
+		$message = new \ApnsPHP_Message_Custom($token);
+		$message->setBadge($badges);
+		$message->setText($text);
+		$message->setExpiry(30);
+		$message->setSound();
+		
+		return $message;
+	}
 
 }
