cab3bf055c0e4d74064f7c01afa95ac8a06ee81b
Jean-Luc Petit <webpetit@gmail.com>
2016-04-18 11:39:07 +0200

    Implement push mobile post api resource.

---
+++ src/Base/CoreBundle/Entity/PushMobile.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Base\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PushMobile
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class PushMobile
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="uuid", type="string", length=255)
+     */
+    private $uuid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="os", type="string", length=255)
+     */
+    private $os;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set uuid
+     *
+     * @param string $uuid
+     * @return PushMobile
+     */
+    public function setUuid($uuid)
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    /**
+     * Get uuid
+     *
+     * @return string 
+     */
+    public function getUuid()
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * Set os
+     *
+     * @param string $os
+     * @return PushMobile
+     */
+    public function setOs($os)
+    {
+        $this->os = $os;
+
+        return $this;
+    }
+
+    /**
+     * Get os
+     *
+     * @return string 
+     */
+    public function getOs()
+    {
+        return $this->os;
+    }
+}
