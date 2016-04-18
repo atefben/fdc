05e037f45a40aa7ab8ed043d69806bec486f841e
Jean-Luc Petit <webpetit@gmail.com>
2016-04-18 16:34:45 +0200

    Fix api news repositories.

--- src/Base/CoreBundle/Repository/InfoRepository.php
+++ src/Base/CoreBundle/Repository/InfoRepository.php
@@ -164,9 +164,6 @@ class InfoRepository extends EntityRepository
             $qb
                 ->andWhere('n.festival = :festival')
                 ->andWhere('n.publishedAt BETWEEN :morning AND :midnight')
-                ->andWhere('n.publishedAt <= :datetime')
-                ->andWhere('(n.publishEndedAt IS NULL OR n.publishEndedAt >= :datetime)')
-                ->setParameter('datetime', $dateTime)
                 ->setParameter('morning', $morning)
                 ->setParameter('midnight', $midnight)
             ;
