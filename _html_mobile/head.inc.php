c178b5807f645d5650f3647cd27ee3e9345ece15
Johann Pinson <johann.pinson@gmail.com>
2016-04-22 15:37:18 +0200

    init SASS Mobile

--- _html_mobile/head.inc.php
+++ _html_mobile/head.inc.php
@@ -9,42 +9,6 @@
 
 <!-- build:css dist/app.min.css -->
 <!-- fdc-app tags start -->
-<link rel="stylesheet" type="text/css" href="css/accordeon.css"/>
-<link rel="stylesheet" type="text/css" href="css/article.css"/>
-<link rel="stylesheet" type="text/css" href="css/articles.css"/>
-<link rel="stylesheet" type="text/css" href="css/artist.css"/>
-<link rel="stylesheet" type="text/css" href="css/audioplayer.css"/>
-<link rel="stylesheet" type="text/css" href="css/contact.css"/>
-<link rel="stylesheet" type="text/css" href="css/faq.css"/>
-<link rel="stylesheet" type="text/css" href="css/filters.css"/>
-<link rel="stylesheet" type="text/css" href="css/fullscreenplayer.css"/>
-<link rel="stylesheet" type="text/css" href="css/grid-isotope.css"/>
-<link rel="stylesheet" type="text/css" href="css/horizontal-menu.css"/>
-<link rel="stylesheet" type="text/css" href="css/jurys.css"/>
-<link rel="stylesheet" type="text/css" href="css/legals.css"/>
-<link rel="stylesheet" type="text/css" href="css/main.css"/>
-<link rel="stylesheet" type="text/css" href="css/movie.css"/>
-<link rel="stylesheet" type="text/css" href="css/my-calendar.css"/>
-<link rel="stylesheet" type="text/css" href="css/palmares.css"/>
-<link rel="stylesheet" type="text/css" href="css/participate.css"/>
-<link rel="stylesheet" type="text/css" href="css/practical_guide.css"/>
-<link rel="stylesheet" type="text/css" href="css/press.css"/>
-<link rel="stylesheet" type="text/css" href="css/press_accreditation.css"/>
-<link rel="stylesheet" type="text/css" href="css/press_downloads.css"/>
-<link rel="stylesheet" type="text/css" href="css/press_maps.css"/>
-<link rel="stylesheet" type="text/css" href="css/press_medias.css"/>
-<link rel="stylesheet" type="text/css" href="css/programmation.css"/>
-<link rel="stylesheet" type="text/css" href="css/search.css"/>
-<link rel="stylesheet" type="text/css" href="css/searchpage.css"/>
-<link rel="stylesheet" type="text/css" href="css/selection.css"/>
-<link rel="stylesheet" type="text/css" href="css/sitemap.css"/>
-<link rel="stylesheet" type="text/css" href="css/slideshow-video.css"/>
-<link rel="stylesheet" type="text/css" href="css/slideshows-chocolat.css"/>
-<link rel="stylesheet" type="text/css" href="css/videos.css"/>
-<link rel="stylesheet" type="text/css" href="css/webtv.css"/>
-<link rel="stylesheet" type="text/css" href="css/include/footer.css"/>
-<link rel="stylesheet" type="text/css" href="css/include/header.css"/>
-<link rel="stylesheet" type="text/css" href="css/include/menu.css"/>
-<link rel="stylesheet" type="text/css" href="css/include/myselection.css"/>
+<link rel="stylesheet" type="text/css" href="scss/app.css"/>
 <!-- fdc-app tags end -->
 <!-- endbuild -->
