8f688fdfff11f581d81b0bccf3e05cc5ee6ccc11
Pierre BELLENGER <p.bellenger@ohwee.fr>
2016-04-22 19:09:03 +0200

    #1951

--- src/Base/AdminBundle/Resources/public/js/edit.js
+++ src/Base/AdminBundle/Resources/public/js/edit.js
@@ -188,6 +188,20 @@ function editEvents() {
             } else {
                 $("select").val("0").trigger("change");
             }
+            var url = window.location.href;
+            url = url.split("/");
+            if(url[6] == 'mediaimage' ||
+               url[6] == 'mediaaudio' ||
+               url[6] == 'mediaimagesimple' ||
+               url[6] == 'mediavideo' ||
+               url[6] == 'gallery') {
+                 status = ['0', '1', '4', '6'];
+                if(locale != 'fr') {
+                    status = ['0', '2', '3', '5'];
+                } else {
+                    $("select").val("0").trigger("change");
+                }
+            }
         }
 
         if ($('select[name*="status"]').hasClass('orange')) {
