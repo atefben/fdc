2a9ef19b5a0f90b6e1caf30cfc005b97361efa1b
Jean-Luc Petit <webpetit@gmail.com>
2016-05-03 20:55:53 +0200

    Fix article, infos, and statement links and image.

--- src/Base/CoreBundle/Twig/Extension/NewsInstanceOfExtension.php
+++ src/Base/CoreBundle/Twig/Extension/NewsInstanceOfExtension.php
@@ -2,14 +2,17 @@
  
 namespace Base\CoreBundle\Twig\Extension;
 
+use Base\CoreBundle\Entity\Info;
 use Base\CoreBundle\Entity\InfoArticle;
 use Base\CoreBundle\Entity\InfoAudio;
 use Base\CoreBundle\Entity\InfoImage;
 use Base\CoreBundle\Entity\InfoVideo;
+use Base\CoreBundle\Entity\News;
 use Base\CoreBundle\Entity\NewsArticle;
 use Base\CoreBundle\Entity\NewsAudio;
 use Base\CoreBundle\Entity\NewsImage;
 use Base\CoreBundle\Entity\NewsVideo;
+use Base\CoreBundle\Entity\Statement;
 use Base\CoreBundle\Entity\StatementArticle;
 use Base\CoreBundle\Entity\StatementAudio;
 use Base\CoreBundle\Entity\StatementImage;
@@ -38,6 +41,9 @@ class NewsInstanceOfExtension extends Twig_Extension
             new \Twig_SimpleTest('news_audio', function ($object) { return $object instanceof NewsAudio || $object instanceof InfoAudio || $object instanceof StatementAudio; }),
             new \Twig_SimpleTest('news_image', function ($object) { return $object instanceof NewsImage || $object instanceof InfoImage || $object instanceof StatementImage; }),
             new \Twig_SimpleTest('news_video', function ($object) { return $object instanceof NewsVideo || $object instanceof InfoVideo || $object instanceof StatementVideo; }),
+            new \Twig_SimpleTest('news_object', function ($object) { return $object instanceof News; }),
+            new \Twig_SimpleTest('info_object', function ($object) { return $object instanceof Info; }),
+            new \Twig_SimpleTest('statement_object', function ($object) { return $object instanceof Statement; }),
         );
     }
 
