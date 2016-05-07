8bea3c5e7c9f8210b1357bc0ee0ec8e43f50f664
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-05-07 11:58:57 +0200

    test

--- _html/js/festival-cannes/prefooter.module.js
+++ _html/js/festival-cannes/prefooter.module.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
       $(this).addClass('active');
       sliderPrefooter.trigger('to.owl.carousel', [i, 900, true]);
       var isIE = (navigator.userAgent.indexOf("MSIE") != -1);
-      alert('test cache 1');
+      alert(navigator.userAgent.indexOf("MSIE"));
       if ( isIE) {
         alert('test cache !');
         $('#slider-prefooter .imgSlide img').hide();
