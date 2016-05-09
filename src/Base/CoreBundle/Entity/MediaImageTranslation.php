b9e5d3e9bf0874500dcc7b52d38b94598135ba35
Jean-Luc Petit <webpetit@gmail.com>
2016-05-09 14:39:57 +0200

    Fix copyright

--- src/Base/CoreBundle/Entity/MediaImageTranslation.php
+++ src/Base/CoreBundle/Entity/MediaImageTranslation.php
@@ -156,7 +156,7 @@ class MediaImageTranslation implements TranslateChildInterface
      */
     public function getApiCopyright()
     {
-        return $this->copyright === null ? $this->copyright : ' ';
+        return $this->copyright === null ? ' ' : $this->copyright;
     }
 
     /**
