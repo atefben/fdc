976e824635af134e360e40d4960711798ee9893f
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-05-10 16:36:52 +0200

    fix

--- src/Base/CoreBundle/Entity/FilmFilm.php
+++ src/Base/CoreBundle/Entity/FilmFilm.php
@@ -2021,7 +2021,11 @@ class FilmFilm implements FilmFilmInterface, TranslateMainInterface
             ksort($tempDayProjections);
             $days[$key]['projections'] = array_values($tempDayProjections);
         }
-        return array_values(array_reverse($days));
+        if($projection->getProjection()->getType() == 'Un Certain Regard') {
+            return array_values(array_reverse($days));
+        } else {
+            return array_values($days);
+        }
     }
 
     /**
