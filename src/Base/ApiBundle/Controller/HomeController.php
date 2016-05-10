58bb56938ffe8a08484dfafee6282f72f03776cb
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-05-10 18:09:14 +0200

    fix

--- src/Base/ApiBundle/Controller/HomeController.php
+++ src/Base/ApiBundle/Controller/HomeController.php
@@ -131,7 +131,6 @@ class HomeController extends FOSRestController
     public function getNextProjections($festival)
     {
         $now = new \DateTime();
-        //$now->setTimestamp(1463652000);
         $results = $this
             ->getDoctrine()
             ->getManager()
