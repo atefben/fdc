a6c134976f868e0a59d7d1eba0f51d6484dc7cc2
Jacques tatiana <tatout08090@gmail.com>
2016-05-11 16:55:54 +0200

    prehome fix css

--- _html/prehome.php
+++ _html/prehome.php
@@ -9,9 +9,9 @@
 
     <div class="content">
       <div id="logo">
-        <a href="/"><img src="img/palme-white.svg" alt="Festival de Cannes" class="palm"></a>
-        <h1><a href="/"><img width="200" src="img/logo-white.svg" alt="Festival de Cannes"></a></h1>
-        <h2><a href="/">11-22 Mai 2016</a></h2>
+        <span><img src="img/palme-white.svg" alt="Festival de Cannes" class="palm"></span>
+        <h1><img width="200" src="img/logo-white.svg" alt="Festival de Cannes"></h1>
+        <h2>11-22 Mai 2016</h2>
       </div>
 
       <div class="link">
