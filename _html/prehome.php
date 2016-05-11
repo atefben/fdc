86fcddfa06ff4157d74c46682d9baef2c6f66e66
Jacques tatiana <tatout08090@gmail.com>
2016-05-11 16:39:13 +0200

    prehomme

---
+++ _html/prehome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <?php include('head.html'); ?>
+  </head>
+
+  <body class="prehome-01">
+
+    <div class="content">
+      <div id="logo">
+        <a href="/"><img src="img/palme-white.svg" alt="Festival de Cannes" class="palm"></a>
+        <h1><a href="/"><img width="200" src="img/logo-white.svg" alt="Festival de Cannes"></a></h1>
+        <h2><a href="/">11-22 Mai 2016</a></h2>
+      </div>
+
+      <div class="link">
+        <ul>
+          <li><a href="#">Entrer</a></li>
+          <li><a href="#">Enter</a></li>
+          <li><a href="#">Entrar</a></li>
+          <li><a href="#">进入</a></li>
+        </ul>
+
+      </div>
+    </div>
+
+    <!-- //// SCRIPTS \\\\ -->
+    <?php include('scripts.inc.php'); ?>
+  </body>
+</html>
