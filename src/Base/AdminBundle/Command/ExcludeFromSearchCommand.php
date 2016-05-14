8adca16b9497848d984f9bb6b5f68bd8558c9056
Jean-Luc Petit <webpetit@gmail.com>
2016-05-14 17:48:32 +0200

    Publish all images that contains "extrait" or "trailer"

--- src/Base/AdminBundle/Command/ExcludeFromSearchCommand.php
+++ src/Base/AdminBundle/Command/ExcludeFromSearchCommand.php
@@ -9,6 +9,7 @@ use Base\CoreBundle\Entity\MediaImage;
 use Base\CoreBundle\Entity\MediaImageTranslation;
 use Base\CoreBundle\Entity\MediaVideo;
 use Base\CoreBundle\Entity\MediaVideoTranslation;
+use Base\CoreBundle\Interfaces\TranslateChildInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -31,6 +32,7 @@ class ExcludeFromSearchCommand extends ContainerAwareCommand
         $this->excludeMediaVideos($output);
         $this->excludeMediaAudios($output);
         $this->excludeMediaImages($output);
+        $this->publishMediaImages($output);
     }
 
     protected function excludeMediaVideos(OutputInterface $output)
@@ -139,6 +141,66 @@ class ExcludeFromSearchCommand extends ContainerAwareCommand
                 $this->getDoctrineManager()->flush();
                 $this->getDoctrineManager()->clear();
                 $output->writeln(count($ids) . ' images have been excluded from search.');
+            }
+        }
+    }
+
+    protected function publishMediaImages(OutputInterface $output)
+    {
+        $qb = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:MediaImageTranslation')
+            ->createQueryBuilder('mit')
+        ;
+        if ($qb instanceof QueryBuilder) {
+            $qb
+                ->andWhere('mit.legend LIKE :slice OR mit.legend LIKE :trailer')
+                ->setParameter('slice', '%extrait%')
+                ->setParameter('trailer', '%loop%')
+            ;
+        }
+        $siteFdcEvent = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:Site')
+            ->findOneBy(array('slug' => 'site-evenementiel'));
+
+
+        $flush = false;
+        $imageTranslations = $qb->getQuery()->getResult();
+        $ids = array();
+        foreach ($imageTranslations as $imageTranslation) {
+        if ($imageTranslation instanceof MediaImageTranslation) {
+                $image = $imageTranslation->getTranslatable();
+                if ($image instanceof MediaImage && !in_array($image->getId(), $ids)) {
+                    $ids[] = $image->getId();
+                    if (!$image->getSites()->contains($siteFdcEvent)) {
+                        $image->addSite($siteFdcEvent);
+                    }
+                    $image->setDisplayedMobile(true);
+                    $this->getDoctrineManager()->persist($image);
+                    foreach ($image->getTranslations() as $translation) {
+                        if ($translation instanceof MediaImageTranslation) {
+                            $translation->setisPublishedOnFDCEvent(true);
+                            if ($translation->getLocale() == 'fr') {
+                                $translation->setStatus(TranslateChildInterface::STATUS_PUBLISHED);
+                            }
+                            else {
+                                $translation->setStatus(TranslateChildInterface::STATUS_TRANSLATED);
+                            }
+                            $this->getDoctrineManager()->persist($translation);
+                        }
+                    }
+                }
+            }
+        }
+        if ($flush) {
+            $this->getDoctrineManager()->flush();
+            $this->getDoctrineManager()->clear();
+
+            if ($flush) {
+                $this->getDoctrineManager()->flush();
+                $this->getDoctrineManager()->clear();
+                $output->writeln(count($ids) . ' images and its have been published on FDC Event and Mobile.');
             }
         }
     }
