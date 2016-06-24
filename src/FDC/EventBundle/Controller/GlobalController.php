efe397b99fe1a633a073ecb98f9188630440c5f2
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-06-24 15:16:37 +0200

    too ANTOINE

--- src/FDC/EventBundle/Controller/GlobalController.php
+++ src/FDC/EventBundle/Controller/GlobalController.php
@@ -64,11 +64,13 @@ class GlobalController extends Controller {
         $menus = $em->getRepository('BaseCoreBundle:FDCEventRoutes')->childrenHierarchy();
 
         // Menu Participer
-        $participatePage = $em->getRepository('BaseCoreBundle:FDCPageParticipate')->findAll();
-        $preparePage = $em->getRepository('BaseCoreBundle:FDCPagePrepare')->findById($this->getParameter('admin_fdc_page_prepare_id'));
+ //TODO ANTOINE
+ //       $participatePage = $em->getRepository('BaseCoreBundle:FDCPageParticipate')->findAll();
+ //       $preparePage = $em->getRepository('BaseCoreBundle:FDCPagePrepare')->findById($this->getParameter('admin_fdc_page_prepare_id'));
 
-        $participateMenu = array_merge($preparePage, $participatePage);
-
+ //       $participateMenu = array_merge($preparePage, $participatePage);
+ $participateMenu = '';
+ //TODO ANTOINE
         $displayedMenus = array();
         foreach($menus as $menu){
             if($menu['site'] == FDCEventRoutesInterface::EVENT) {
