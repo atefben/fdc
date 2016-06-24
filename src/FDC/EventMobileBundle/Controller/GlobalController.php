94de8a4a6ca0033548b331df1346f3f90bb254a2
Matthieu DA CONCEICAO <md@ohwee.fr>
2016-06-24 15:11:23 +0200

    TODO ANTOINE

--- src/FDC/EventMobileBundle/Controller/GlobalController.php
+++ src/FDC/EventMobileBundle/Controller/GlobalController.php
@@ -59,12 +59,11 @@ class GlobalController extends Controller {
         $menus = $em->getRepository('BaseCoreBundle:FDCEventRoutes')->childrenHierarchy();
 
         // Menu Participer
-        $participatePage = $em->getRepository('BaseCoreBundle:FDCPageParticipate')->findAll();
-        $preparePage = $em->getRepository('BaseCoreBundle:FDCPagePrepare')->findById($this->getParameter('admin_fdc_page_prepare_id'));
+        //$participatePage = $em->getRepository('BaseCoreBundle:FDCPageParticipate')->findAll();
+        //$preparePage = $em->getRepository('BaseCoreBundle:FDCPagePrepare')->findById($this->getParameter('admin_fdc_page_prepare_id'));
 
         //$participateMenu = array_merge($preparePage, $participatePage);
-	//TODO DELETE :
- $participateMenu = '';
+
         $displayedMenus = array();
         foreach($menus as $menu){
             if($menu['site'] == FDCEventRoutesInterface::EVENT) {
@@ -126,12 +125,11 @@ class GlobalController extends Controller {
         $menus = $em->getRepository('BaseCoreBundle:FDCEventRoutes')->childrenHierarchy();
   
         // Menu Participer
-        $participatePage = $em->getRepository('BaseCoreBundle:FDCPageParticipate')->findAll();
-        $preparePage = $em->getRepository('BaseCoreBundle:FDCPagePrepare')->findById($this->getParameter('admin_fdc_page_prepare_id'));
+        //$participatePage = $em->getRepository('BaseCoreBundle:FDCPageParticipate')->findAll();
+        //$preparePage = $em->getRepository('BaseCoreBundle:FDCPagePrepare')->findById($this->getParameter('admin_fdc_page_prepare_id'));
   
         //$participateMenu = array_merge($preparePage, $participatePage);
-  //TODO DELETE :
-  $participateMenu = '';
+  
         $displayedMenus = array();
         foreach($menus as $menu){
             if($menu['site'] == FDCEventRoutesInterface::EVENT) {
