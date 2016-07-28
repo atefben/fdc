bb1a99886ae9a792054ddccd52e742c604d23b24
Antoine Mineau <am2@ohwee.fr>
2016-07-28 11:11:46 +0200

    avoid entity manager is closed

--- src/Base/SoifBundle/Manager/JuryManager.php
+++ src/Base/SoifBundle/Manager/JuryManager.php
@@ -201,6 +201,18 @@ class JuryManager extends CoreManager
         
         // save entities
         $this->em->flush();
+        try {
+            $this->em->flush();
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            // reset entity manager
+            if (!$this->em->isOpen()) {
+                $this->em = $this->entityManager->create(
+                    $this->em->getConnection(),
+                    $this->em->getConfiguration()
+                );
+            }
+        }
         
         // end timer
         $this->end(__METHOD__);
