baf3af2b5b82349eff2cf85ce5f0a9cd2da7dd0c
Steone <s.onephandara@ohwee.fr>
2016-09-05 18:51:18 +0200

    #4601 big slider fix & medias page

--- src/FDC/CorporateBundle/Controller/NewsController.php
+++ src/FDC/CorporateBundle/Controller/NewsController.php
@@ -2,12 +2,14 @@
 
 namespace FDC\CorporateBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FDC\EventBundle\Component\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use DateTime;
 
 /**
- * @Route("/")
+ * @Route("/69-editions/retrospective")
  */
 class NewsController extends Controller
 {
@@ -18,5 +20,283 @@ class NewsController extends Controller
     public function homeAction()
     {
         return array();
+    }
+
+    /**
+     * @Route("/articles")
+     * @Template("FDCCorporatesBundle:News/list:article.html.twig")
+     */
+    public function getArticlesAction(Request $request, $year)
+    {
+        //$offset = 30;
+        $this->isPageEnabled($request->get('_route'));
+
+        $dateTime = new DateTime();
+
+        $em = $this->getDoctrine()->getManager();
+        $locale = $request->getLocale();
+
+        $festival = $this->getFestival($year);
+        $festivals = $this->getDoctrine()->getRepository('BaseCoreBundle:FilmFestival')->findAll();
+
+        // SEO
+        $id = $this->getParameter('admin_fdc_page_news_articles_id');
+        $page = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:FDCPageNewsArticles')
+            ->find($id)
+        ;
+
+        if ($page == NULL) {
+            throw $this->createNotFoundException('Page not found');
+        }
+
+        $this->get('base.manager.seo')->setFDCEventPageAllNewsSeo($page, $locale);
+
+        //GET ALL NEWS ARTICLES
+        $newsArticles = $em->getRepository('BaseCoreBundle:News')->getAllNews($locale, $festival->getId());
+        $newsArticles = $this->removeUnpublishedNewsAudioVideo($newsArticles, $locale, null, true);
+        if ($newsArticles === null || count($newsArticles) == 0) {
+            throw new NotFoundHttpException();
+        }
+
+        //set default filters
+        $filters = array();
+        $filters['dates'][0] = 'all';
+        $filters['dateFormated'][0] = 'all';
+        $filters['themes']['content'][0] = 'all';
+        $filters['themes']['id'][0] = 'all';
+
+
+        foreach ($newsArticles as $key => $newsArticle) {
+            $isPublished = ($newsArticles !== null) ? ($newsArticle->findTranslationByLocale('fr')->getStatus() === NewsArticleTranslation::STATUS_PUBLISHED) : false;
+            if ($isPublished) {
+
+                if (($key % 3) == 0) {
+                    $newsArticle->double = true;
+                }
+
+                //check if filters don't already exist
+                $date = $newsArticle->getPublishedAt();
+                if ($date && !array_key_exists($date->format('y-m-d'), $filters['dates'])) {
+                    $filters['dates'][$date->format('y-m-d')] = $date;
+                }
+
+                if (!in_array($newsArticle->getTheme()->getId(), $filters['themes']['id'])) {
+                    $filters['themes']['id'][] = $newsArticle->getTheme()->getId();
+                    $filters['themes']['content'][] = $newsArticle->getTheme();
+                }
+            } else {
+                unset($newsArticles[$key]);
+            }
+        }
+
+        return array(
+            'articles' => $newsArticles,
+            'filters'  => $filters,
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     * @Route("/{year}/photos")
+     * @Template("FDCCorporateBundle:News/list:medias.html.twig")
+     */
+    public function getPhotosAction(Request $request, $year)
+    {
+
+        $this->isPageEnabled($request->get('_route'));
+
+        $em = $this->getDoctrine()->getManager();
+        $dateTime = new DateTime();
+        $locale = $request->getLocale();
+        $festival = $this->getFestival($year);
+        $festivals = $this->getDoctrine()->getRepository('BaseCoreBundle:FilmFestival')->findAll();
+
+        // SEO
+        $id = $this->getParameter('admin_fdc_page_news_images_id');
+        $page = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:FDCPageNewsImages')
+            ->find($id)
+        ;
+
+        if ($page === null) {
+            $this->createNotFoundException('Page not found');
+        }
+
+        $this->get('base.manager.seo')->setFDCEventPageAllNewsSeo($page, $locale);
+
+        //GET ALL MEDIA PHOTOS
+        $photos = $em->getRepository('BaseCoreBundle:Media')->getImageMedia($locale, $festival->getId(), $dateTime);
+
+        //set default filters
+        $filters = array();
+        $filters['dates'][0] = 'all';
+        $filters['dateFormated'][0] = 'all';
+        $filters['themes']['content'][0] = 'all';
+        $filters['themes']['id'][0] = 'all';
+
+        foreach ($photos as $key => $photo) {
+            $photo->theme = $photo->getTheme();
+
+            if (($key % 3) == 0) {
+                $photo->double = true;
+            }
+
+            //check if filters don't already exist
+            $date = $photo->getPublishedAt();
+            if ($date && !array_key_exists($date->format('y-m-d'), $filters['dates'])) {
+                $filters['dates'][$date->format('y-m-d')] = $date;
+            }
+
+            if (!in_array($photo->getTheme()->getId(), $filters['themes']['id'])) {
+                $filters['themes']['id'][] = $photo->getTheme()->getId();
+                $filters['themes']['content'][] = $photo->getTheme();
+            }
+        }
+
+        return array(
+            'photos'  => $photos,
+            'filters' => $filters,
+            'festivals' => $festivals,
+        );
+    }
+
+    /**
+     * @Route("/videos")
+     * @Template("FDCCorporateBundle:News/list:video.html.twig")
+     */
+    public function getVideosAction(Request $request)
+    {
+
+        $this->isPageEnabled($request->get('_route'));
+
+        $em = $this->getDoctrine()->getManager();
+        $dateTime = new DateTime();
+        $locale = $this->getRequest()->getLocale();
+
+        // GET FDC SETTINGS
+        $settings = $em->getRepository('BaseCoreBundle:Settings')->findOneBySlug('fdc-year');
+        if ($settings === null || $settings->getFestival() === null) {
+            throw new NotFoundHttpException();
+        }
+
+        // SEO
+        $id = $this->getParameter('admin_fdc_page_news_videos_id');
+        $page = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:FDCPageNewsVideos')
+            ->find($id)
+        ;
+
+        if ($page === null) {
+            $this->createNotFoundException('Page not found');
+        }
+
+        $this->get('base.manager.seo')->setFDCEventPageAllNewsSeo($page, $locale);
+
+        //GET ALL MEDIA VIDEOS
+        $videos = $em->getRepository('BaseCoreBundle:Media')->getVideoMedia($locale, $settings->getFestival()->getId(), $dateTime);
+
+        //set default filters
+        $filters = array();
+        $filters['dates'][0] = 'all';
+        $filters['themes']['content'][0] = 'all';
+        $filters['themes']['slug'][0] = 'all';
+
+        foreach ($videos as $key => $video) {
+            $video->theme = $video->getTheme();
+
+            if (($key % 3) == 0) {
+                $video->double = true;
+            }
+
+            //check if filters don't already exist
+            $date = $video->getPublishedAt();
+            if ($date && !array_key_exists($date->format('y-m-d'), $filters['dates'])) {
+                $filters['dates'][$date->format('y-m-d')] = $date;
+            }
+
+            if (!in_array($video->getTheme()->getName(), $filters['themes']['content'])) {
+                $filters['themes']['slug'][] = $video->getTheme()->getName();
+                $filters['themes']['content'][] = $video->getTheme();
+            }
+        }
+
+        return array(
+            'videos'  => $videos,
+            'filters' => $filters,
+        );
+
+    }
+
+    /**
+     * @Route("/audios")
+     * @Template("FDCCorporateBundle:News/list:audio.html.twig")
+     */
+    public function getAudiosAction(Request $request)
+    {
+
+        $this->isPageEnabled($request->get('_route'));
+
+        $em = $this->getDoctrine()->getManager();
+        $dateTime = new DateTime();
+        $locale = $this->getRequest()->getLocale();
+
+        // GET FDC SETTINGS
+        $settings = $em->getRepository('BaseCoreBundle:Settings')->findOneBySlug('fdc-year');
+        if ($settings === null || $settings->getFestival() === null) {
+            throw new NotFoundHttpException();
+        }
+
+        // SEO
+        $id = $this->getParameter('admin_fdc_page_news_audios_id');
+        $page = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:FDCPageNewsAudios')
+            ->find($id)
+        ;
+
+        if ($page === null) {
+            $this->createNotFoundException('Page not found');
+        }
+
+        $this->get('base.manager.seo')->setFDCEventPageAllNewsSeo($page, $locale);
+
+        //GET ALL MEDIA AUDIOS
+        $audios = $em->getRepository('BaseCoreBundle:Media')->getAudioMedia($locale, $settings->getFestival()->getId(), $dateTime);
+
+        //set default filters
+        $filters = array();
+        $filters['dates'][0] = 'all';
+        $filters['themes']['content'][0] = 'all';
+        $filters['themes']['slug'][0] = 'all';
+
+        foreach ($audios as $key => $audio) {
+            $audio->theme = $audio->getTheme();
+
+            if (($key % 3) == 0) {
+                $audio->double = true;
+            }
+
+            //check if filters don't already exist
+            $date = $audio->getPublishedAt();
+            if ($date && !array_key_exists($date->format('y-m-d'), $filters['dates'])) {
+                $filters['dates'][$date->format('y-m-d')] = $date;
+            }
+
+            if (!in_array($audio->getTheme()->getName(), $filters['themes']['content'])) {
+                $filters['themes']['slug'][] = $audio->getTheme()->getName();
+                $filters['themes']['content'][] = $audio->getTheme();
+            }
+
+        }
+
+        return array(
+            'audios'  => $audios,
+            'filters' => $filters,
+        );
     }
 }
