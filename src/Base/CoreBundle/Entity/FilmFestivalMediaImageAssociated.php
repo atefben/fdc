67dbfe774188f9a8cfb7122fc44c692192c904a4
Steone <s.onephandara@ohwee.fr>
2016-10-06 15:14:04 +0200

    slider images per year

---
+++ src/Base/CoreBundle/Entity/FilmFestivalMediaImageAssociated.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Base\CoreBundle\Entity;
+
+use \DateTime;
+
+use A2lix\I18nDoctrineBundle\Doctrine\ORM\Util\Translatable;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+use Base\CoreBundle\Util\Time;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+use JMS\Serializer\Annotation\Groups;
+
+/**
+ * FilmFestivalMediaImageAssociated
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ */
+class FilmFestivalMediaImageAssociated
+{
+    use Time;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var News
+     *
+     * @ORM\ManyToOne(targetEntity="FilmFestival", inversedBy="associatedMediaImages")
+     */
+    protected $festival;
+
+    /**
+     * @var MediaImage
+     *
+     * @ORM\ManyToOne(targetEntity="MediaImage")
+     */
+    protected $association;
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set news
+     *
+     * @param \Base\CoreBundle\Entity\FilmFestival $festival
+     * @return FilmFestivalMediaImageAssociated
+     */
+    public function setFestival(\Base\CoreBundle\Entity\FilmFestival $festival = null)
+    {
+        $this->festival = $festival;
+
+        return $this;
+    }
+
+    /**
+     * Get festival
+     *
+     * @return \Base\CoreBundle\Entity\FilmFestival
+     */
+    public function getFestival()
+    {
+        return $this->festival;
+    }
+
+    /**
+     * Set association
+     *
+     * @param \Base\CoreBundle\Entity\MediaImage $association
+     * @return FilmFestivalMediaImageAssociated
+     */
+    public function setAssociation(\Base\CoreBundle\Entity\MediaImage $association = null)
+    {
+        $this->association = $association;
+
+        return $this;
+    }
+
+    /**
+     * Get association
+     *
+     * @return \Base\CoreBundle\Entity\MediaImage
+     */
+    public function getAssociation()
+    {
+        return $this->association;
+    }
+}
