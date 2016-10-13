e473ee88e8f79c8b6b72bb3622ce883e045df39c
Steone <s.onephandara@ohwee.fr>
2016-10-13 18:34:35 +0200

    corporate homepage

--- src/FDC/CorporateBundle/Controller/DefaultController.php
+++ src/FDC/CorporateBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace FDC\CorporateBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use DateTime;
 
 class DefaultController extends Controller
 {
@@ -12,8 +14,47 @@ class DefaultController extends Controller
      * @Route("/")
      * @Template("FDCCorporateBundle:News:home.html.twig")
      */
-    public function homeAction()
+    public function homeAction(Request $request)
     {
-        return array();
+        $em = $this->get('doctrine')->getManager();
+        $dateTime = new DateTime();
+        $locale = $request->getLocale();
+
+        // GET HOMEPAGE SETTINGS
+        $homepage = $em->getRepository('BaseCoreBundle:HomepageCorporate')->find(1);
+        if ($homepage === null) {
+            throw new NotFoundHttpException();
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////      SLIDER      //////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////
+
+        $slides = $em->getRepository('BaseCoreBundle:HomepageCorporateSlide')->getAllSlide($locale, $dateTime);
+
+        $displayHomeSlider = $homepage->getDisplayedSlider();
+        $homeSlider = array();
+        foreach ($slides as $slide) {
+            if ($slide->getInfo() != null) {
+                $homeSlider[] = $slide->getInfo();
+            } elseif ($slide->getStatement() != null) {
+                $homeSlider[] = $slide->getStatement();
+            }
+        }
+
+        //set default filters
+        $filters = array();
+        $filters['format'][0] = 'all';
+        $filters['themes']['content'][0] = 'all';
+        $filters['themes']['id'][0] = 'all';
+
+
+        return array(
+            'homepage'           => $homepage,
+            'displayHomeSlider'  => $displayHomeSlider,
+            'homeSlider'         => $homeSlider,
+            'festivalStartsAt'      => $homepage->getFestivalStartsAt(),
+            'filters'            => $filters
+        );
     }
 }
