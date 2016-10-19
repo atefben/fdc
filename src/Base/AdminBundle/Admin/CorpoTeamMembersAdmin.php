9db50b00932a34db5ff75d311f3d630e3d026d51
Pierre BELLENGER <p.bellenger@ohwee.fr>
2016-10-19 12:26:32 +0200

    add corps team & widgets

--- src/Base/AdminBundle/Admin/CorpoTeamMembersAdmin.php
+++ src/Base/AdminBundle/Admin/CorpoTeamMembersAdmin.php
@@ -2,14 +2,50 @@
 
 namespace Base\AdminBundle\Admin;
 
+use Base\CoreBundle\Entity\FDCPageLaSelectionCannesClassics;
+use Base\CoreBundle\Entity\FDCPageLaSelectionCannesClassicsTranslation;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
 
 class CorpoTeamMembersAdmin extends Admin
 {
+
+    protected $datagridValues = array(
+        '_page' => 1,
+        '_sort_order' => 'DESC',
+        '_sort_by' => 'id'
+    );
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('show');
+        $collection->remove('export');
+    }
+
+    protected $formOptions = array(
+        'cascade_validation' => true
+    );
+
+    protected $translationDomain = 'BaseAdminBundle';
+
+
+    public function configure()
+    {
+        $this->setTemplate('edit', 'BaseAdminBundle:CRUD:edit_polycollection.html.twig');
+    }
+
+    public function getFormTheme()
+    {
+        return array_merge(
+            parent::getFormTheme(),
+            array('BaseAdminBundle:Form:polycollection.html.twig')
+        );
+    }
+
     /**
      * @param DatagridMapper $datagridMapper
      */
@@ -52,13 +88,67 @@ class CorpoTeamMembersAdmin extends Admin
      */
     protected function configureFormFields(FormMapper $formMapper)
     {
+        $securityContext = $this->getConfigurationPool()->getContainer()->get('security.context');
+        $isTranslatorEnEsCh = (
+            $securityContext->isGranted('ROLE_TRANSLATOR_EN') ||
+            $securityContext->isGranted('ROLE_TRANSLATOR_ES') ||
+            $securityContext->isGranted('ROLE_TRANSLATOR_ZH')
+        ) ? true : false;
+        $requiredLocales = ($isTranslatorEnEsCh) ? array() : array('fr');
+
         $formMapper
-            ->add('id')
-            ->add('createdAt')
-            ->add('updatedAt')
+            ->add('translations', 'a2lix_translations', array(
+                'label'              => false,
+                'translation_domain' => 'BaseAdminBundle',
+                'required_locales'   => $requiredLocales,
+                'fields'             => array(
+                    'applyChanges' => array(
+                        'field_type' => 'hidden',
+                        'attr' => array (
+                            'class' => 'hidden'
+                        )
+                    ),
+                    'status'         => array(
+                        'label'                     => 'form.label_status',
+                        'translation_domain'        => 'BaseAdminBundle',
+                        'field_type'                => 'choice',
+                        'choices'                   => FDCPageLaSelectionCannesClassicsTranslation::getStatuses(),
+                        'choice_translation_domain' => 'BaseAdminBundle'
+                    ),
+                    'firstname' => array(
+                        'label' => 'Prénom'
+                    ),
+                    'lastname' => array(
+                        'label' => 'Nom'
+                    ),
+                    'function' => array(
+                        'label' => 'Fonction'
+                    ),
+                    'createdAt'      => array(
+                        'display' => false
+                    ),
+                    'updatedAt'      => array(
+                        'display' => false
+                    ),
+                )
+            ))
+            ->add('mainImage', 'sonata_type_model_list', array(
+                'label'    => 'Photo du membre',
+                'help'     => 'form.fdc_page_web_tv_trailers.helper_image',
+                'required' => false,
+            ))
             ->add('translate')
-            ->add('translateOptions')
-            ->add('priorityStatus')
+            ->add('translateOptions', 'choice', array(
+                'choices' => FDCPageLaSelectionCannesClassics::getAvailableTranslateOptions(),
+                'translation_domain' => 'BaseAdminBundle',
+                'multiple' => true,
+                'expanded' => true
+            ))
+            ->add('priorityStatus', 'choice', array(
+                'choices'                   => FDCPageLaSelectionCannesClassics::getPriorityStatuses(),
+                'choice_translation_domain' => 'BaseAdminBundle',
+            ))
+            // must be added to display informations about creation user / date, update user / date (top of right sidebar)
         ;
     }
 
