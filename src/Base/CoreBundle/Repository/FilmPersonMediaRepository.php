68b0532d623b87ceb21f9472a9a0728ee2043e80
Pierre BELLENGER <p.bellenger@ohwee.fr>
2016-10-28 11:59:35 +0200

    fix breadcrumb

--- src/Base/CoreBundle/Repository/FilmPersonMediaRepository.php
+++ src/Base/CoreBundle/Repository/FilmPersonMediaRepository.php
@@ -1,38 +1,7 @@
 <?php
-
-namespace Base\CoreBundle\Repository;
-
-use Base\CoreBundle\Component\Repository\EntityRepository;
-use Doctrine\ORM\Tools\Pagination\Paginator;
-
 /**
- * FilmPersonMediaRepository class.
- *
- * \@extends EntityRepository
- * @author   Stevens-Son ONEPHANDARA
- * \@company Ohwee
+ * Created by PhpStorm.
+ * User: Steone
+ * Date: 27/10/2016
+ * Time: 17:45
  */
-class FilmPersonMediaRepository extends EntityRepository
-{
-    public function getMedias($search, $yearStart, $yearEnd)
-    {
-        $qb = $this->createQueryBuilder('fpm')
-            ->leftJoin('fpm.media', 'fm')
-            ->leftJoin('fpm.person', 'p')
-            ->leftJoin('p.translations', 'pt')
-            ->andWhere('fpm.media IS NOT NULL')
-            ->andWhere('fpm.person IS NOT NULL')
-            ->andWhere('p.firstname LIKE :search OR p.lastname LIKE :search OR p.asianName LIKE :search')
-            ->setParameter('search', '%'.$search.'%');
-
-        if($yearStart && $yearEnd) {
-            $qb->andWhere('fpm.createdAt BETWEEN :yearStart AND :yearEnd')
-                ->setParameter('yearStart', $yearStart)
-                ->setParameter('yearEnd', $yearEnd)
-            ;
-        }
-
-        return $qb->getQuery()->getResult();
-    }
-
-}
