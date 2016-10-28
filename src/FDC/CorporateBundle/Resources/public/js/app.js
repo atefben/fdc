310f9369ad2ad6ab74836ce26416105937d9d012
Jacques tatiana <tatout08090@gmail.com>
2016-10-28 14:33:50 +0200

    test slider medialib

--- src/FDC/CorporateBundle/Resources/public/js/app.js
+++ src/FDC/CorporateBundle/Resources/public/js/app.js
@@ -3090,12 +3090,20 @@ var owInitSliderSelect = function(id) {
     ];
 
 
+      ys = $('#s-yearstart').val();
+      ye = $('#s-yearend').val();
+
+      ys = parseInt(ys);
+      ye = parseInt(ye);
+
+      console.log(ys+' , '+ye);
+
     noUiSlider.create(slider, {
-    	start: [1946, GLOBALS.year],//todo script
+    	start: [ys, ye],//todo script
     	connect: true,
     	range: {
-    		'min': 1946,
-    		'max': GLOBALS.year
+    		'min': ys,
+    		'max': ye
     	}
      });
 
