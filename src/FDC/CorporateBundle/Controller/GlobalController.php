a6135a4d88d2b643c43fa6b479bbc24820735b42
Pierre BELLENGER <p.bellenger@ohwee.fr>
2016-11-08 11:16:56 +0100

    fix social + mails + fix footer

--- src/FDC/CorporateBundle/Controller/GlobalController.php
+++ src/FDC/CorporateBundle/Controller/GlobalController.php
@@ -35,7 +35,7 @@ class GlobalController extends Controller {
      * @param $description
      * @return array
      */
-    public function shareEmailAction(Request $request, $section = null, $detail = null, $title = null, $description = null, $url = null, $artist = null) {
+    public function shareEmailAction(Request $request, $section = null, $detail = null, $title = null, $description = null, $url = null, $artist = null, $color = false) {
         $email = array(
             'section' => $section,
             'detail' => $detail,
@@ -115,13 +115,15 @@ class GlobalController extends Controller {
                 'share_email' => $email,
                 'form' => $form,
                 'hasErrors' => $hasErrors,
-                'artist' => $this->getDoctrineManager()->getRepository('BaseCoreBundle:FilmPerson')->find($artist)
+                'artist' => $this->getDoctrineManager()->getRepository('BaseCoreBundle:FilmPerson')->find($artist),
+                'color'  => $color
             );
         } else {
             return array(
                 'share_email' => $email,
                 'form' => $form,
                 'hasErrors' => $hasErrors,
+                'color'  => $color
             );
         }
 
