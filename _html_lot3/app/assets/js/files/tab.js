86292c04d6bf6abec46e644717a71aa4fe4fa3fa
Jacques tatiana <tatout08090@gmail.com>
2016-11-09 18:19:08 +0100

    LOT3 - #5092

--- _html_lot3/app/assets/js/files/tab.js
+++ _html_lot3/app/assets/js/files/tab.js
@@ -4,6 +4,30 @@ var owInitTab = function(id) {
 
     var $tab = $('.tab1 td');
 
+    var hash = window.location.hash;
+    hash = hash.substring(5)
+
+    if(hash != '') {
+
+      var dataTab = hash;
+      var $block = $('.block-contain-txt-register-movie[data-article='+dataTab+']');
+      var $element = $('#tab1 tr[data-article='+dataTab+']');
+
+
+
+      $tab.removeClass('active');
+      $element.addClass('active');
+
+      $('.block-contain-txt-register-movie').removeClass('active animated fadeInUp');
+
+      $block.addClass('active animated fadeInUp');
+
+      var hashPush = '#tab='+dataTab;
+      history.pushState(null, null, hashPush);
+    }
+
+
+
 
     $tab.on('click', function(e) {
       var $element = $(this);
@@ -16,7 +40,14 @@ var owInitTab = function(id) {
       $('.block-contain-txt-register-movie').removeClass('active animated fadeInUp');
 
       $block.addClass('active animated fadeInUp');
+
+      var hashPush = '#tab='+dataTab;
+      history.pushState(null, null, hashPush);
+
     });
 
   }
+
+
+
 };
