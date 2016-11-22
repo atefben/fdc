f07e7dca60c8cded38919ce4e9e5ee7d9916bfc9
Jean-Luc Petit <webpetit@gmail.com>
2016-11-22 16:57:20 +0100

    Fix media video submission and createdAt item

--- src/Base/CoreBundle/Util/Time.php
+++ src/Base/CoreBundle/Util/Time.php
@@ -20,7 +20,7 @@ trait Time
     /**
      * @var \DateTime
      *
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", nullable=true)
      *
      * @Groups({"time"})
      */
