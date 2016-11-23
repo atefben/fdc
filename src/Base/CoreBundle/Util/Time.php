2cabcc169c1c0cdd962eeab25e7cbaf378a13bea
Jean-Luc Petit <webpetit@gmail.com>
2016-11-23 10:49:05 +0100

    Fix updatedAt date

--- src/Base/CoreBundle/Util/Time.php
+++ src/Base/CoreBundle/Util/Time.php
@@ -29,7 +29,7 @@ trait Time
     /**
      * @var \DateTime
      *
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", nullable=true)
      *
      * @Groups({"time"})
      */
