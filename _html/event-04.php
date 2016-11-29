c64004f0947c92620ddad69bc2ea818a7da8da00
axcff <axel.constantinoff@gmail.com>
2016-11-29 18:33:27 +0100

    29/11 afternoon

--- _html/event-04.php
+++ _html/event-04.php
@@ -46,7 +46,7 @@
       </div>-->
       <div class="content-article">
         <div class="container small-container">
-          <div class="info">
+            <div class="info">
             <a href="#" class="category">Cinéma de la plage</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
             <span class="update">Mise à jour :</span><span class="date">18.05.15</span> . <span class="hour">10:00</span>
           </div>
@@ -55,6 +55,62 @@
             <a id="share-article" href="#" class="button"><i class="icon icon_share"></i>Partager</a>
             <a href="#" class="button print" onclick="window.print()"><i class="icon icon_print"></i>Imprimer</a>
           </div>
+
+          <div class="single-photo">
+            <div class="photo-container">
+              <img src="img/article/003.jpg" alt="" />
+              <p class="caption">Équipe du film - Photocall - The Lobster © FDC / Théophile Delange</p>
+            </div>
+          </div>
+
+          <div class="chapeau">Ancien journaliste devenu producteur et cinéaste, Éric Hannezo s'aventure pour son premier film sur les routes nord-américaines et signe un polar hybride à l'affiche duquel on retrouve <a href="#">Lambert Wilson</a> et Franck Gastambide.</div>
+          
+          <div class="text">
+            <p><strong>Enragés est l’adaptation d’un film de Mario Bava intitulé Rabid Dogs. Pourquoi l’avoir choisi comme base de votre scénario ?</strong></p>
+            <p>Je suis tombé dessus par hasard dans ma boulimie de cinéma et je me suis aperçu qu’il y avait énormément de choses dans sa structure qui me séduisaient. <span class="italic">C’est un film considéré comme culte car il a été censuré plus de vingt ans avant d'être visible</span>. Puis, je suis rentré en travail d’écriture pour prendre la tangente par rapport à l’original.</p>
+          </div>
+          
+          <div class="text">
+          <a href="#"><div class="formBtn modal-toggle centered">voir le formulaire</div></a>
+          </div>
+
+          <div class="quote">
+            <blockquote>
+              “J’ai découvert que Lambert avait une passion pour le film de genre”
+            </blockquote>
+          </div>
+
+          <div class="text">
+            <p><strong>Enragés est l’adaptation d’un film de Mario Bava intitulé Rabid Dogs. Pourquoi l’avoir choisi comme base de votre scénario ?</strong></p>
+            <p>Je suis tombé dessus par hasard dans ma boulimie de cinéma et je me suis aperçu qu’il y avait énormément de choses dans sa structure qui me séduisaient. C’est un film considéré comme culte car il a été censuré plus de vingt ans avant d'être visible. Puis, je suis rentré en travail d’écriture pour prendre la tangente par rapport à l’original.</p>
+          </div>
+          
+          <div class="wideWrapper goldQuote">
+            <div class="goldBorders">
+              <h3>Dans quel sens ?</h3>
+              <p>Je voulais être libre au niveau dans la narration et la forme. Le film tire sa singularité du fait que nous l’avons tourné aux États-Unis avec un casting français. Je ne souhaitais pas rester dans le schéma classique, avec les méchants d’un côté et les gentils de l’autre, mais que ce</p>
+            </div>
+          </div>
+          
+          <div class="text">
+            <p><strong>Enragés est l’adaptation d’un film de Mario Bava intitulé Rabid Dogs. Pourquoi l’avoir choisi comme base de votre scénario ?</strong></p>
+            <p>Je suis tombé dessus par hasard dans ma boulimie de cinéma et je me suis aperçu qu’il y avait énormément de choses dans sa structure qui me séduisaient. C’est un film considéré comme culte car il a été censuré plus de vingt ans avant d'être visible. Puis, je suis rentré en travail d’écriture pour prendre la tangente par rapport à l’original.</p>
+          </div>
+          
+          <div class="text wysiwygBlocks">
+            <h6>Cinéaste, producteur, distributeur et acteur italien, Nanni Moretti est né en 1953 à Brunico en Italie. Il réalise un certain nombre de courts métrages avant de terminer son premier long métrage Io Sono un Autarchico en 1976 et de jouer dans Padre Padrone des frères Taviani présenté à Cannes en 1977. Plusieurs de ses films suivants sont primés à travers le monde : Sogni d'Oro (Prix spécial du jury à Venise).</h6>
+          </div>
+        </div>
+
+          <div class="wysiwygGreyblock">
+            <div class="smallWrapper">
+            <h4>Enragés est l’adaptation d’un film</h4>
+            <p>Cela fait vingt ans que je travaille l’image par le biais du reportage ou du documentaire pour me rapprocher de plus en plus de cette envie de cinéma qui ne m’a jamais quittée depuis l’adolescence.</p>
+            </div>
+          </div>
+
+        <div class="container">
+
           <div class="slideshow eventSlideshow">
             <div class="slideshow-img">
               <div class="images">
@@ -122,63 +178,35 @@
             </div>
             <p class="caption"><strong>Équipe du film - Photocall - The Lobster</strong> © FDC / Théophile Delange</p>
           </div>
-          <div class="minPadding">   
-              <div class="wideWrapper wysiwygBlocks">
-                <h2>Workshops et networking pour une formation ciblée</h2>
-                <p>En plus de tous les avantages d'un badge Marché du Film, vous bénéficiez, durant les premiers jours du Festival de Cannes et du Marché du Film, d’un accès au Producers Workshop: un programme ciblé, mené par des experts de l’industrie, destiné à soutenir les producteurs dans leurs démarches à l’international et à rendre plus efficace leur participation au Marché du Film et au Festival de Cannes.</p><br>
-                <p>Un cocktail de networking inaugure le programme en toute convivialité ! Abordez le marché international avec confiance grâce à nos ateliers et séminaires sur le rôle de producteur, les techniques de pitch, la coproduction et le financement international, les stratégies marketing,  la distribution et les ventes internationales du mercredi 11 au samedi 14 mai 2016. Le dimanche</p><br>
-                <p>Pour plus de renseignements, merci de contacter Lisa Kermabon – Manager Producers Workshop / Industry Programs: <a href="mailto:lkermabon@festival-cannes.fr">lkermabon@festival-cannes.fr</a></p><br>
-                <p>Visitez notre FAQ où vous trouverez les réponses aux questions les plus fréquentes concernant ce programme.</p>
-              </div>     
-          </div>
 
-          <div class="wideWrapper wysiwygBlocks">
-            <h3>- renvoi</h3>
-            <p>Presse écrite et multimédia – Agences de presse : presse@festival-cannes.fr<br>
-                Média web : <a href="">webmedia@festival-cannes.fr</a><br>
-                Télévision – Radio – Agences de presse audiovisuelle : <a href="">presseaudio@festival-cannes.fr</a><br>
-                Photographes de presse – Agences photo : <a href="">presseaudio@festival-cannes.fr</a></p>
-          </div>
-
-          <div class="wideWrapper wysiwygBlocks">
-              <h3>-  par courrier :</h3>
-              <p>Festival de Cannes - Service de Presse<br>
-              3, rue Amélie, 75007 Paris, France<br>
-              En février, à réception des pièces demandées, le service de presse vous communiquera<br>
-              l'adresse Internet d’un portail de pré-inscription ainsi qu'une référence personnalisée<br>
-              donnant accès à un formulaire d'accréditation en ligne que vous devez compléter et valider.<br>
-              Ce portail vous permettra également de suivre le traitement de votre inscription en ligne.</p>
-          </div>
-          <div class="wideWrapper wysiwygBlocks">
-          <h3 class="goldTitle">comment déposer votre demande ?</h3>
-          <p>Dès janvier, vous pouvez faire votre demande d'accréditation en envoyant au service de<br>
-          presse les pièces justificatives listées dans la rubrique vous concernant : Presse écrite et<br>
-          multimédia, Média web, Agence de presse, Agence de presse audiovisuelle, Agence Photo,<br>
-          Photographe de presse, Télévision, Radio.<br>
-          Les documents peuvent être adressés :</p>
-          </div>
-
-          <div class="wideWrapper wysiwygBlocks goldwysiwygBlocks">
-            <div class="goldBorders">
-              <h3>La date limite pour déposer votre demande d’accréditation est fixée au 31 mars.</h3>
-              <p>Pour préserver votre confort et vos conditions de travail le nombre d'accréditations est
-                  limité. Nous insistons donc sur la nécessité de nous adresser votre demande avant la date
-                  limite.</p>
+          <div class="tableau">
+            <div class="tableauArticle">
+              <h3>compétition long métrages (25)</h3>
+              <h4>un certain regard (10)</h4>
+              <h4>un certain regard (10)</h4>
+            </div>
+            <div class="tableauArticle">
+              <h3>compétition court métrages (3)</h3>
+              <h4>un certain regard (10)</h4>
+              <h4>un certain regard (10)</h4>
             </div>
           </div>
 
-          <!-- EXEMPLE ARTICLE-->
-          <div class="content-article">
-            <div class="container small-container">
-              <div class="single-photo">
-                <div class="photo-container">
-                  <img src="img/article/001.jpg" alt="" />
-                  <p class="caption">Maïwenn - Photocall © FDC / Théophile Delange</p>
-                </div>
-              </div>
-              <div class="chapeau">Ancien journaliste devenu producteur et cinéaste, Éric Hannezo s'aventure pour son premier film sur les routes nord-américaines et signe un polar hybride à l'affiche duquel on retrouve <a href="#">Lambert Wilson</a> et Franck Gastambide.</div>
-            </div>
+          <div class="text bio">
+            <h2>Sous-titre 1</h2>
+            <p>Cinéaste, producteur, distributeur et acteur italien, Nanni Moretti est né en 1953 à Brunico en Italie. Il réalise un certain nombre de courts métrages avant de terminer son premier long métrage Io Sono un Autarchico en 1976 et de jouer dans Padre Padrone des frères Taviani présenté à Cannes en 1977. Plusieurs de ses films suivants sont primés à travers le monde : <a href="">Sogni d'Oro (Prix spécial du jury à Venise en 1981)</a>, La Messa e' finita (Ours d'Argent à Berlin en 1986) ou sélectionnés en compétition officielle à Cannes: Ecce Bombo en 1978, Aprile en 1998, Caro Diario qui remporte le prix de la mise en scène en 1994 et La Stanza del figlio, palme d'or au Festival 2001.</p>
+            <h3>Sous-titre 2</h3>
+            <p>Acteur dans la majorité de ses films, Nanni Moretti fonde en 1986 la maison de production Sacher film avec Angelo Barbagallo et produit, outre ses propres films, Notte Italiana (1987) de Carlo Mazzacurati.</p>
+            <h4>Sous-titre 3</h4>
+            <p>Cinéaste, producteur, distributeur et acteur italien, Nanni Moretti est né en 1953 à Brunico en Italie. Il réalise un certain nombre de courts métrages avant de terminer son premier long métrage Io Sono un Autarchico en 1976 et de jouer dans Padre Padrone des frères Taviani présenté à Cannes en 1977. Plusieurs de ses films suivants sont primés à travers le monde : Sogni d'Oro (Prix spécial du jury à Venise en 1981), La Messa e' finita (Ours d'Argent à Berlin en 1986) ou sélectionnés en compétition officielle à Cannes: Ecce Bombo en 1978, Aprile en 1998, Caro Diario qui remporte le prix de la mise en scène en 1994 et La Stanza del figlio, palme d'or au Festival 2001.</p>
           </div>
+          
+          <div class="text">
+            <div class="author">Rédigé par Morgane Urbain</div>
+          </div>
+  
+
+
 
           <!-- EXEMPLE PLAYER VIDEO -->
           <div class="video-container state-init video"
@@ -235,6 +263,49 @@
           </blockquote>
         </div>
 
+          <div class="wideWrapper wysiwygBlocks">
+              <h2>Workshops et networking pour une formation ciblée</h2>
+              <p>En plus de tous les avantages d'un badge Marché du Film, vous bénéficiez, durant les premiers jours du Festival de Cannes et du Marché du Film, d’un accès au Producers Workshop: un programme ciblé, mené par des experts de l’industrie, destiné à soutenir les producteurs dans leurs démarches à l’international et à rendre plus efficace leur participation au Marché du Film et au Festival de Cannes.</p><br>
+              <p>Un cocktail de networking inaugure le programme en toute convivialité ! Abordez le marché international avec confiance grâce à nos ateliers et séminaires sur le rôle de producteur, les techniques de pitch, la coproduction et le financement international, les stratégies marketing,  la distribution et les ventes internationales du mercredi 11 au samedi 14 mai 2016. Le dimanche</p><br>
+              <p>Pour plus de renseignements, merci de contacter Lisa Kermabon – Manager Producers Workshop / Industry Programs: <a href="mailto:lkermabon@festival-cannes.fr">lkermabon@festival-cannes.fr</a></p><br>
+              <p>Visitez notre FAQ où vous trouverez les réponses aux questions les plus fréquentes concernant ce programme.</p>
+          </div>     
+
+          <div class="wideWrapper wysiwygBlocks">
+            <h3>- renvoi</h3>
+            <p>Presse écrite et multimédia – Agences de presse : presse@festival-cannes.fr<br>
+                Média web : <a href="">webmedia@festival-cannes.fr</a><br>
+                Télévision – Radio – Agences de presse audiovisuelle : <a href="">presseaudio@festival-cannes.fr</a><br>
+                Photographes de presse – Agences photo : <a href="">presseaudio@festival-cannes.fr</a></p>
+          </div>
+
+          <div class="wideWrapper wysiwygBlocks">
+              <h3>-  par courrier :</h3>
+              <p>Festival de Cannes - Service de Presse<br>
+              3, rue Amélie, 75007 Paris, France<br>
+              En février, à réception des pièces demandées, le service de presse vous communiquera<br>
+              l'adresse Internet d’un portail de pré-inscription ainsi qu'une référence personnalisée<br>
+              donnant accès à un formulaire d'accréditation en ligne que vous devez compléter et valider.<br>
+              Ce portail vous permettra également de suivre le traitement de votre inscription en ligne.</p>
+          </div>
+          <div class="wideWrapper wysiwygBlocks">
+          <h3 class="goldTitle">comment déposer votre demande ?</h3>
+          <p>Dès janvier, vous pouvez faire votre demande d'accréditation en envoyant au service de<br>
+          presse les pièces justificatives listées dans la rubrique vous concernant : Presse écrite et<br>
+          multimédia, Média web, Agence de presse, Agence de presse audiovisuelle, Agence Photo,<br>
+          Photographe de presse, Télévision, Radio.<br>
+          Les documents peuvent être adressés :</p>
+          </div>
+
+          <div class="wideWrapper wysiwygBlocks goldwysiwygBlocks">
+            <div class="goldBorders">
+              <h3>La date limite pour déposer votre demande d’accréditation est fixée au 31 mars.</h3>
+              <p>Pour préserver votre confort et vos conditions de travail le nombre d'accréditations est
+                  limité. Nous insistons donc sur la nécessité de nous adresser votre demande avant la date
+                  limite.</p>
+            </div>
+          </div>
+
           <div class="categorie-items blackBcgk">
            <div class="title-list-cat">
               <h2 class="title">Hommage a manoel de oliveira</h2>
@@ -242,7 +313,7 @@
             </div>
           </div>        
 
-        <div class="single-photo">
+          <div class="single-photo">
             <div class="photo-container">
               <img src="img/article/003.jpg" alt="" />
               <p class="caption">Équipe du film - Photocall - The Lobster © FDC / Théophile Delange</p>
@@ -261,7 +332,6 @@
                 <p class="caption">© FDC / Théophile Delange</p>
               </div>
             </div>
-            <div class="author">Rédigé par Morgane Urbain</div>
           </div>
           <section class="categorie-items honneur-invit">
             <div class="contain-article-invit">
@@ -290,7 +360,17 @@
             </div>
           </section>
         </div>
-
+          <div class="share" id="share" >
+              <p>Partagez l'article</p>
+              <div class="buttons square">
+                <a href="//www.facebook.com/sharer.php?u=html.festival-cannes-2016.com.ohwee.fr&t=le%20titre" rel="nofollow" class="button facebook ajax"><i class="icon icon_facebook"></i></a>
+                <a href="//twitter.com/intent/tweet?text=Enrages%20Polar%20Hybride" class="button twitter"><i class="icon icon_twitter"></i>
+                </a>
+                <a href="#" class="button link self"><i class="icon icon_link"></i></a>
+                <a href="#" class="button email self"><i class="icon icon_lettre"></i></a>
+                <a onclick="javascript:window.print()" href="#" class="button print"><i class="icon icon_print"></i></a>
+              </div>
+          </div>
         <?php include('box-catalogue.php'); ?>  
         <?php include('box-social.php'); ?>  
         <?php include('box-news.php'); ?>
