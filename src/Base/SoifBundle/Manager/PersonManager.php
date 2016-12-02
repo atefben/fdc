36956433b0f229433eedde96510ab16c2b7f964b
Jean-Luc Petit <webpetit@gmail.com>
2016-12-02 17:01:38 +0100

    Fix LinkedDeletedPersonneDto

--- src/Base/SoifBundle/Manager/PersonManager.php
+++ src/Base/SoifBundle/Manager/PersonManager.php
@@ -2,21 +2,18 @@
 
 namespace Base\SoifBundle\Manager;
 
-use \Exception;
-
-use Doctrine\Common\Collections\ArrayCollection;
-
-use Base\CoreBundle\Entity\FilmPerson;
-use Base\CoreBundle\Entity\FilmPersonMedia;
-use Base\CoreBundle\Entity\FilmPersonTranslation;
 use Base\CoreBundle\Entity\FilmFilmPerson;
 use Base\CoreBundle\Entity\FilmFilmPersonFunction;
 use Base\CoreBundle\Entity\FilmFunction;
 use Base\CoreBundle\Entity\FilmFunctionTranslation;
+use Base\CoreBundle\Entity\FilmPerson;
+use Base\CoreBundle\Entity\FilmPersonMedia;
+use Base\CoreBundle\Entity\FilmPersonTranslation;
+use Doctrine\Common\Collections\ArrayCollection;
+use Exception;
 
 /**
  * PersonManager class.
- * 
  * @extends CoreManager
  * @author  Antoine Mineau <a.mineau@ohwee.fr>
  * @company Ohwee
@@ -25,7 +22,6 @@ class PersonManager extends CoreManager
 {
     /**
      * mediaManager
-     *
      * @var mixed
      * @access private
      */
@@ -47,7 +43,6 @@ class PersonManager extends CoreManager
 
     /**
      * __construct function.
-     * 
      * @access public
      * @return void
      */
@@ -59,44 +54,42 @@ class PersonManager extends CoreManager
         $this->wsParameterKey = 'idPersonne';
         $this->wsResultObjectKey = 'PersonneDto';
         $this->mapper = array(
-            'setId' => $this->entityIdKey,
+            'setId'        => $this->entityIdKey,
             'setAsianName' => 'IsAsianName',
-            'setLastname' => 'Nom',
+            'setLastname'  => 'Nom',
             'setFirstname' => 'Prenom',
-            'setSelfkit' => 'IdSelfkit',
+            'setSelfkit'   => 'IdSelfkit',
         );
         $this->mapperTranslations = array(
-            'CiviliteTraductions' => array(
-                'result' => 'CiviliteTraductionDto',
-                'setters' => array(
-                    'setGender' => 'Libelle'
+            'CiviliteTraductions'   => array(
+                'result'    => 'CiviliteTraductionDto',
+                'setters'   => array(
+                    'setGender' => 'Libelle',
                 ),
-                'wsLangKey' => 'CodeLangue'
+                'wsLangKey' => 'CodeLangue',
             ),
             'BiographieTraductions' => array(
-                'result' => 'BiographieTraductionDto',
-                'setters' => array(
-                    'setBiography' => 'Description'
+                'result'    => 'BiographieTraductionDto',
+                'setters'   => array(
+                    'setBiography' => 'Description',
                 ),
-                'wsLangKey' => 'CodeLangue'
+                'wsLangKey' => 'CodeLangue',
             ),
             'ProfessionTraductions' => array(
-                'result' => 'ProfessionTraductionDto',
-                'setters' => array(
-                    'setProfession' => 'Libelle'
+                'result'    => 'ProfessionTraductionDto',
+                'setters'   => array(
+                    'setProfession' => 'Libelle',
                 ),
-                'wsLangkey' => 'CodeLangue'
-            )
+                'wsLangkey' => 'CodeLangue',
+            ),
         );
     }
 
     /**
      * getById function.
-     * 
      * @access public
      * @param mixed $id
      * @return void
-     *
      * @todo save PersonneFilmDto
      */
     public function getById($id)
@@ -108,29 +101,28 @@ class PersonManager extends CoreManager
 
         // call the ws
         $result = $this->soapCall($this->wsMethod, array($this->wsParameterKey => $id));
-        
+
         if (!isset($result->{$this->wsResultKey}->Resultats->{$this->wsResultObjectKey})) {
             $this->logger->warn($this->wsMethod . " {$id} not found");
             return null;
         }
 
         $resultObject = $result->{$this->wsResultKey}->Resultats->{$this->wsResultObjectKey};
-        
+
         // set entity
         $entity = $this->set($resultObject, $result);
-        
+
         // update entity
         $this->update($entity);
-        
+
         // end timer
         $this->end(__METHOD__);
-        
+
         return $entity;
     }
 
     /**
      * getModified function.
-     * 
      * @access public
      * @param mixed $from
      * @param mixed $to
@@ -163,14 +155,13 @@ class PersonManager extends CoreManager
             }
             $this->update($entity);
         }
-        
+
         // end timer
         $this->end(__METHOD__);
     }
 
     /**
      * getRemoved function.
-     * 
      * @access public
      * @param mixed $from
      * @param mixed $to
@@ -180,27 +171,26 @@ class PersonManager extends CoreManager
     {
         $this->wsMethod = 'GetRemovedPersons';
         $this->wsResultKey = 'GetRemovedPersonsResult';
-         
+
         // start timer
         $this->start(__METHOD__);
 
         // call the ws
         $result = $this->soapCall($this->wsMethod, array('fromTimeStamp' => $from, 'toTimeStamp' => $to), false);
         $resultObjects = $this->mixedToArray($result->{$this->wsResultKey}->Resultats);
-        
+
         // delete objects with duplicate check
         $this->deleteMultiple($resultObjects, true);
-        
+
         // save entities
         //$this->em->flush();
-        
+
         // end timer
         $this->end(__METHOD__);
     }
 
     /**
      * set function.
-     * 
      * @access private
      * @param mixed $resultObject
      * @param mixed $result
@@ -210,26 +200,26 @@ class PersonManager extends CoreManager
     {
         // create / get entity
         $entity = ($this->findOneById(array('id' => $resultObject->{$this->entityIdKey}))) ?: new FilmPerson();
-        
+
         // set soif last update time
         $this->setSoifUpdatedAt($result, $entity);
-        
+
         // set entity properties
         $this->setEntityProperties($resultObject, $entity);
-        
+
         // set nationality
         $country = $this->em->getRepository('BaseCoreBundle:Country')->findOneByIso($resultObject->Nationalite);
         if ($country === null) {
-            $msg = __METHOD__. "Country {$resultObject->Nationalite} not found";
+            $msg = __METHOD__ . "Country {$resultObject->Nationalite} not found";
             $this->logger->warn($msg);
         } else {
             $entity->setNationality($country);
         }
-        
+
         // set nationality2
         $country = $this->em->getRepository('BaseCoreBundle:Country')->findOneByIso($resultObject->NationaliteSecondaire);
         if ($country === null) {
-            $msg = __METHOD__. "Country {$resultObject->NationaliteSecondaire} not found";
+            $msg = __METHOD__ . "Country {$resultObject->NationaliteSecondaire} not found";
             $this->logger->warn($msg);
         } else {
             $entity->setNationality2($country);
@@ -244,8 +234,9 @@ class PersonManager extends CoreManager
                 // find if filmMedia already exists
                 $entityRelated = $this->em->getRepository('BaseCoreBundle:FilmPersonMedia')->findOneBy(array(
                     'person' => $entity,
-                    'media' => $filmPersonMedia->Id
-                ));
+                    'media'  => $filmPersonMedia->Id,
+                ))
+                ;
                 $entityRelated = ($entityRelated !== null) ? $entityRelated : new FilmPersonMedia();
                 $entityRelated->setFilename($filmPersonMedia->FileName);
                 $entityRelated->setType($filmPersonMedia->IdType);
@@ -270,51 +261,54 @@ class PersonManager extends CoreManager
             $collectionFunctions = new ArrayCollection();
             foreach ($resultObject->PersonneFilms->PersonneFilmDto as $obj) {
                 $entityRelated = $this->em->getRepository('BaseCoreBundle:FilmFilmPerson')->findOneBy(array(
-                    'film' => $obj->IdFilm,
+                    'film'   => $obj->IdFilm,
                     'person' => $entity->getId(),
-                ));
+                ))
+                ;
                 $entityRelated = ($entityRelated !== null) ? $entityRelated : new FilmFilmPerson();
-                
+
                 $film = $this->em->getRepository('BaseCoreBundle:FilmFilm')->findOneById(array('id' => $obj->IdFilm));
                 if ($film === null) {
                     $this->filmsNotImported[] = $obj->IdFilm;
-                    $msg = __METHOD__. " Film {$obj->IdFilm} not found, call php app/console base:soif:get_film {$obj->IdFilm} to import it";
+                    $msg = __METHOD__ . " Film {$obj->IdFilm} not found, call php app/console base:soif:get_film {$obj->IdFilm} to import it";
                     $this->logger->warn($msg);
                     continue;
                 }
                 $entityRelated->setFilm($film);
                 // set functions
                 if (isset($obj->FonctionsTraductions) &&
-                    isset($obj->FonctionsTraductions->FonctionTraductionDto)  &&
-                    count($obj->FonctionsTraductions->FonctionTraductionDto) > 0) {
+                    isset($obj->FonctionsTraductions->FonctionTraductionDto) &&
+                    count($obj->FonctionsTraductions->FonctionTraductionDto) > 0
+                ) {
                     $entityRelatedFunction = $this->em->getRepository('BaseCoreBundle:FilmFilmPersonFunction')->findOneBy(array(
                         'filmPerson' => $entityRelated->getId(),
-                        'function' => $obj->IdFonction,
-                    ));
+                        'function'   => $obj->IdFonction,
+                    ))
+                    ;
                     $entityRelatedFunction = ($entityRelatedFunction !== null) ? $entityRelatedFunction : new FilmFilmPersonFunction();
                     $function = $this->em->getRepository('BaseCoreBundle:FilmFunction')->findOneById(array('id' => $obj->IdFonction));
                     if ($function === null) {
                         $function = new FilmFunction();
                     }
                     $function->setId($obj->IdFonction);
-                    
+
                     // loop through translations
                     $entityTranslation = array();
                     $translations = $obj->FonctionsTraductions->FonctionTraductionDto;
                     foreach ($translations as $translation) {
                         if (!isset($localesMapper[$translation->CodeLangue])) {
-                            $this->logger->warning(__METHOD__. " the locales mapper {$translation->CodeLangue} doesn't exist");
+                            $this->logger->warning(__METHOD__ . " the locales mapper {$translation->CodeLangue} doesn't exist");
                             continue;
                         }
                         if (!isset($entityTranslation[$translation->CodeLangue])) {
                             $entityTranslation[$translation->CodeLangue] = $function->findTranslationByLocale($localesMapper[$translation->CodeLangue]);
                         }
-                        
+
                         // set translations
                         $entityTranslation[$translation->CodeLangue] = ($entityTranslation[$translation->CodeLangue] !== null) ? $entityTranslation[$translation->CodeLangue] : new FilmFunctionTranslation();
                         $entityTranslation[$translation->CodeLangue]->setName($translation->Libelle);
                         $entityTranslation[$translation->CodeLangue]->setLocale($localesMapper[$translation->CodeLangue]);
-                        
+
                         // if new entity add translation to parent
                         if ($entityTranslation[$translation->CodeLangue]->getId() === null) {
                             $function->addTranslation($entityTranslation[$translation->CodeLangue]);
@@ -328,7 +322,7 @@ class PersonManager extends CoreManager
                 $entity->addFilm($entityRelated);
                 // save in array all the entities
                 $collection->add($entityRelated);
-                
+
                 $this->update($entity);
             }
 
@@ -340,57 +334,61 @@ class PersonManager extends CoreManager
         if (property_exists($resultObject, 'LinkedDeletedPersonnes')) {
             $collection = new ArrayCollection();
             $duplicates = clone $entity->getDuplicates();
-            $objects = $this->mixedToArray($resultObject->LinkedDeletedPersonnes->LinkedDeletedPersonneDto);
-            $duplicateIds = array();
-            $duplicateSelfkits = array();
-            foreach ($objects as $obj) {
-                $duplicateIds[] = $obj->PersonneId;
-                $duplicateSelfkits[] = $obj->PersonneSelfkitId;
-            }
-            $entity->setDuplicateIds($duplicateIds);
-            $entity->setDuplicateSelfkits($duplicateSelfkits);
-            foreach ($objects as $obj) {
-                $person = $this->em->getRepository('BaseCoreBundle:FilmPerson')->find($obj->PersonneId);
-                if ($person !== null) {
-                    $person->setDuplicate(1);
-                    $person->setOwner($entity);
-                    $person->setSelfkit($obj->PersonneSelfkitId);
-                    $collection->add($person);
+            $linkedDeteletedPersonDto = @$resultObject->LinkedDeletedPersonnes->LinkedDeletedPersonneDto;
+            if (!$linkedDeteletedPersonDto) {
 
-                    /* link owner to the duplicate person films */
-                    foreach ($person->getFilms() as $filmFilmPersonDuplicate) {
-                        $filmDuplicate = $filmFilmPersonDuplicate->getFilm();
-                        $found = false;
-                        foreach ($entity->getFilms() as $filmFilmPersonOwner) {
-                            $filmOwner = $filmFilmPersonOwner->getFilm();
-                            if ($filmDuplicate->getId() == $filmOwner->getId()) {
-                                $found = true;
-                                break;
+                $objects = $this->mixedToArray($resultObject->LinkedDeletedPersonnes->LinkedDeletedPersonneDto);
+                $duplicateIds = array();
+                $duplicateSelfkits = array();
+                foreach ($objects as $obj) {
+                    $duplicateIds[] = $obj->PersonneId;
+                    $duplicateSelfkits[] = $obj->PersonneSelfkitId;
+                }
+                $entity->setDuplicateIds($duplicateIds);
+                $entity->setDuplicateSelfkits($duplicateSelfkits);
+                foreach ($objects as $obj) {
+                    $person = $this->em->getRepository('BaseCoreBundle:FilmPerson')->find($obj->PersonneId);
+                    if ($person !== null) {
+                        $person->setDuplicate(1);
+                        $person->setOwner($entity);
+                        $person->setSelfkit($obj->PersonneSelfkitId);
+                        $collection->add($person);
+
+                        /* link owner to the duplicate person films */
+                        foreach ($person->getFilms() as $filmFilmPersonDuplicate) {
+                            $filmDuplicate = $filmFilmPersonDuplicate->getFilm();
+                            $found = false;
+                            foreach ($entity->getFilms() as $filmFilmPersonOwner) {
+                                $filmOwner = $filmFilmPersonOwner->getFilm();
+                                if ($filmDuplicate->getId() == $filmOwner->getId()) {
+                                    $found = true;
+                                    break;
+                                }
+                            }
+
+                            if ($found == false) {
+                                $entity->addFilm($filmFilmPersonDuplicate);
                             }
                         }
 
-                        if ($found == false) {
-                            $entity->addFilm($filmFilmPersonDuplicate);
+                        if (!$entity->getDuplicates()->contains($person)) {
+                            $entity->addDuplicate($person);
                         }
                     }
-
-                    if (!$entity->getDuplicates()->contains($person)) {
-                        $entity->addDuplicate($person);
-                    }
                 }
+
+                //// unset old duplicates when we have data
+                //if ($duplicates->count() > 0) {
+                //    foreach ($duplicates as $duplicate) {
+                //        if (!$collection->contains($duplicate)) {
+                //            $duplicate->setDuplicate(false);
+                //            $duplicate->setOwner(null);
+                //            $entity->removeDuplicate($duplicate);
+                //        }
+                //    }
+                //}
+
             }
-
-            //// unset old duplicates when we have data
-            //if ($duplicates->count() > 0) {
-            //    foreach ($duplicates as $duplicate) {
-            //        if (!$collection->contains($duplicate)) {
-            //            $duplicate->setDuplicate(false);
-            //            $duplicate->setOwner(null);
-            //            $entity->removeDuplicate($duplicate);
-            //        }
-            //    }
-            //}
-
         }
 
         /*if (count($this->getFilmsNotImported()) > 0) {
