2d478c06fdf195fd7bfa52371d7aff6434689bd5
axcff <axel.constantinoff@gmail.com>
2016-12-05 17:40:53 +0100

    5/12

---
+++ _html/ccm-plan-du-site.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <?php include('head.html'); ?>
+    <style type="text/css">
+
+    </style>
+  </head>
+
+  <body>
+    <?php include('header.html'); ?>
+
+    <div id="main" class="sitemap loading">
+    <h2 class="title">Plan du site</h2>
+    <section class="sitemap-container">
+        <article class="sitemap-item">
+            <h3 class="title sitemap-item-title">L'actualité</h3>
+            <ul>
+              <li><a href="#">Jour après jour</a></li>
+              <li><a href="article.php">Articles</a></li>
+              <li><a href="photos.php">Photos</a></li>
+              <li><a href="videos.php">Vidéos</a></li>
+              <li><a href="audios.php">Audios</a></li>
+            </ul>
+        </article>
+        <article class="sitemap-item">
+            <h3 class="title sitemap-item-title">Web tv</h3>
+            <ul>
+              <li><a href="webtv.php">En direct</a></li>
+              <li><a href="channels.ph">Chaînes</a></li>
+              <li><a href="webtv_trailer_competition.php">Bandes annonces</a></li>  
+            </ul>
+        </article>
+        <article class="sitemap-item">
+            <h3 class="title sitemap-item-title">La Sélection</h3>
+            <ul>
+                <li><a href="selectionofficielle_competition.php">En Compétition</a></li>
+                <li><a href="selectionofficielle_uncertainregard.php">Un Certain Regard</a></li>
+                <li><a href="selectionofficielle_horscompetition.php">Hors Compétition</a></li>
+                <li><a href="selectionofficielle_seancespeciales.php">Séances Spéciales</a></li>
+                <li><a href="selectionofficielle_cinefondation.php">Cinéfondation</a></li>
+                <li><a href="selectionofficielle_courtsmetrages.php">Courts Métrages</a></li>
+                <li><a href="films_invitedhonneur.php">Cannes Classics</a></li>
+                <li><a href="films_cinemadelaplage.php">Cinéma de la Plage</a></li>
+            </ul>
+        </article>
+        <article class="sitemap-item">
+            <h3 class="title sitemap-item-title">Les Jurys</h3>
+            <ul>
+              <li><a href="jury_longsmetrages.php">Longs métrages</a></li>
+              <li><a href="jury_cinefondation_courtsmetrages.php">Cinéfondation & Courts Métrages</a></li>
+              <li><a href="jury_uncertainregard.php">Un certain regard</a></li>
+              <li><a href="jury_camerador.php">Caméra d'or</a></li>  
+            </ul>
+        </article>
+        <article class="sitemap-item">
+            <h3 class="title sitemap-item-title">Le palmarès</h3>
+            <ul>
+               <li><a href="palmares_competition.php">Compétition</a></li>
+               <li><a href="palmares_uncertainregard.php">Un certain regard</a></li>
+               <li><a href="palmares_cinefondation.php">Cinéfondation</a></li>
+               <li><a href="palmares_camerador.php">Caméra d'or</a></li>
+               <li><a href="palmares_toutlepalmares.php">Tout le palmarès</a></li>
+            </ul>
+        </article>
+        <article class="sitemap-item">
+            <h3 class="title sitemap-item-title">Les évènements</h3>
+            <ul>
+               <li><a href="#">Leçons</a></li>
+               <li><a href="#">Expositions</a></li>
+               <li><a href="#">Rendez-vous Européen</a></li>
+               <li><a href="#">Leçons de musique</a></li>
+               <li><a href="#">Leçons de cinéma</a></li>
+               <li><a href="#">Leçons d'acteur</a></li>
+               <li><a href="#">Hommages</a></li>
+            </ul>
+        </article>
+        <article class="sitemap-item">
+            <h3 class="title sitemap-item-title">Participer</h3>
+            <ul>
+                <li>
+                    <a href="participer_sejour.php">Préparer son séjour</a>
+                </li>
+                <li>
+                    <a href="participer_modeemploi.php">Festival mode d'emploi</a>
+                </li>
+                <li>
+                    <a href="participer_preparer.php">Accès aux projections</a>
+                </li>
+                <li>
+                    <a href="service_partenaires.php">Partenaires</a>
+                </li>
+                <li>
+                    <a href="service_fournisseurs.php">Fournisseurs</a>
+                </li>
+            </ul>
+        </article>
+        <article class="sitemap-item">
+            <h3 class="title sitemap-item-title">Espace presse</h3>
+            <ul>
+                <li>
+                    <a href="#">Accueil</a>
+                </li>
+                <li>
+                    <a href="#">Communiqués / Infos</a>
+                </li>
+                <li>
+                    <a href="#">S'accréditer</a>
+                </li>
+                <li>
+                    <a href="#">Guide pratique</a>
+                </li>
+                <li>
+                    <a href="#">Programmation</a>
+                </li>
+                <li>
+                    <a href="#">Médiathèque</a>
+                </li>
+            </ul>
+        </article>
+    </section>
+    </div>
+    <?php include('footer.html'); ?>
+
+    <!-- //// SCRIPTS \\\\ -->
+    <?php include('scripts.inc.php'); ?>
+  </body>
+</html>
