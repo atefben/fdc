2301192262725e7952f6f8021c61cba06aed578b
Pierre BELLENGER <p.bellenger@ohwee.fr>
2016-12-05 11:33:38 +0100

    fix admin

--- src/Base/AdminBundle/Admin/HomepageCorporateAdmin.php
+++ src/Base/AdminBundle/Admin/HomepageCorporateAdmin.php
@@ -279,7 +279,7 @@ class HomepageCorporateAdmin extends Admin
             ))
             ->add('pushEditionImage', 'sonata_type_model_list', array(
                 'label' => 'form.label_image_push',
-                'help' => 'form.homepage.helper_pushes',
+                'help' => 'form.homepage.helper_prefooter',
                 'required' => false,
             ))
             ->add('pushMainImage1', 'sonata_type_model_list', array(
