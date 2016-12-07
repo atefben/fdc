ca482ce6d31167419459e7245610bfe6df4c2b7b
axcff <axel.constantinoff@gmail.com>
2016-12-07 17:47:18 +0100

    7/12

--- _html/ccm-palmares-competition.php
+++ _html/ccm-palmares-competition.php
@@ -92,9 +92,9 @@
                     <i class="icon icon_palme palme-dor-img"></i>
                     <strong class="palme-dor-text verticalline">Prix d'interprétation féminine ex-aequo</strong>
                     <!--<img src="img/palmares/palmedor-compet-portait.jpg" alt="palme d or" class="palme-dor-portrait">-->
-                    <a href=""><h3 class="title-item verticalline">Emmanuelle bercot</h3></a>
+                    <a href=""><h3 class="title-item verticalline">Emmanuelle bercot</h3>
                     <h3 class="title-item">Mon roi</h3>
-                    <span class="nom-item">Maïwenn</span>
+                    <span class="nom-item">Maïwenn</span></a>
                   </div>
                 </div>
                </div>
