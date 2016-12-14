9bf2f0eb2967e753733abb2ed4fca85522443a26
Pierre BELLENGER <p.bellenger@ohwee.fr>
2016-12-14 16:55:26 +0100

    remove dump

--- src/FDC/CorporateBundle/Controller/NewsController.php
+++ src/FDC/CorporateBundle/Controller/NewsController.php
@@ -51,7 +51,7 @@ class NewsController extends Controller
         $statementArticles = $em->getRepository('BaseCoreBundle:Statement')->getStatementRetrospective($locale, $festival->getId(),$festival->getFestivalStartsAt(),$festival->getFestivalEndsAt());
         $infoArticles = $em->getRepository('BaseCoreBundle:Info')->getInfoRetrospective($locale, $festival->getId(),$festival->getFestivalStartsAt(),$festival->getFestivalEndsAt());
         $newsArticles = array_merge($newsArticles, $statementArticles, $infoArticles);
-        dump($newsArticles);exit;
+
         $newsArticles = $this->removeUnpublishedNewsAudioVideo($newsArticles, $locale, null, true);
         if ($newsArticles === null || count($newsArticles) == 0) {
             throw new NotFoundHttpException();
