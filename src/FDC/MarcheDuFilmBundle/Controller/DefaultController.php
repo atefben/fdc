7041aa9cabd2be40717d841bacdfe478fc56320a
Luca Vlad <vlad.luca@pitechnologies.ro>
2016-12-15 13:33:59 +0200

    Static homepage template.

--- src/FDC/MarcheDuFilmBundle/Controller/DefaultController.php
+++ src/FDC/MarcheDuFilmBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('FDCMarcheDuFilmBundle::base.html.twig');
+        return $this->render('FDCMarcheDuFilmBundle::homepage/homepage.html.twig');
     }
 }
