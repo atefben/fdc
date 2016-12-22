5e9376e5371ccda6793616cd8e11cb044bb3272d
paul.pop <paul.pop@pitechnologies.ro>
2016-12-22 17:12:18 +0200

    Added services to news and dipatch de service widget entities.

--- src/FDC/MarcheDuFilmBundle/Form/Type/DispatchDeServiceWidgetType.php
+++ src/FDC/MarcheDuFilmBundle/Form/Type/DispatchDeServiceWidgetType.php
@@ -5,6 +5,7 @@ namespace FDC\MarcheDuFilmBundle\Form\Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Base\AdminBundle\Admin\MediaMdfImageAdmin;
 
 class DispatchDeServiceWidgetType extends AbstractType
 {
@@ -12,6 +13,33 @@ class DispatchDeServiceWidgetType extends AbstractType
      * @var string
      */
     protected $dataClass = 'FDC\MarcheDuFilmBundle\Entity\DispatchDeServiceWidget';
+
+    /**
+     * admin
+     *
+     * @var mixed
+     * @access private
+     */
+    private $admin;
+    /**
+     * @var MediaMdfImageAdmin
+     */
+    private $mediaImageAdmin;
+    /**
+     * setSonataAdmin function.
+     *
+     * @access public
+     * @param mixed $admin
+     * @return void
+     */
+    public function setSonataAdmin($admin)
+    {
+        $this->admin = $admin;
+    }
+    public function setMediaImageAdmin($mediaImageAdmin)
+    {
+        $this->mediaImageAdmin = $mediaImageAdmin;
+    }
 
     /**
      * @param FormBuilderInterface $builder
@@ -55,7 +83,17 @@ class DispatchDeServiceWidgetType extends AbstractType
                         'translation_domain' => 'BaseAdminBundle',
                     ),
                 ),
+            ))
+            ->add('image', 'sonata_type_model_list', array(
+                'label' => 'form.label_header_image',
+                'sonata_field_description' =>  $this->admin->getFormFieldDescriptions()['image'],
+                'model_manager' => $this->mediaImageAdmin->getModelManager(),
+                'class' => $this->mediaImageAdmin->getClass(),
+                'translation_domain' => 'BaseAdminBundle',
+                'btn_delete' => false,
+                'required' => false
             ));
+        ;
     }
 
     /**
