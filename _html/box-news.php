5e96dcdf2a951c08bf48ec22ab62902b01aef06e
axcff <axel.constantinoff@gmail.com>
2017-01-05 18:38:22 +0100

    5/1

--- _html/box-news.php
+++ _html/box-news.php
@@ -3,31 +3,34 @@
 <span>DECOUVREZ</span>
 <h3>les derniÈrEs actualités</h3>
 <div class="innerWrapper">
-  <div class="articles">
-    <div class="readMore">...</div>
-    <div class="articles-content">
-      <span class="customClass4">producers workshop <span class="article-date">26 jun 2016</span></span>
-      <h6>Participez aux conférences NEXT dédiées au futur du cinéma</h6>
-      <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans la tête d’un androïde à qui on a implanté la mémoire d'un humain. La réalité virtuelle, qui permet au spectateur d'un film d'en être aussi l'acteur, est une star à Cannes Vous entrez dans la peau d'un lapin bataillant contre des alien</p>
+  <a href="accreditations.php">
+    <div class="articles">
+      <div class="readMore">...</div>
+      <div class="articles-content">
+        <span class="customClass4">producers workshop <span class="article-date">26 jun 2016</span></span>
+        <h6>Participez aux conférences NEXT dédiées au futur du cinéma</h6>
+        <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans la tête d’un androïde à qui on a implanté la mémoire d'un humain. La réalité virtuelle, qui permet au spectateur d'un film d'en être aussi l'acteur, est une star à Cannes Vous entrez dans la peau d'un lapin bataillant contre des alien</p>
+      </div>
+      
     </div>
-    
-  </div>
+  </a>
   <div class="articles">
     <div class="readMore">...</div>
     <div class="articles-img"><img src="/img/slider/slider03.jpg"></div>
     <div class="articles-content">
       <span class="customClass4">producers workshop <span class="article-date">26 jun 2016</span></span>
-      <h6>Participez aux conférences NEXT dédiées au futur du cinéma, Participez aux conférences NEXT dédiées au futur du cinéma</h6>
-      <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dansu dans</p>
+      <a href="accreditations.php"><h6>Participez aux conférences NEXT dédiées au futur du cinéma, Participez aux conférences NEXT dédiées au futur du cinéma</h6>
+      <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dansu dans</p></a>
     </div>
   </div>
   <div class="articles noShow">
+    <a href="accreditations.php">
     <div class="readMore">...</div>
     <div class="articles-content">
       <span class="customClass4">producers workshop <span class="article-date">26 jun 2016</span></span>
       <h6>Participez aux conférences NEXT dédiées au futur du cinéma</h6>
-      <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans la tête d’un androïde à qui on a implanté la mémoire d'un humain. La réalité virtuelle, qui permet au spectateur d'un film d'en être aussi l'acteur, est une star à Cannes</p>
-    </div>
+     <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans la tête d’un androïde à qui on a implanté la mémoire d'un humain. La réalité virtuelle, qui permet au spectateur d'un film d'en être aussi l'acteur, est une star à Cannes</p>
+    </a></div>
   </div>
 </div>
 </div>
