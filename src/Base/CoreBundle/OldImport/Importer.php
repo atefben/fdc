4451cd657733e736578dc5b32c729086e4e54580
Jean-Luc Petit <webpetit@gmail.com>
2017-01-06 11:31:48 +0100

    Remove local video file after media video persist and flush

--- src/Base/CoreBundle/OldImport/Importer.php
+++ src/Base/CoreBundle/OldImport/Importer.php
@@ -799,7 +799,7 @@ class Importer
         }
         $this->setMediaVideoFilmFilmAssociations($mediaVideo);
         $this->getManager()->flush();
-
+        unset($file);
         return $mediaVideo;
     }
 
