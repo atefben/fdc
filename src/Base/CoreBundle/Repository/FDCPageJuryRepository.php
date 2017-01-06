a848373824f484508ef60801b489f1b427003899
Jean-Luc Petit <webpetit@gmail.com>
2017-01-06 11:26:35 +0100

    Update comments and reduce code

--- src/Base/CoreBundle/Repository/FDCPageJuryRepository.php
+++ src/Base/CoreBundle/Repository/FDCPageJuryRepository.php
@@ -7,44 +7,48 @@ use Doctrine\ORM\NonUniqueResultException;
 
 
 /**
- *
+ * Class FDCPageJuryRepository
+ * @package Base\CoreBundle\Repository
  */
 class FDCPageJuryRepository extends TranslationRepository
 {
+
     /**
-     * @param $locale
-     * @param $slug
+     * @param string $locale
+     * @param string $slug
      * @return FDCPageJury|null
      * @throws NonUniqueResultException
      */
     public function getPageBySlug($locale, $slug)
     {
-        $qb = $this->createQueryBuilder('p');
-
-        $qb
+        return $this
+            ->createQueryBuilder('p')
             ->join('p.translations', 't')
             ->andWhere('t.locale = :locale')
             ->andWhere('t.slug = :slug')
             ->andWhere('p.image IS NOT NULL')
             ->setParameter('locale', $locale)
             ->setParameter('slug', $slug)
+            ->getQuery()
+            ->getOneOrNullResult()
         ;
-
-        return $qb->getQuery()->getOneOrNullResult();
     }
 
+    /**
+     * @param string $locale
+     * @return FDCPageJury[]
+     */
     public function getPages($locale)
     {
-        $qb = $this->createQueryBuilder('p');
-
-        $qb
+        return $this
+            ->createQueryBuilder('p')
             ->join('p.translations', 't')
             ->andWhere('t.locale = :locale AND t.slug IS NOT NULL AND t.slug <> \'\'')
             ->andWhere('p.image IS NOT NULL')
             ->setParameter('locale', $locale)
             ->orderBy('p.id', 'asc')
+            ->getQuery()
+            ->getResult()
         ;
-
-        return $qb->getQuery()->getResult();
     }
 }
