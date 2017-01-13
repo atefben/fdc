4d573d17463a10b25516ef6eb792fcf113a58775
Luca Vlad <vlad.luca@pitechnologies.ro>
2017-01-13 11:06:41 +0200

    Refactoring.

--- src/Base/AdminBundle/Admin/MdfConferenceProgramAdmin.php
+++ src/Base/AdminBundle/Admin/MdfConferenceProgramAdmin.php
@@ -102,10 +102,10 @@ class MdfConferenceProgramAdmin extends Admin
                 'label'              => 'form.mdf.label.new_program_day',
                 'translation_domain' => 'BaseAdminBundle',
             ), array(
-                      'edit'     => 'inline',
-                      'inline'   => 'table',
-                      'sortable' => 'position',
-                  ))
+                'edit'     => 'inline',
+                'inline'   => 'table',
+                'sortable' => 'position',
+            ))
         ;
 
     }
