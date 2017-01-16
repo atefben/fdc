0b7c93b5468679b44cf2b0d0d41b00808095e59a
kevtut <k.tutala@ohwee.fr>
2017-01-16 14:43:49 +0100

    #5049 Bouton afficher les infos qui mène vers une erreur
    Pour répliquer ce qui se passe dans le site event sur le site corporate,
    j'ai créer l'action getArticlesFromAction dans DefaultController.php
    ainsi que les fichiers twigs article-home-ajax, article-home,
    slideshow-actu-home. Je pense que la différence entre les deux sites est
    qu'il n'y an pas d'infos côté corporate.

--- src/FDC/CorporateBundle/Controller/DefaultController.php
+++ src/FDC/CorporateBundle/Controller/DefaultController.php
@@ -134,4 +134,122 @@ class DefaultController extends Controller
             'filters'            => $filters
         );
     }
+    
+    /**
+     * @Route("/home-more-infos")
+     * @Template("FDCCorporateBundle:News:widgets/article-home-ajax.html.twig")
+     * @param Request $request
+     * @return array
+     */
+    public function getArticlesFromAction(Request $request)
+    {
+
+        $timestamp = $request->query->get('timestamp') - 1;
+        $nextDay = $request->query->get('end');
+        $type = $request->query->get('type');
+
+        $em = $this->get('doctrine')->getManager();
+        $locale = $request->getLocale();
+
+        // GET HOMEPAGE SETTINGS
+        $homepage = $em->getRepository('BaseCoreBundle:Homepage')->findOneBy(array(
+            'festival' => $this->getFestival(),
+        )) 
+       ;
+
+        
+        $date = new DateTime();
+        $dateTime = $date->setTimestamp($timestamp);
+        $count = 7;
+        $countNext = 2;
+
+        $endOfArticles = false;
+        $homeArticlesNext = false;
+
+        if ($nextDay == 1) {
+            $dateTime = $dateTime->modify('-1 day');
+            if ($homepage->getTopNewsType() == false) {
+                $homeArticles = $em->getRepository('BaseCoreBundle:News')->getOlderNewsButSameDay($locale, $this->getFestival()->getId(), $dateTime, $count);
+                $homeArticles = $this->removeUnpublishedNewsAudioVideo($homeArticles, $locale, $count);
+            } else {
+                $homeInfos = $em->getRepository('BaseCoreBundle:Info')->getInfosByDate($locale, $this->getFestival()->getId(), $dateTime, $count);
+                $homeStatement = $em->getRepository('BaseCoreBundle:Statement')->getStatementByDate($locale, $this->getFestival()->getId(), $dateTime, $count);
+                $homeArticles = $em->getRepository('BaseCoreBundle:News')->getNewsByDate($locale, $this->getFestival()->getId(), $dateTime, $count);
+
+                $homeArticles = array_merge($homeInfos, $homeStatement, $homeArticles);
+                $homeArticles = $this->removeUnpublishedNewsAudioVideo($homeArticles, $locale, $count);
+            }
+            $homeArticles = $this->removeUnpublishedNewsAudioVideo($homeArticles, $locale, $count);
+
+        } else {
+            if ($homepage->getTopNewsType() == false) {
+                $homeArticles = $em->getRepository('BaseCoreBundle:News')->getOlderNewsButSameDay($locale, $this->getFestival()->getId(), $dateTime, $count);
+                $homeArticles = $this->removeUnpublishedNewsAudioVideo($homeArticles, $locale, $count);
+            } else {
+                $homeInfos = $em->getRepository('BaseCoreBundle:Info')->getInfosByDate($locale, $this->getFestival()->getId(), $dateTime, $count);
+                $homeStatement = $em->getRepository('BaseCoreBundle:Statement')->getStatementByDate($locale, $this->getFestival()->getId(), $dateTime, $count);
+                $homeArticles = $em->getRepository('BaseCoreBundle:News')->getNewsByDate($locale, $this->getFestival()->getId(), $dateTime, $count);
+
+
+                $homeArticles = array_merge($homeInfos, $homeStatement, $homeArticles);
+                $homeArticles = $this->removeUnpublishedNewsAudioVideo($homeArticles, $locale, $count);
+            }
+        }
+
+        if (sizeof($homeArticles) < $count || $homeArticles == null) {
+            $endOfArticles = true;
+            if ($homepage->getTopNewsType() == false) {
+                $dateTimeNext = $dateTime->modify('-1 day');
+                $homeArticlesNext = $em->getRepository('BaseCoreBundle:News')->getNewsByDate($locale, $this->getFestival()->getId(), $dateTimeNext, $countNext);
+            } else {
+                $homeArticlesNext = array();
+            }
+            if ($homeArticles != null) {
+                $homeArticlesNext = $this->removeUnpublishedNewsAudioVideo($homeArticlesNext, $locale, $countNext);
+            }
+        }
+
+        //get images for slider articles
+        if (!isset($nextDay)) {
+            $homeArticlesSlider = $em->getRepository('BaseCoreBundle:Media')->getImageMediaByDay($locale, $this->getFestival()->getId(), $date->setTimestamp($timestamp));
+        } else {
+            $homeArticlesSlider = null;
+        }
+
+        //set default filters
+        $filters = array();
+        $filters['format'][0] = 'all';
+        $filters['themes']['content'][0] = 'all';
+        $filters['themes']['id'][0] = 'all';
+
+        foreach ($homeArticles as $key => $homeArticle) {
+            $homeArticle->setTheme($homeArticle->getTheme());
+
+            if (($key % 3) == 0) {
+                $homeArticle->double = true;
+            }
+
+            if (!in_array($homeArticle->getTheme()->getId(), $filters['themes']['id'])) {
+                $filters['themes']['id'][] = $homeArticle->getTheme()->getId();
+                $filters['themes']['content'][] = $homeArticle->getTheme();
+            }
+
+            if (!in_array($homeArticle->getNewsType(), $filters['format'])) {
+                $filters['format'][] = $homeArticle->getNewsType();
+            }
+        }
+        
+
+        if (count($homeArticles) > 6) {
+            unset($homeArticles[6]);
+        }
+
+        return array(
+            'homeArticlesSlider' => $homeArticlesSlider,
+            'endOfArticles'      => $endOfArticles,
+            'homeArticles'       => $homeArticles,
+            'homeArticlesNext'   => $homeArticlesNext,
+            'filters'            => $filters,
+        );
+    }
 }
