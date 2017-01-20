060570e4da0c8f647b2f2e6af30b04c29da6d4e1
Jean-Luc Petit <webpetit@gmail.com>
2017-01-20 17:25:14 +0100

    Add fix festival command

---
+++ src/Base/CoreBundle/Command/FixFestivalCommand.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Base\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FixFestivalCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('base:fix:festival')
+            ->addArgument('type', InputArgument::REQUIRED)
+            ->addOption('page', null, InputOption::VALUE_OPTIONAL)
+            ->addOption('count', null, InputOption::VALUE_NONE)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $page = $input->getOption('page')?:1;
+
+        if ($input->getOption('count')) {
+            $count = $this
+                ->getManager()
+                ->getRepository('BaseCoreBundle:' . ucfirst($input->getArgument('type')))
+                ->createQueryBuilder('n')
+                ->select('count(n)')
+                ->andWhere('n.oldNewsId is not null')
+                ->getQuery()
+                ->getOneOrNullResult()
+            ;
+            dump($count);
+            return null;
+        }
+
+        $items = $this
+            ->getManager()
+            ->getRepository('BaseCoreBundle:' . ucfirst($input->getArgument('type')))
+            ->createQueryBuilder('n')
+            ->andWhere('n.oldNewsId is not null')
+            ->setMaxResults(100)
+            ->setFirstResult(($page -1) * 100)
+            ->getQuery()
+            ->getResult()
+        ;
+
+
+        $bar = new ProgressBar($output, count($items));
+        $bar->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%');
+        $bar->start();
+
+        foreach ($items as $item) {
+            $bar->advance();
+            $this->fixItem($item);
+        }
+        $bar->finish();
+        $output->writeln('');
+
+
+    }
+
+    protected function fixItem($item)
+    {
+        $year = $item->getPublishedAt()->format('Y');
+        if ($item->getFestival()->getYear() != $year) {
+            $item->setFestival($this->getFestivalByYear($year));
+            $this->getManager()->flush();
+        }
+    }
+
+    /**
+     * @param $year
+     * @return \Base\CoreBundle\Entity\FilmFestival|null|object
+     */
+    protected function getFestivalByYear($year)
+    {
+        return $this
+            ->getManager()
+            ->getRepository('BaseCoreBundle:FilmFestival')
+            ->findOneBy(['year' => $year])
+            ;
+    }
+
+
+    protected function getManager()
+    {
+        return $this->getContainer()->get('doctrine')->getManager();
+    }
+}
