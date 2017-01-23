9255e7032b8138d5bbfa76a2b036a873bb53db70
paul.pop <paul.pop@pitechnologies.ro>
2017-01-23 10:31:05 +0200

    Added news for conference.

---
+++ src/Base/AdminBundle/Admin/MdfConferenceNewsPageProducersNetworkAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Base\AdminBundle\Admin;
+
+use FDC\MarcheDuFilmBundle\Entity\MdfConferenceProgram;
+
+class MdfConferenceNewsPageProducersNetworkAdmin extends MdfConferenceNewsPageMain
+{
+    protected $baseRoutePattern = 'mdfconferencenewspagepn';
+    protected $baseRouteName = 'mdf_conference_news_page_pn';
+
+    public function createQuery($context = 'list')
+    {
+        $query = parent::createQuery($context);
+        $query->andWhere(
+            $query->expr()->eq($query->getRootAlias().'.type', ':type')
+        );
+        $query->setParameter('type', MdfConferenceProgram::TYPE_PRODUCERS_NETWORK);
+
+        return $query;
+    }
+
+    public function prePersist($page)
+    {
+        parent::prePersist($page);
+        $page->setType(MdfConferenceProgram::TYPE_PRODUCERS_NETWORK);
+    }
+}
---
+++ src/Base/AdminBundle/Controller/MdfConferenceNewsPageProducersNetworkAdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Base\AdminBundle\Controller;
+
+use Application\Sonata\AdminBundle\Controller\CRUDController;
+
+/**
+ * Class MdfConferenceNewsPageProducersNetworkAdminController
+ *
+ * @package Base\AdminBundle\Controller
+ */
+class MdfConferenceNewsPageProducersNetworkAdminController extends CRUDController
+{
+
+}
