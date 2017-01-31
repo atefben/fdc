fe7a975a110fba1182ae7dba80d752237c79f812
axcff <axel.constantinoff@gmail.com>
2017-01-31 11:48:13 +0100

    31/01

--- _html/box-news.php
+++ _html/box-news.php
@@ -14,23 +14,28 @@
       
     </div>
   </a>
+  
+  <a href="accreditations.php">
   <div class="articles">
     <div class="readMore">...</div>
     <div class="articles-img"><img src="/img/slider/slider03.jpg"></div>
     <div class="articles-content">
       <span class="customClass4">producers workshop <span class="article-date">26 jun 2016</span></span>
-      <a href="accreditations.php"><h6>Participez aux conférences NEXT dédiées au futur du cinéma, Participez aux conférences NEXT dédiées au futur du cinéma</h6>
-      <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dansu dans</p></a>
+      <h6>Participez aux conférences NEXT dédiées au futur du cinéma, Participez aux conférences NEXT dédiées au futur du cinéma</h6>
+      <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dansu dans</p>
     </div>
   </div>
+  </a>
+
+  <a href="accreditations.php">
   <div class="articles noShow">
-    <a href="accreditations.php">
     <div class="readMore">...</div>
     <div class="articles-content">
       <span class="customClass4">producers workshop <span class="article-date">26 jun 2016</span></span>
       <h6>Participez aux conférences NEXT dédiées au futur du cinéma</h6>
      <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans la tête d’un androïde à qui on a implanté la mémoire d'un humain. La réalité virtuelle, qui permet au spectateur d'un film d'en être aussi l'acteur, est une star à Cannes</p>
-    </a></div>
+    </div>
   </div>
+  </a>
 </div>
 </div>
