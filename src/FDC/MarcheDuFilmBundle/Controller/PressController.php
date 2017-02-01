5bb037f3f5be91a7e8773e828052b26eef86771a
Luca Vlad <vlad.luca@pitechnologies.ro>
2017-02-01 16:51:47 +0200

    Press controller.

---
+++ src/FDC/MarcheDuFilmBundle/Controller/PressController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FDC\MarcheDuFilmBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class PressController
+ *
+ * @package FDC\MarcheDuFilmBundle\Controller
+ */
+class PressController extends Controller
+{
+
+}
