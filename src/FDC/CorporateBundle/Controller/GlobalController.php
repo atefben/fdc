b82c4e1312d64d25991c7fb6c3522441f31a7334
Jean-Luc Petit <webpetit@gmail.com>
2017-02-02 10:22:59 +0100

    Optimize Gloabal Controller

--- src/FDC/CorporateBundle/Controller/GlobalController.php
+++ src/FDC/CorporateBundle/Controller/GlobalController.php
@@ -2,29 +2,24 @@
 
 namespace FDC\CorporateBundle\Controller;
 
-use Base\CoreBundle\Entity\FilmPerson;
-use Base\CoreBundle\Entity\Newsletter;
 use Base\CoreBundle\Interfaces\FDCEventRoutesInterface;
+use DateTime;
+use Exception;
 use FDC\EventBundle\Component\Controller\Controller;
+use FDC\EventBundle\Form\Type\ShareEmailType;
+use Guzzle\Http\Client;
+use Guzzle\Http\Exception\BadResponseException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use \DateTime;
-use \Exception;
-
-use FDC\EventBundle\Form\Type\ShareEmailType;
-use FDC\EventBundle\Form\Type\NewsletterType;
-use FDC\EventBundle\Form\Type\SearchType;
-
-use Guzzle\Http\Exception\BadResponseException;
-use Guzzle\Http\Client;
 
 /**
  * @Route("/")
  */
-class GlobalController extends Controller {
+class GlobalController extends Controller
+{
     /**
      * @Route("/share-email/", options={"expose"=true})
      * @Template("FDCCorporateBundle:Global:share-email.html.twig")
@@ -33,19 +28,23 @@ class GlobalController extends Controller {
      * @param $detail
      * @param $title
      * @param $description
-     * @return array
+     * @param $url
+     * @param $artist
+     * @param $color
+     * @return array|JsonResponse
      */
-    public function shareEmailAction(Request $request, $section = null, $detail = null, $title = null, $description = null, $url = null, $artist = null, $color = false) {
-        $email = array(
-            'section' => $section,
-            'detail' => $detail,
-            'title' => $title,
+    public function shareEmailAction(Request $request, $section = null, $detail = null, $title = null, $description = null, $url = null, $artist = null, $color = false)
+    {
+        $email = [
+            'section'     => $section,
+            'detail'      => $detail,
+            'title'       => $title,
             'description' => $description,
-            'url' => $url
-        );
+            'url'         => $url
+        ];
 
         $translator = $this->get('translator');
-        $hasErrors  = false;
+        $hasErrors = false;
 
         $form = $this->createForm(new ShareEmailType($translator, $artist));
         if ($request->isMethod('POST')) {
@@ -62,38 +61,51 @@ class GlobalController extends Controller {
             }
 
             if ($form->isValid()) {
-                foreach ($emails as $email){
-                    $data    = $form->getData();
-                    $artist = $request->get('artist');
+                foreach ($emails as $email) {
+                    $data = $form->getData();
+                    $artist = null;
+                    if ($request->get('artist')) {
+                        $artist = $this
+                            ->getDoctrineManager()
+                            ->getRepository('BaseCoreBundle:FilmPerson')
+                            ->find($request->get('artist'))
+                        ;
+                    }
                     $message = \Swift_Message::newInstance()
-                            ->setSubject($data['title'])
-                            ->setFrom($data['user'])->setTo($email)
-                            ->setBody($this->renderView('FDCCorporateBundle:Emails:share.html.twig', array(
-                        'message' => $data['message'],
-                        'section' => $data['section'],
-                        'title' => $data['title'],
-                        'description' => strip_tags($data['description'], '<p><em>'),
-                        'detail' => $data['detail'],
-                        'url' => $data['url'],
-                        'artist' => ($artist != null) ? $this->getDoctrineManager()->getRepository('BaseCoreBundle:FilmPerson')->find($artist) : null
-                    )), 'text/html');
-                    $mailer  = $this->get('mailer');
+                        ->setSubject($data['title'])
+                        ->setFrom($data['user'])->setTo($email)
+                        ->setBody($this->renderView('FDCCorporateBundle:Emails:share.html.twig', [
+                            'message'     => $data['message'],
+                            'section'     => $data['section'],
+                            'title'       => $data['title'],
+                            'description' => strip_tags($data['description'], '<p><em>'),
+                            'detail'      => $data['detail'],
+                            'url'         => $data['url'],
+                            'artist'      => $artist,
+                        ]), 'text/html')
+                    ;
+                    $mailer = $this->get('mailer');
                     $mailer->send($message);
 
                     $response['success'] = true;
 
                     //send mail copy
                     if ($data['copy']) {
-                        $message = \Swift_Message::newInstance()->setSubject($data['title'])->setFrom($data['user'])->setTo($data['user'])->setBody($this->renderView('FDCCorporateBundle:Emails:share.html.twig', array(
-                            'message' => $data['message'],
-                            'section' => $data['section'],
-                            'title' => $data['title'],
-                            'description' => strip_tags($data['description'], '<p><em>'),
-                            'detail' => $data['detail'],
-                            'url' => $data['url'],
-                            'artist' => ($artist != null) ? $this->getDoctrineManager()->getRepository('BaseCoreBundle:FilmPerson')->find($artist) : null
-                        )), 'text/html');
-                        $mailer  = $this->get('mailer');
+                        $message = \Swift_Message::newInstance()
+                            ->setSubject($data['title'])
+                            ->setFrom($data['user'])
+                            ->setTo($data['user'])
+                            ->setBody($this->renderView('FDCCorporateBundle:Emails:share.html.twig', [
+                                'message'     => $data['message'],
+                                'section'     => $data['section'],
+                                'title'       => $data['title'],
+                                'description' => strip_tags($data['description'], '<p><em>'),
+                                'detail'      => $data['detail'],
+                                'url'         => $data['url'],
+                                'artist'      => $artist,
+                            ]), 'text/html')
+                        ;
+                        $mailer = $this->get('mailer');
                         $mailer->send($message);
                     }
 
@@ -102,7 +114,7 @@ class GlobalController extends Controller {
                         $locale = ($request->getLocale() == 'fr') ? 'fr' : 'en';
                         $exist = $this->newsletterEmailExists($data['user']);
                         if ($exist == false) {
-                            $subscribed = $this->newsletterEmailSubscribe($data['email'], $locale);
+                            $this->newsletterEmailSubscribe($data['email'], $locale);
                         }
                     }
                 }
@@ -113,24 +125,24 @@ class GlobalController extends Controller {
         }
 
         if ($artist) {
-            return array(
+            return [
                 'share_email' => $email,
-                'form' => $form,
-                'hasErrors' => $hasErrors,
-                'artist' => $this->getDoctrineManager()->getRepository('BaseCoreBundle:FilmPerson')->find($artist),
-                'color'  => $color
-            );
+                'form'        => $form,
+                'hasErrors'   => $hasErrors,
+                'artist'      => $artist,
+                'color'       => $color
+            ];
         } else {
-            return array(
+            return [
                 'share_email' => $email,
-                'form' => $form,
-                'hasErrors' => $hasErrors,
-                'color'  => $color
-            );
+                'form'        => $form,
+                'hasErrors'   => $hasErrors,
+                'color'       => $color
+            ];
         }
 
     }
-    
+
     private function newsletterEmailExists($email)
     {
         $client = new Client($this->container->getParameter('fdc_newsletter_api_url'));
@@ -143,33 +155,34 @@ class GlobalController extends Controller {
         try {
             $request->send();
         } catch (BadResponseException $e) {
-            $this->get('logger')->err('Unable to verify if email exists in newsletter - '. $e->getMessage());
+            $this->get('logger')->err('Unable to verify if email exists in newsletter - ' . $e->getMessage());
             return false;
         } catch (Exception $e) {
-            $this->get('logger')->err('Unexpected error when verifying if email exists in newsletter - '. $e->getMessage());
+            $this->get('logger')->err('Unexpected error when verifying if email exists in newsletter - ' . $e->getMessage());
             return false;
         }
 
         return ($request->getResponse()->getStatusCode() === 200) ? true : false;
     }
 
-        private function newsletterEmailSubscribe($email, $locale) {
+    private function newsletterEmailSubscribe($email, $locale)
+    {
         $date = new DateTime();
         $client = new Client($this->container->getParameter('fdc_newsletter_api_url'));
-        $query = json_encode(array(
-            'email' => $email,
-            'lang' => $locale,
-            'date' => $date->format('Y-m-d H:i:s'),
+        $query = json_encode([
+            'email'     => $email,
+            'lang'      => $locale,
+            'date'      => $date->format('Y-m-d H:i:s'),
             'firstname' => '',
-            'lastname' => '',
-            'lists' => array(
-                array('id' => '0')
-            )
-        ));
+            'lastname'  => '',
+            'lists'     => [
+                ['id' => '0']
+            ]
+        ]);
         $request = $client->post('contact/', null, $query);
 
         $request->setAuth(
-                $this->container->getParameter('fdc_newsletter_api_universe'), $this->container->getParameter('fdc_newsletter_api_password')
+            $this->container->getParameter('fdc_newsletter_api_universe'), $this->container->getParameter('fdc_newsletter_api_password')
         );
 
         try {
@@ -190,17 +203,21 @@ class GlobalController extends Controller {
      * @Template("FDCCorporateBundle:Global:landing.html.twig")
      * @return array
      */
-    public function landingAction() {
-        $em = $this->get('doctrine')->getManager();
-        $homepage = $em->getRepository('BaseCoreBundle:HomepageCorporate')->find(1);
+    public function landingAction()
+    {
+        $homepage = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:HomepageCorporate')
+            ->find(1)
+        ;
         if ($homepage === null) {
-            throw new NotFoundHttpException();
+            throw $this->createNotFoundException();
         }
 
-        return array(
-            'date' => $homepage->getFestivalStartsAt(),
+        return [
+            'date'     => $homepage->getFestivalStartsAt(),
             'homepage' => $homepage
-        );
+        ];
     }
 
     /**
@@ -208,52 +225,66 @@ class GlobalController extends Controller {
      * @Template("FDCCorporateBundle:Global:timer.html.twig")
      * @return array
      */
-    public function timerAction() {
-        $em = $this->get('doctrine')->getManager();
-        $homepage = $em->getRepository('BaseCoreBundle:HomepageCorporate')->find(1);
+    public function timerAction()
+    {
+        $homepage = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:HomepageCorporate')
+            ->find(1)
+        ;
         if ($homepage === null) {
-            throw new NotFoundHttpException();
+            throw $this->createNotFoundException();
         }
 
-        return array(
+        return [
             'homepage' => $homepage
-        );
+        ];
     }
 
     /**
      * @Route("/menu")
      * @Template("FDCCorporateBundle:Global:nav.html.twig")
+     * @param $route
      * @return array
      */
-    public function menuAction($route) {
+    public function menuAction($route)
+    {
+        $menus = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:FDCEventRoutes')
+            ->childrenHierarchy()
+        ;
 
-        $em = $this->get('doctrine')->getManager();
-        $menus = $em->getRepository('BaseCoreBundle:FDCEventRoutes')->childrenHierarchy();
-
-        // Menu Participer
-        $participatePage = $em->getRepository('BaseCoreBundle:FDCPageParticipate')->findAll();
-        $preparePage = $em->getRepository('BaseCoreBundle:FDCPagePrepare')->findById($this->getParameter('admin_fdc_page_prepare_id'));
+        $participatePage = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:FDCPageParticipate')
+            ->findAll()
+        ;
+        $preparePage = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:FDCPagePrepare')
+            ->findBy(['id' => $this->getParameter('admin_fdc_page_prepare_id')])
+        ;
 
         $participateMenu = array_merge($preparePage, $participatePage);
 
-        $displayedMenus = array();
-        foreach($menus as $menu){
-            if($menu['site'] == FDCEventRoutesInterface::CORPO && $menu['type'] == 1) {
+        $displayedMenus = [];
+        foreach ($menus as $menu) {
+            if ($menu['site'] == FDCEventRoutesInterface::CORPO && $menu['type'] == 1) {
                 $displayedMenus[] = $menu;
             }
         }
 
-        usort($displayedMenus, function($a, $b) {
+        usort($displayedMenus, function ($a, $b) {
             if ($a["position"] == $b["position"]) {
                 return 0;
             }
             return ($a["position"] < $b["position"]) ? -1 : 1;
         });
 
-       // dump($displayedMenus);exit;
 
         foreach ($displayedMenus as $key => $menu) {
-            usort($displayedMenus[$key]['__children'], function($a, $b) {
+            usort($displayedMenus[$key]['__children'], function ($a, $b) {
                 if ($a["position"] == $b["position"]) {
                     return 0;
                 }
@@ -261,50 +292,53 @@ class GlobalController extends Controller {
             });
         }
 
-        $routesArticles = array(
+        $routesArticles = [
             'fdc_event_news_index',
             'fdc_event_news_get',
             'fdc_event_news_getarticles',
             'fdc_event_news_getphotos',
             'fdc_event_news_getvideos',
             'fdc_event_news_getaudios',
-        );
+        ];
 
-        $routesWebTv = array(
+        $routesWebTv = [
             'fdc_event_television_live',
             'fdc_event_television_channels',
             'fdc_event_television_getchannel',
             'fdc_event_television_gettrailer',
             'fdc_event_television_trailers'
-        );
+        ];
 
-        return array(
-            'menus' => $displayedMenus,
-            'routesArticles' => $routesArticles,
-            'routesWebTv' => $routesWebTv,
-            'route' => $route,
+        return [
+            'menus'           => $displayedMenus,
+            'routesArticles'  => $routesArticles,
+            'routesWebTv'     => $routesWebTv,
+            'route'           => $route,
             'participateMenu' => $participateMenu
-        );
-
+        ];
     }
 
     /**
      * @Route("/menu")
      * @Template("FDCCorporateBundle:Global:footer.html.twig")
+     * @param Request $request
+     * @param $route
      * @return array
      */
-    public function footerAction(Request $request, $route) {
-        $em = $this->get('doctrine')->getManager();
-        $displayedFooterElements = $em->getRepository('BaseCoreBundle:FDCEventRoutes')->findBy(
-            array('type' => 2,'site' => 3),
-            array('position' => 'asc'),
-            null,
-            null
-        );
-        return array(
+    public function footerAction(Request $request, $route)
+    {
+        $criteria = ['type' => 2, 'site' => 3];
+        $sort = ['position' => 'asc'];
+        $displayedFooterElements = $this
+            ->getDoctrineManager()
+            ->getRepository('BaseCoreBundle:FDCEventRoutes')
+            ->findBy($criteria, $sort)
+        ;
+
+        return [
             'footer' => $displayedFooterElements,
-            'route' => $route
-        );
+            'route'  => $route
+        ];
     }
 
 }
