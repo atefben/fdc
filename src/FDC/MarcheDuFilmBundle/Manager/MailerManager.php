801c45bf3588ed068364ce837a8d39b6e126ef09
Andrei Camarasan <andrei.camarasan@pitechnologies.ro>
2017-02-02 20:26:45 +0200

    [WIP]-#Issue-6766.Contac us page.

---
+++ src/FDC/MarcheDuFilmBundle/Manager/MailerManager.php
@@ -0,0 +1,78 @@
+<?php
+namespace FDC\MarcheDuFilmBundle\Manager;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+use Doctrine\ORM\EntityManager;
+use FDC\MarcheDuFilmBundle\Entity\MdfContactSubjectTranslation;
+
+class MailerManager
+{
+    /**
+     * @var
+     */
+    private $mailer;
+    /**
+     * @var
+     */
+    private $templating;
+    /**
+     * @var
+     */
+    private $translator;
+    /**
+     * @var EntityManager
+     */
+    private $em;
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * MailerManager constructor.
+     * @param $mailer
+     * @param $templating
+     * @param $translator
+     * @param EntityManager $entityManager
+     * @param RequestStack $requestStack
+     */
+    public function __construct($mailer, $templating, $translator, EntityManager $entityManager, RequestStack $requestStack)
+    {
+        $this->mailer = $mailer;
+        $this->templating = $templating;
+        $this->translator = $translator;
+        $this->em = $entityManager;
+        $this->requestStack = $requestStack;
+    }
+
+    public function sendMessage($emailData)
+    {
+        $theme = $this->em->getRepository(MdfContactSubjectTranslation::class)
+            ->findOneBy(
+                array(
+                    'slug' => $emailData['select'],
+                    'locale' => $this->requestStack->getMasterRequest()->get('_locale')
+                )
+            );
+
+        $message = \Swift_Message::newInstance();
+        $message
+            ->setSubject($emailData['object'])
+            ->setFrom($this->translator->trans('email.from'))
+            ->setTo($emailData['emailTo'])
+            ->setBody(
+                $this->templating->render(
+                    'FDCMarcheDuFilmBundle::emails/contact.html.twig',
+                    array(
+                        'message' => $emailData['message'],
+                        'theme' => $theme->getContactTheme(),
+                        'name' => $emailData['name'],
+                        'email' => $emailData['emailFrom']
+                    )
+                ),
+                'text/html'
+            );
+        
+        $this->mailer->send($message);
+    }
+}
