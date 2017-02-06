8e180377d455db784d9d180a6bd1792510151a76
Jacques tatiana <tatout08090@gmail.com>
2017-02-06 11:26:55 +0100

    #5262

--- _html_lot3/app/assets/js/files/grid.js
+++ _html_lot3/app/assets/js/files/grid.js
@@ -77,8 +77,12 @@ var owInitGrid = function (id) {
                     success: function(data) {
                         $data = $(data);
 
-                        $grid.append($data).isotope( 'addItems', $data );
+                        $grid.append($data);
 
+                        setTimeout(function(){
+                            $grid.isotope( 'addItems', $data )
+                        }, 1000);
+                        
                         $grid.isotope();
 
                         $('input[name="pg"]').val(parseInt($('input[name="pg"]').val())+1);
