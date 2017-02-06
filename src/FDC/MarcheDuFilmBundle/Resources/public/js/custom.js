2aee4621e8b184b68f1396160af6512f7944922c
Andrei Camarasan <andrei.camarasan@pitechnologies.ro>
2017-02-06 19:13:31 +0200

    #Issue-6396.Messages on global news translated.Messages on global events fixed and translated.

--- src/FDC/MarcheDuFilmBundle/Resources/public/js/custom.js
+++ src/FDC/MarcheDuFilmBundle/Resources/public/js/custom.js
@@ -76,7 +76,7 @@ function setNews() {
                 if (this.id == 'all' || el.length < 1 ) {
                     $('.parent > div').fadeOut(200);
                     $('#empty-news-list').hide();
-                    $('.parent').append('<div class="events message">aucun évenement sélectionné</div>');
+                    $('#no-filter-news').show();
                     $('.selectText').hide();
 
                 } else if (this.id == 'all') {
