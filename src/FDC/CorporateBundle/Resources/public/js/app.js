ea523e3113db1d99105116b569da9764e5a5bc65
Tho Mon <t.montet@ohwee.fr>
2017-02-09 14:47:46 +0100

    check conditions

--- src/FDC/CorporateBundle/Resources/public/js/app.js
+++ src/FDC/CorporateBundle/Resources/public/js/app.js
@@ -3887,7 +3887,7 @@ var owInitSlider = function (sliderName) {
 
         $('.slider-home').on('click', function(e){
             console.log($(e.target));
-            console.log($(e.target).hasClass('owl-dots'));
+            console.log($(e.target).hasClass('owl-dots') || $(e.target).closest('.owl-dots').length);
             if($(e.target).hasClass('owl-dots')){
                 return false;
             }else{
