20bd6f998240a1714f4fabc1993fbad0a5aac6bd
Andrei Camarasan <andrei.camarasan@pitechnologies.ro>
2017-02-09 14:34:09 +0200

    #Issue-6973.Overwrite locale listener from admin bundle.

--- src/FDC/MarcheDuFilmBundle/EventListener/KernelRequestListener.php
+++ src/FDC/MarcheDuFilmBundle/EventListener/KernelRequestListener.php
@@ -10,15 +10,27 @@ class KernelRequestListener
 
     protected $locales;
 
-    public function __construct($host, $locales)
+    protected $defaultLocale;
+
+    public function __construct($host, $locales, $defaultLocale)
     {
         $this->host = $host;
         $this->locales = $locales;
+        $this->defaultLocale = $defaultLocale;
     }
 
     public function onKernelRequest(GetResponseEvent $event)
     {
-        if ($event->getRequest()->getHost() == $this->host && !in_array($event->getRequest()->getLocale(), $this->locales)) {
+        $request = $event->getRequest();
+
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+
+        if ($request->getHost() == $this->host && !in_array($request->getLocale(), $this->locales)) {
             throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException();
         }
     }
