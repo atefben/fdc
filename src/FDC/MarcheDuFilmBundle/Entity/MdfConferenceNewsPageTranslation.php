a7a6d146268918c09b9e982604214edfc4c3bbdd
Andrei Camarasan <andrei.camarasan@pitechnologies.ro>
2017-02-10 16:47:35 +0200

    Added missing statuse in BO.

--- src/FDC/MarcheDuFilmBundle/Entity/MdfConferenceNewsPageTranslation.php
+++ src/FDC/MarcheDuFilmBundle/Entity/MdfConferenceNewsPageTranslation.php
@@ -3,6 +3,8 @@
 namespace FDC\MarcheDuFilmBundle\Entity;
 
 use A2lix\I18nDoctrineBundle\Doctrine\ORM\Util\Translation;
+use Base\CoreBundle\Interfaces\TranslateChildInterface;
+use Base\CoreBundle\Util\TranslateChild;
 use Base\CoreBundle\Util\TranslationChanges;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -11,10 +13,11 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="mdf_conference_news_page_translation")
  * @ORM\Entity(repositoryClass="FDC\MarcheDuFilmBundle\Repository\MdfConferenceNewsPageTranslationRepository")
  */
-class MdfConferenceNewsPageTranslation
+class MdfConferenceNewsPageTranslation implements TranslateChildInterface
 {
     use Translation;
     use TranslationChanges;
+    use TranslateChild;
 
     /**
      * @var string
