79f786a68b68591f3638a1d534b175f9d2cd7ec3
Luca Vlad <vlad.luca@pitechnologies.ro>
2017-02-21 12:06:42 +0200

    WIP: Social wall.

---
+++ src/Base/AdminBundle/Admin/CCM/CcmSocialWallHashTagAdmin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Base\AdminBundle\Admin\CCM;
+
+use Base\AdminBundle\Component\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CcmSocialWallHashTagAdmin extends Admin
+{
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'edit'   => array(),
+                ),
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('hashtag', 'text')
+        ;
+    }
+}
