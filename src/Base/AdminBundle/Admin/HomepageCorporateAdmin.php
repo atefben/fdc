073539756b437283c7315471014e0f0f66f7228b
Jean-Luc Petit <webpetit@gmail.com>
2017-02-21 02:50:31 +0100

    5036

--- src/Base/AdminBundle/Admin/HomepageCorporateAdmin.php
+++ src/Base/AdminBundle/Admin/HomepageCorporateAdmin.php
@@ -298,12 +298,12 @@ class HomepageCorporateAdmin extends Admin
             ))
             ->add('pushSecondaryImage1', 'sonata_type_model_list', array(
                 'label' => 'form.label_image_push',
-                'help' => 'form.homepage.helper_pushes',
+                'help' => 'form.homepage.helper_pushes_306x206',
                 'required' => false,
             ))
             ->add('pushSecondaryImage2', 'sonata_type_model_list', array(
                 'label' => 'form.label_image_push',
-                'help' => 'form.homepage.helper_pushes',
+                'help' => 'form.homepage.helper_pushes_306x206',
                 'required' => false,
             ))
             ->add('pushSecondaryImage3', 'sonata_type_model_list', array(
