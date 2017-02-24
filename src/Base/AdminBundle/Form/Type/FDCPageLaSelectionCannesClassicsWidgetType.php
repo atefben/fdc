cacbf8f4eadbf455443f8b099e124fae2635ca02
Jean-Luc Petit <webpetit@gmail.com>
2017-02-24 01:27:42 +0100

    5224

--- src/Base/AdminBundle/Form/Type/FDCPageLaSelectionCannesClassicsWidgetType.php
+++ src/Base/AdminBundle/Form/Type/FDCPageLaSelectionCannesClassicsWidgetType.php
@@ -53,7 +53,6 @@ class FDCPageLaSelectionCannesClassicsWidgetType extends BaseType
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        dump($this->dataClass);
         $resolver->setDefaults(array(
             'data_class'  => $this->dataClass,
             'model_class' => $this->dataClass,
--- src/Base/CoreBundle/Twig/Extension/MiscExtension.php
+++ src/Base/CoreBundle/Twig/Extension/MiscExtension.php
@@ -2,9 +2,10 @@
 
 namespace Base\CoreBundle\Twig\Extension;
 
+use Base\CoreBundle\Interfaces\TranslateChildInterface;
 use Behat\Transliterator\Transliterator;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use \Twig_Extension;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Twig_Extension;
 
 /**
  * Class MiscExtension
@@ -13,16 +14,79 @@ use \Twig_Extension;
 class MiscExtension extends Twig_Extension
 {
 
+    /**
+     * @var string
+     */
+    protected $defaultLocale;
+
+    /**
+     * @var RequestStack
+     */
+    protected $requestStack;
+
+    /**
+     * @param string $defaultLocale
+     */
+    public function setDefaultLocale($defaultLocale)
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function setRequestStack(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+
     public function getFilters()
     {
-        return array(
-            new \Twig_SimpleFilter('urlize', array($this, 'urlize')),
-        );
+        return [
+            new \Twig_SimpleFilter('urlize', [$this, 'urlize']),
+            new \Twig_SimpleFilter('is_published', [$this, 'isPublished']),
+        ];
     }
 
     public function urlize($text)
     {
         return Transliterator::urlize($text);
+    }
+
+    public function isPublished($object, $site = null)
+    {
+        if (!method_exists($object, 'findTranslationByLocale')) {
+            return null;
+        }
+        if ($site) {
+            $inSite = false;
+            foreach ($object->getSites() as $site) {
+                if ($site->getSlug() == $site) {
+                    $inSite = true;
+                }
+            }
+            if (!$inSite) {
+                return false;
+            }
+        }
+        $currentLocale = $this->requestStack->getCurrentRequest()->getLocale();
+        $trans = $object->findTranslationByLocale($currentLocale);
+        if (!$trans) {
+            return false;
+        }
+        if ($this->defaultLocale == $currentLocale) {
+            if ($trans->getStatus() == TranslateChildInterface::STATUS_PUBLISHED) {
+                return true;
+            }
+            return false;
+        } else {
+            $main = $object->findTranslationByLocale($this->defaultLocale);
+            if ($main->getStatus() == TranslateChildInterface::STATUS_PUBLISHED && $trans->getStatus() == TranslateChildInterface::STATUS_TRANSLATED) {
+
+            }
+            return false;
+        }
     }
 
     /**
