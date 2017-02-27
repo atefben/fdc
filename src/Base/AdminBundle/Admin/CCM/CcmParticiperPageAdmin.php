6a95fd7a8e934a3af9b50e821f03f5859a8411d6
Andrei Camarasan <andrei.camarasan@pitechnologies.ro>
2017-02-27 10:22:21 +0200

    #Issue-7101-7102.Page name listed.

--- src/Base/AdminBundle/Admin/CCM/CcmParticiperPageAdmin.php
+++ src/Base/AdminBundle/Admin/CCM/CcmParticiperPageAdmin.php
@@ -6,6 +6,7 @@ use Base\AdminBundle\Component\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use FDC\CourtMetrageBundle\Entity\CcmParticiperPageTranslation;
 
@@ -31,7 +32,7 @@ class CcmParticiperPageAdmin extends Admin
     {
         $listMapper
             ->add('id', null, array('label' => 'filter.common.label_id'))
-            ->add('title')
+            ->add('name')
             ->add('_action', 'actions', array(
                 'actions' => array(
                     'edit' => array(),
@@ -103,5 +104,15 @@ class CcmParticiperPageAdmin extends Admin
                 ),
             ))
         ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('name');
     }
 }
