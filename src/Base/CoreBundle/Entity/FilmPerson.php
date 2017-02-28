eb33ec95915e7bd0175d6eb700c293c206f5dd7a
Jean-Luc Petit <webpetit@gmail.com>
2017-02-28 21:46:44 +0100

    6968

--- src/Base/CoreBundle/Entity/FilmPerson.php
+++ src/Base/CoreBundle/Entity/FilmPerson.php
@@ -404,7 +404,7 @@ class FilmPerson implements TranslateMainInterface
      *      joinColumns={@ORM\JoinColumn(name="person", referencedColumnName="id")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="image", referencedColumnName="id")}
      *      )
-     * @ORM\OrderBy({"updatedAt" = "desc"})
+     * @ORM\OrderBy({"updatedAt"="asc","createdAt"="asc"})
      */
     protected $selfkitImages;
 
