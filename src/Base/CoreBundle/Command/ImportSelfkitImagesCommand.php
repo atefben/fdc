3af56b5fffc2396292c092d932d1717d6869f834
Jean-Luc Petit <webpetit@gmail.com>
2017-03-02 01:53:41 +0100

    6968

--- src/Base/CoreBundle/Command/ImportSelfkitImagesCommand.php
+++ src/Base/CoreBundle/Command/ImportSelfkitImagesCommand.php
@@ -335,7 +335,6 @@ class ImportSelfkitImagesCommand extends ContainerAwareCommand
 
         $media->setName($old->getTitre());
         $this->getMediaManager()->save($media, false);
-        unlink($filename); // remove original file
 
         $provider = $this->getContainer()->get($media->getProviderName());
         $format = $provider->getFormatName($media, 'reference');
@@ -346,10 +345,12 @@ class ImportSelfkitImagesCommand extends ContainerAwareCommand
             $this->applyAssociation($media, $old);
             $this->generatePresets($media);
             $this->getManager()->flush();
+            unlink($filename); // remove original file
             return $media;
         } else {
             $this->getManager()->remove($media);
             $this->getManager()->flush();
+            unlink($filename); // remove original file
         }
     }
 
