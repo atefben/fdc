8079c575db5c2de1957bbb5d25d6ae602da087a7
Jean-Luc Petit <webpetit@gmail.com>
2017-03-03 11:02:09 +0100

    5084

--- src/Base/AdminBundle/Resources/public/ckeditor/config_corpo.js
+++ src/Base/AdminBundle/Resources/public/ckeditor/config_corpo.js
@@ -30,14 +30,8 @@ CKEDITOR.editorConfig = function( config ) {
         // Paragraphs
         {name: 'Description titre centré', element: 'p', attributes: {'class': 'descrip-title'}},
         {name: 'Description google map', element: 'span', attributes: {'class': 'googlemap-descrip'}},
-        {
-            name: 'Cadre doré',
-            element: 'div',
-            attributes: {'class': 'info'},
-            styles: {
-                element: 'p',
-            }
-        }
+        {name: 'Cadre doré', element: 'div', attributes: {'class': 'info'}},
+        {name: 'Paragraphe', element: 'p', attributes: {'class': 'paragraph'}}
     ];
 
     // TOOLBAR
