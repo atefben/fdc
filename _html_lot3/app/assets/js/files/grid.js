47504501935161b1046b8140bf5d5a1b24d90e43
Tho Mon <t.montet@ohwee.fr>
2017-03-03 16:55:23 +0100

    [debug] movie grid

--- _html_lot3/app/assets/js/files/grid.js
+++ _html_lot3/app/assets/js/files/grid.js
@@ -252,7 +252,7 @@ var owInitGrid = function (id) {
                 //get max heights
                 $.each($('.item.block-poster'), function (i,e) {
                     var naturalIndex = i+1;
-                    var ww = $(window).width();
+                    var ww = $(document).width();
                     var colNumber = 4;
                     if(ww > 1280){
                         colNumber = 5;
