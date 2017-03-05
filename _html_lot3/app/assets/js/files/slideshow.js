bd9bd914b9b0e7794e872cb81fa34482d6a57e13
thomon <t.montet@ohwee.fr>
2017-03-06 00:44:29 +0100

    [debug] #7174

--- _html_lot3/app/assets/js/files/slideshow.js
+++ _html_lot3/app/assets/js/files/slideshow.js
@@ -75,7 +75,7 @@ var openSlideShow = function (slider, hash, affiche) {
     var w = $(window).width();
     var centerElement = 0;
     var caption = "";
-    console.log(slider.find('.item, .img, .poster').size());
+    console.log(slider.html());
     slider.find('.item, .img, .poster').each(function (index, value) {
         console.log(!$(value).hasClass('video') && !$(value).hasClass('audio'));
         if(!$(value).hasClass('video') && !$(value).hasClass('audio')){
