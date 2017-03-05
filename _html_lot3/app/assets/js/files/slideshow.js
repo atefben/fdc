277557ad7d615a768bdab64a21e16349b6269c4b
thomon <t.montet@ohwee.fr>
2017-03-05 23:35:04 +0100

    [debug] viewer

--- _html_lot3/app/assets/js/files/slideshow.js
+++ _html_lot3/app/assets/js/files/slideshow.js
@@ -358,11 +358,12 @@ var openSlideShow = function (slider, hash, affiche) {
     $('.c-fullscreen-slider').addClass('chocolat-wrapper');
 
     $('.c-fullscreen-slider').append('<div class="chocolat-top"><i class="icon icon-close chocolat-close"></i></div>');
-    
+    console.log(images[centerElement].caption);
+    console.log(images[centerElement].caption.indexOf('Crédit Image :'));
     if(images[centerElement].caption.indexOf('Crédit Image :') == -1){
         images[centerElement].caption = 'Crédit Image : '+images[centerElement].caption;
     }
-    
+
     $('.c-fullscreen-slider').append('<div class="c-chocolat-bottom">' +
         '<div class="chocolat-bottom">' +
         '<span class="chocolat-fullscreen"></span>' +
