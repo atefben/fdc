0426d607ddff1e7c582fad2d1b3d4551a54cc9d7
Alexandru-Ciprian Vasileniuc <deepmikoto@gmail.com>
2017-03-07 14:25:44 +0200

    only show news type when displaying all news articles list

--- src/Base/AdminBundle/Admin/CCM/CcmNewsImageAdmin.php
+++ src/Base/AdminBundle/Admin/CCM/CcmNewsImageAdmin.php
@@ -2,9 +2,8 @@
 
 namespace Base\AdminBundle\Admin\CCM;
 
-use FDC\CourtMetrageBundle\Entity\CcmNews;
+
 use FDC\CourtMetrageBundle\Entity\CcmNewsImageTranslation;
-use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
@@ -48,6 +47,16 @@ class CcmNewsImageAdmin extends CcmNewsAdmin
             parent::getFormTheme(),
             array('BaseAdminBundle:Form:polycollection.html.twig')
         );
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        parent::configureListFields($listMapper);
+
+        $listMapper->remove('type');
     }
 
     /**
