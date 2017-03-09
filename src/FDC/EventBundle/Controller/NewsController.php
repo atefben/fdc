6ebd519ab7ef5598cd4a6d40202540e99afbd80b
Jean-Luc Petit <webpetit@gmail.com>
2017-03-09 19:01:44 +0100

    7500

--- src/FDC/EventBundle/Controller/NewsController.php
+++ src/FDC/EventBundle/Controller/NewsController.php
@@ -113,6 +113,7 @@ class NewsController extends Controller
             $homeArticles = $em->getRepository('BaseCoreBundle:News')->getNewsByDate($locale, $this->getFestival()->getId(), $dateTime, $count);
 
             $homeArticles = array_merge($homeInfos, $homeStatement, $homeArticles);
+            usort($articles, [$this, 'compareArticle']);
         }
 
         $homeArticles = $this->removeUnpublishedNewsAudioVideo($homeArticles, $locale, $count);
@@ -247,6 +248,9 @@ class NewsController extends Controller
 
         $films = $homepage->getFilmsAssociated();
 
+        foreach ($homeArticles as $homeArticle) {
+            dump($homeArticle->getPublishedAt()->format('Y-m-d H:i:s'));
+        }
         // SEO
         $this->get('base.manager.seo')->setFDCEventPageHomepageSeo($homepage, $locale);
 
@@ -837,6 +841,21 @@ class NewsController extends Controller
             $mark += $incr;
         }
         return $partition;
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return int
+     */
+    private function compareArticle($a, $b)
+    {
+        $aTime = $a->getPublishedAt()->getTimestamp();
+        $bTime = $b->getPublishedAt()->getTimestamp();
+        if ($aTime == $bTime) {
+            return 0;
+        }
+        return ($aTime > $bTime) ? -1 : 1;
     }
 
 }
