c53a2bf9a57583d0a521a079f061e7c218102d62
Tho Mon <t.montet@ohwee.fr>
2017-03-09 12:14:31 +0100

    debug JS error

--- _html_lot3/build/js/app.js
+++ _html_lot3/build/js/app.js
@@ -4327,7 +4327,7 @@ var initRs = function () {
 
         $('.popin-mail-open').on('click touchstart', function (e) {
             e.preventDefault();
-
+            console.log($(e.target));
             $('.overlay-popin').addClass('visible-popin');
 
             $('.overlay-popin').on('click', function (e) {
