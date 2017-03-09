3b8700074aa5c4e5cca6b2e9f97442038e326165
Jean-Luc Petit <webpetit@gmail.com>
2017-03-09 11:28:33 +0100

    Fix info

--- src/Base/CoreBundle/Entity/Info.php
+++ src/Base/CoreBundle/Entity/Info.php
@@ -84,7 +84,7 @@ abstract class Info implements TranslateMainInterface
      * @ORM\Column(type="boolean", options={"default":0})
      *
      */
-    protected $displayedOnCorpoHome = 0;
+    protected $displayedOnCorpoHome = false;
 
     /**
      * @var boolean
