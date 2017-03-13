1c449da7d178ff267d9966928077d3975770285f
Tho Mon <t.montet@ohwee.fr>
2017-03-13 17:56:42 +0100

    debug audio player header

--- _html_webview/js/article.js
+++ _html_webview/js/article.js
@@ -120,7 +120,7 @@ var ow = ow || {};
 					}
 				}).on('ready',function(){
 					player.find('audio').prop('playsinline',true);
-					console.log(player, player.attr('id'));
+					console.log(player, $('.Article-header #'+player.attr('id')).length);
 					if(player.closest('.Article-header').length){
 						//hide video and set transparent audio header
 						player.find('.jw-media').css('opacity',0);
