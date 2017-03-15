06420471159c0d40cee08d56f057e32dcf9c4c0a
Tho Mon <t.montet@ohwee.fr>
2017-03-15 17:06:22 +0100

    debug contents

--- _html_lot3/app/assets/js/files/grid.js
+++ _html_lot3/app/assets/js/files/grid.js
@@ -85,7 +85,6 @@ var owInitGrid = function (id) {
                 $.get( url, {date: dateTime, theme: theme, format: format}, function( data ) {
                     if(data == null){
                         return false;
-
                     }else{
                         $data = $(data);
                         //add new filters
@@ -149,7 +148,63 @@ var owInitGrid = function (id) {
                                 });
                             }
                         });*/
-                        //filterClick();
+
+                        $('.filters .select span').off('click').on('click', function () {
+                            $('.filter .select').each(function () {
+                                $that = $(this);
+                                $id = $(this).closest('.filter').attr('id');
+
+                                $that.find(".pages:not([data-filter='all'])").each(function () {
+                                    $this = $(this);
+
+                                    var getVal = $this.data('filter');
+                                    var numItems = $('.item[data-' + $id + '="' + getVal + '"]:not([style*="display: none"]').length;
+
+                                    if (numItems === 0) {
+                                        $this.addClass('disabled');
+                                    } else {
+                                        $this.removeClass('disabled');
+                                    }
+                                });
+                            });
+
+                            var h = $(this).parent().html();
+
+                            $('#filters').remove();
+                            $('body').append('<div id="filters"><div class="vCenter"><div class="vCenterKid"></div></div><div class="close-button"><i class="icon icon-close"></i></div></div>');
+                            $('#filters .vCenterKid').html(h);
+                            $('#filters .vCenterKid').find(':not(span)').remove();
+                            $('#filters .vCenterKid').find('span.disabled').remove();
+                            $('#filters').attr('data-id', $(this).parents('.filter').attr('id'));
+
+                            setTimeout(function () {
+                                $('#filters').addClass('show');
+                            }, 100);
+
+                            setTimeout(function () {
+                                $('#filters span').addClass('show');
+                            }, 400);
+
+                            $('#filters span').on('click', function () {
+                                var id = $('#filters').data('id'),
+                                    f = $(this).data('filter');
+
+                                $('#' + id + ' .select span').removeClass('active');
+                                $('#' + id + ' .select span[data-filter="' + f + '"]').addClass('active');
+
+                                $('.pages:not(.' + f + ')').css('display', 'none');
+                                $('.pages.' + f).css('display', 'block');
+                            });
+
+                            // close filters
+                            $('body').on('click', '#filters', function () {
+                                $('#filters').removeClass('show');
+                                setTimeout(function () {
+                                    $('#filters').remove();
+                                }, 700);
+                            });
+                        });
+
                         //BUTTON BEHAVIOUR
                         var moreBtn = $data.find('.ajax-request').attr('href');
                         if(typeof moreBtn !== 'undefined'){
@@ -220,10 +275,7 @@ var owInitGrid = function (id) {
                                 $clamp(title.get(0), {clamp: 1});
                                 $clamp(cat.get(0), {clamp: 1});
                             });
-
                         });
-                        
-                        
 
                         $('input[name="pg"]').val(parseInt($('input[name="pg"]').val())+1);
                         
