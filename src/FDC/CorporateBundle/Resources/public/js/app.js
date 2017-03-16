75686bf3484a61f50f6be1762b1cf0c5d2d244eb
Tho Mon <t.montet@ohwee.fr>
2017-03-16 12:12:12 +0100

    [debug] - js reorder HP on ajax

--- src/FDC/CorporateBundle/Resources/public/js/app.js
+++ src/FDC/CorporateBundle/Resources/public/js/app.js
@@ -2331,6 +2331,33 @@ var initFaq = function() {
 
 var isotopeHomepageItems = [];
 
+var contains = function(needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+
+    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+            var i = -1, index = -1;
+
+            for(i = 0; i < this.length; i++) {
+                var item = this[i];
+
+                if((findNaN && item !== item) || item === needle) {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        };
+    }
+
+    return indexOf.call(this, needle) > -1;
+};
+
 var owInitFilter = function (isTabSelection) {
     isTabSelection = isTabSelection || false;
     var homepageItemsFilled = false;
@@ -2387,9 +2414,11 @@ var owInitFilter = function (isTabSelection) {
                         var grid = $this.find('.isotope-01');
                         $this.find('article').each(function(index,value){
                             isotopeHomepageItems.push(value);
+                            console.log(contains.call(isotopeHomepageItems, value));
                         });
                     });
                 }
+
             });
             var lockInterval = window.setInterval(function(){
                 lock = false;
