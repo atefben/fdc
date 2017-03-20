63680e2200d13c9dac45ae3b61f2aa1727f8d28d
Tho Mon <t.montet@ohwee.fr>
2017-03-20 18:10:05 +0100

    debug focus strate

--- _html_lot3/app/assets/js/files/grid.js
+++ _html_lot3/app/assets/js/files/grid.js
@@ -30,7 +30,7 @@ var owInitGrid = function (id) {
 
         //ratio calculation on media library
         if($('.media-library').length){
-            var landscapeRatio = 1,5842 //width / height
+            //var landscapeRatio = 1,5842 //width / height
         }
         var clickAllow = true;
         var $gridDom = $('.add-ajax-request');
