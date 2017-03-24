8d7c08eff96d111d416ae76103defc1a744560d4
Alexandru-Ciprian Vasileniuc <deepmikoto@gmail.com>
2017-03-24 11:08:56 +0200

    Issue #3220.Add both sources to homepage video players

--- src/FDC/EventMobileBundle/Resources/public/js/home_custom.js
+++ src/FDC/EventMobileBundle/Resources/public/js/home_custom.js
@@ -54,4 +54,41 @@ $(document).ready(function () {
 
         moveTimeline($(this), $(this).data('date'));
     });
+
+    $('.item-video').off('click').on('click',function(e) {
+        e.preventDefault();
+        var $this = $(this),
+            sources = [],
+            sourceAttrs = ['data-webmurl', 'data-mp4url'],
+            $fullscreenplayer = $('.fullscreenplayer')
+        ;
+
+        setTimeout(function() {
+            $fullscreenplayer.addClass('show');
+            $('body').addClass('allow-landscape');
+        }, 200);
+
+        $.each(sourceAttrs, function (idx, name) {
+            $this.attr(name) ? sources.push({ file: $this.attr(name) }) : null;
+        });
+
+        if($("#player1").length !== 0) {
+            playerInstance = jwplayer("player1");
+            playerInstance.setup({
+                image        : $this.data('poster'),
+                width        : "100%",
+                aspectratio  : "16:9",
+                displaytitle : false,
+                mediaid      : '123456',
+                skin         : {
+                    name : "five"
+                },
+                sources: sources
+            });
+        }
+
+        $fullscreenplayer.find('.category').html($(this).find('.category').html());
+        $fullscreenplayer.find('.title-video').html($(this).find('.titleLink').html());
+        $fullscreenplayer.find('.date').html($(this).find('.titleLink').attr('data-date'));
+    });
 });
