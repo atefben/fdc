1aba6aa91bc649fd393b8d50f2f6106193beedfb
Alexandru-Ciprian Vasileniuc <deepmikoto@gmail.com>
2017-03-27 13:47:50 +0300

    WIP

--- src/FDC/CourtMetrageBundle/Repository/CcmMainNavTranslationRepository.php
+++ src/FDC/CourtMetrageBundle/Repository/CcmMainNavTranslationRepository.php
@@ -15,15 +15,22 @@ class CcmMainNavTranslationRepository extends EntityRepository
     {
         $qb = $this->createQueryBuilder('s');
         $qb
-            ->where('s.locale = :locale')
+            ->where('s.locale = :locale_fr')
             ->andWhere('s.translatable = :translatable')
-            ->andWhere('s.status = :publish or s.status = :translate')
-            ->setParameter('publish',CcmMainNavTranslation::STATUS_PUBLISHED)
-            ->setParameter('translate',CcmMainNavTranslation::STATUS_TRANSLATED)
+            ->andWhere('s.status = :published')
+            ->setParameter('published',CcmMainNavTranslation::STATUS_PUBLISHED)
+            ->setParameter(':locale_fr', 'fr')
             ->innerJoin('s.translatable', 't')
-            ->setParameter(':locale', $locale)
             ->setParameter(':translatable', $translatable)
         ;
+        if ($locale != 'fr') {
+            $qb
+                ->join('t.translations', 'tt')
+                ->andWhere('tt.locale = :locale and tt.status = :translated')
+                ->setParameter(':locale', $locale)
+                ->setParameter('translated',CcmMainNavTranslation::STATUS_TRANSLATED)
+            ;
+        }
 
         return $qb->getQuery()->getOneOrNullResult();
     }
