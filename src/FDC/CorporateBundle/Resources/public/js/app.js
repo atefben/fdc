5c5f9671af74c24e346e965b444445932c0ce86f
Tho Mon <t.montet@ohwee.fr>
2017-03-29 15:02:10 +0200

    debug hotfix IE retrospective archives

--- src/FDC/CorporateBundle/Resources/public/js/app.js
+++ src/FDC/CorporateBundle/Resources/public/js/app.js
@@ -3406,8 +3406,9 @@ var videoMovie;
 // Single Movie
 // =========================
 $(document).ready(function() {
-
+  console.log('lalal');
   if($('.content-movie').length) {
+    console.log('la');
     $('body').on('click', '.content-movie .prevmovie', function (e) {
       console.log('click');
       $('.content-movie .nav.prev').trigger('click');
@@ -3415,7 +3416,7 @@ $(document).ready(function() {
 
     $('body').on('click', '.content-movie .nextmovie', function (e) {
       console.log('click');
-      $('.single-movie .nav.next').trigger('click');
+      $('.content-movie .nav.next').trigger('click');
     });
   }
 
