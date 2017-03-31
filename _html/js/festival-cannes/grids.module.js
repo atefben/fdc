770b217759a5476d8ababa50278bd46a6b9447fa
Tho Mon <t.montet@ohwee.fr>
2017-03-31 11:42:13 +0200

    debug event grid

--- _html/js/festival-cannes/grids.module.js
+++ _html/js/festival-cannes/grids.module.js
@@ -311,7 +311,7 @@ function setGrid(grid, dom, init){
     }
 
     grid.append($data).isotope('appended', $data);
-    console.log($data);
+    console.log('grid',grid);
     //grid.isotope('insert',$data);
     grid.imagesLoaded().progress(function () {
       console.log('imagesloaded progress');
