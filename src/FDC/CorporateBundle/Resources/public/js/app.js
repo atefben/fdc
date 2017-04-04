987788017fde2403c3ec3920cc865517e9539656
Tho Mon <t.montet@ohwee.fr>
2017-04-04 11:04:52 +0200

    remove logs

--- src/FDC/CorporateBundle/Resources/public/js/app.js
+++ src/FDC/CorporateBundle/Resources/public/js/app.js
@@ -2918,7 +2918,6 @@ var owInitGrid = function (id) {
                 }
 
                 //console.log('data sent to GET',postData);
-                console.log('ajax url',url);
                 if(!ajaxLock && url != '#'){
                     ajaxLock = true;
                     $.ajax({
