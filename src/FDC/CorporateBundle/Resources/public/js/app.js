d0b95fb983513dd205da24f7b605cf4218ee870e
Tho Mon <t.montet@ohwee.fr>
2017-04-05 16:30:38 +0200

    remove parallax effect on retrospective for debug

--- src/FDC/CorporateBundle/Resources/public/js/app.js
+++ src/FDC/CorporateBundle/Resources/public/js/app.js
@@ -4032,7 +4032,7 @@ var onInitParallax = function () {
         $(window).on('scroll', function () {
             if ($('header.sticky').length) {
                 var s = $(this).scrollTop() - 120;
-                $('.block-push.big').css('background-position', '0px ' + s + 'px');
+                //$('.block-push.big').css('background-position', '0px ' + s + 'px');
             } else {
                 $('.block-push.big').css('background-position', '0px ' + '-20%');
             }
