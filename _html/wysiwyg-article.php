6a90e50b101073d69c80b0167bc39f88f07acef7
Tho Mon <t.montet@ohwee.fr>
2017-04-05 17:33:50 +0200

    rollback html lot 2

---
+++ _html/wysiwyg-article.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+        <?php include('head.html'); ?>
+  </head>
+
+  <body>
+    <?php include('header.html'); ?>
+
+    <div id="main" class="single-article loading ">
+
+      <div class="container small-container">
+
+      <!-- module information -->
+
+      <div class="info">
+        <a href="#" class="category">Cinéma de la plage</a><span class="date">18.05.15</span> . <span class="hour">09:00</span>
+        <span class="update">Mise à jour :</span><span class="date">18.05.15</span> . <span class="hour">10:00</span>
+      </div>
+
+      <!-- titre -->
+
+      <h2 class="title-article">Enragés, polar hybride d'Eric Hannezo</h2>
+
+      <!-- bouton partage et imprimer -->
+
+      <div class="buttons">
+        <a id="share-article" href="#" class="button"><i class="icon icon_share"></i>Partager</a>
+        <a href="#" class="button print"><i class="icon icon_print"></i>
+Imprimer</a>
+      </div>
+
+      <!-- texte intro -->
+      <br><br>
+
+      <div class="chapeau">Ancien journaliste devenu producteur et cinéaste, Éric Hannezo s'aventure pour son premier film sur les routes nord-américaines et signe un polar hybride à l'affiche duquel on retrouve <a href="#">Lambert Wilson</a> et Franck Gastambide.</div>
+
+
+      <!-- textes -->
+
+      <div class="text">
+        <p><strong>Il y a différents statuts d’acteurs : Lambert Wilson, Virginie Ledoyen d’un côté et ensuite tu as Guillaume Gouix, jeune acteur, Franck Gastambile et le québécois François Arnaud … Vous êtes satisfait du rendu ?</strong></p>
+        <p>Symboliquement, j’avais une famille : Lambert qui joue le père, Virginie la mère, Guillaume l’ainé, Franck le jeune un peu perdu et François le sale gosse. Et oui, j’en suis super content parce que je voulais une lecture nouvelle sur le film de genre .</p>
+
+        <br><br>
+        <!-- bouton toute la programmation  il faut qu'il soit dans une div texte pour être bien aligné-->
+        <a href="programmation-evenementiel.php" class="button single-button">Toute la programmation</a>
+
+      </div>
+
+
+      <!-- quote -->
+
+
+      <div class="quote">
+        <blockquote>
+          “J’ai découvert que Lambert avait une passion pour le film de genre”
+        </blockquote>
+      </div>
+
+      <!-- mise en évidence 2 -->
+      <div class="quote italic">
+        <blockquote>
+          Au cinéma comme en amour, il faut savoir s’abandonner
+        </blockquote>
+      </div>
+
+      <!-- Sous titre 1 -->
+
+      <h2 class="sub-title">Sous titre 1</h2>
+
+
+
+      <!-- textes -->
+
+      <div class="text">
+
+        <!-- texte -->
+
+        <p>
+          Cinéaste, producteur, distributeur et acteur italien, Nanni Moretti est né en 1953 à Brunico en Italie. Il réalise un certain nombre de courts métrages avant de terminer son premier long métrage Io Sono un Autarchico en 1976 et de jouer dans Padre Padrone des frères Taviani présenté à Cannes en 1977. Plusieurs de ses films suivants sont primés à travers le monde : Sogni d'Oro (Prix spécial du jury à Venise en 1981), La Messa e' finita (Ours d'Argent à Berlin en 1986) ou sélectionnés en compétition officielle à Cannes: Ecce Bombo en 1978, Aprile en 1998, Caro Diario qui remporte le prix de la mise en scène en 1994 et La Stanza del figlio, palme d'or au Festival 2001.
+        </p>
+
+
+        <!-- sous titre 2 -->
+
+        <h2 class="sub-title2">Sous titre 2</h2>
+
+
+        <!-- texte -->
+
+        <p>
+          Acteur dans la majorité de ses films, Nanni Moretti fonde en 1986 la maison de production Sacher film avec Angelo Barbagallo et produit, outre ses propres films, Notte Italiana (1987) de Carlo Mazzacurati.
+        </p>
+
+        <!-- sous titre 3 -->
+
+        <h3 class="sub-title">Sous titre 3</h3>
+
+        <!-- texte avec un a -->
+
+        <p>
+          <a href="#">Domani Accadra</a> (1988) et <a href="#">Il Portaborse</a> (1991) de Daniele Luchetti puis La Seconda Volta (1995) de Mimmo Calopresti dans lequel il est aussi acteur.
+        </p>
+
+        <p>
+          En 1991, il inaugure le cinéma Nuovo Sacher et depuis 1996, il dirige le Festival Sacher dédié aux courts métrages.
+        </p>
+
+        <!-- sous titre 2 -->
+
+        <h2 class="sub-title2">Sous titre 2</h2>
+
+
+        <!-- texte -->
+
+        <p>
+          La Messa e' finita (Ours d'Argent à Berlin en 1986) ou sélectionnés en compétition officielle à Cannes: Ecce Bombo en 1978, Aprile en 1998, Caro Diario qui remporte le prix de la mise en scène en 1994 et La Stanza del figlio, palme d'or au Festival 2001.
+        </p>
+
+      </div>
+
+      <!-- fin textes -->
+
+
+      <!-- Mise en évidence -->
+
+      <div class="big-quote">
+        <strong>Dans quel sens ?</strong>
+
+        <p>
+          Je voulais être libre au niveau dans la narration et la forme. Le film tire sa singularité du fait que nous l’avons tourné aux États-Unis avec un casting français. Je ne souhaitais pas rester dans le schéma classique, avec les méchants d’un côté et les gentils de l’autre, mais que ce soit plus obscur et complexe.
+        </p>
+
+      </div>
+
+        <!-- Texte gros -->
+
+        <div class="big-txt">
+          <p>
+            Cinéaste, producteur, distributeur et acteur italien, Nanni Moretti est né en 1953 à Brunico en Italie. Il réalise un certain nombre de courts métrages avant de terminer son premier long métrage Io Sono un Autarchico en 1976 et de jouer dans Padre Padrone des frères Taviani présenté à Cannes en 1977. Plusieurs de ses films suivants sont primés à travers le monde : Sogni d'Oro (Prix spécial du jury à Venise).
+          </p>
+        </div>
+
+    </div>
+
+    <!-- fin du container small pour la partie "texte en gris" -->
+
+    <!-- texte avec fond gris -->
+
+    <div class="txt-grey-bkg-container">
+      <div class="content">
+        <strong>Enragés est l’adaptation d’un film </strong>
+        <p>
+          Cela fait vingt ans que je travaille l’image par le biais du reportage ou du documentaire pour me rapprocher de plus en plus de cette envie de cinéma qui ne m’a jamais quittée depuis l’adolescence.
+        </p>
+      </div>
+    </div>
+
+
+    <!-- fin texte avec fond gris -->
+
+
+    <div class="container small-container">
+
+      <!-- partie question -->
+
+      <div class="text">
+          <p><strong>Enragés est l’adaptation d’un film de Mario Bava intitulé Rabid Dogs. Pourquoi l’avoir choisi comme base de votre scénario ?</strong></p>
+          <p>
+            Je suis tombé dessus par hasard dans ma boulimie de cinéma et je me suis aperçu qu’il y avait énormément de choses dans sa structure qui me séduisaient. C’est un film considéré comme culte car il a été censuré plus de vingt ans avant d'être visible. Puis, je suis rentré en travail d’écriture pour prendre la tangente par rapport à l’original.
+          </p>
+      </div>
+
+      <!-- module photo -->
+
+      <div class="single-photo">
+        <div class="photo-container">
+          <img src="img/article/002.jpg" alt="">
+          <p class="caption">Équipe du film - Photocall - The Lobster © FDC / Théophile Delange</p>
+        </div>
+      </div>
+
+
+      <!-- tableau -->
+
+      <table class="table-article">
+          <tr>
+            <th>
+              compétition long métrages (25)
+            </th>
+            <th>
+               compétition court métrages (3)
+            </th>
+          </tr>
+          <tr>
+            <td>
+              un certain regard (10)
+            </td>
+            <td>
+              séances de minuit
+            </td>
+          </tr>
+          <tr>
+            <td>
+              cinéfondation (9)
+            </td>
+            <td>
+              cannes classics (5)
+            </td>
+          </tr>
+      </table>
+
+
+      <span class="caption-article">Rédigé par <span class="name">Morgane Urbain</span></span>
+
+    </div>
+
+    </div>
+
+    <?php include('footer.html'); ?>
+
+    <!-- //// SCRIPTS \\\\ -->
+    <?php include('scripts.inc.php'); ?>
+  </body>
+</html>
