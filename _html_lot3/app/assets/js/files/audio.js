8468f76a5f8c45aef41e3cc677f18a821519ae66
Tho Mon <t.montet@ohwee.fr>
2017-04-05 11:56:25 +0200

    debug audio player

--- _html_lot3/app/assets/js/files/audio.js
+++ _html_lot3/app/assets/js/files/audio.js
@@ -427,7 +427,7 @@ var initAudio = function (hash) {
             setTimeout(function(){
                 var hashPush = '#aid='+aid;
                 history.pushState(null, null, hashPush);
-                
+
 
                 audioPopin.play();
                 audioPopin.play();
@@ -435,7 +435,7 @@ var initAudio = function (hash) {
 
             // CUSTOM LINK FACEBOOK
             var shareUrl = document.location.href;
-            console.log($(e.target),$(e.target).closest('.audio').data('aid'));
+            console.log($(e.target),$(e.target).data('aid'));
             if(typeof $(e.target).closest('.audio').data('facebookurl') !== 'undefined'){
                 shareUrl = $(e.target).closest('.audio').data('facebookurl');
             }
