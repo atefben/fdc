b8d78245fd88a8b8ad5dc33f2d69d42e9bd91278
Jean-Luc Petit <webpetit@gmail.com>
2017-04-13 16:19:45 +0200

    Fix curl timeout

--- src/Base/CoreBundle/OldImport/Importer.php
+++ src/Base/CoreBundle/OldImport/Importer.php
@@ -320,7 +320,7 @@ class Importer
         set_time_limit(0);
         $fp = fopen("$folder$file", 'w+');
         $ch = curl_init(str_replace(" ", "%20", $url));
-        curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
         curl_setopt($ch, CURLOPT_FILE, $fp);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_exec($ch);
