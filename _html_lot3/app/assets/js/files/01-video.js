737bee6d07f94b8717312fb440002d858b6a3cd7
Silvester56 <silvester@garage56.fr>
2017-04-14 16:49:20 +0200

    JS search videos

--- _html_lot3/app/assets/js/files/01-video.js
+++ _html_lot3/app/assets/js/files/01-video.js
@@ -948,11 +948,11 @@ function playerLoad(vid, playerInstance, havePlaylist, live, callback) {
             var $popinVideo = $('.popin-video'),
                 vid = $(e.target).closest('.video').data('vid'),
                 source = $(e.target).closest('.video').data('file') || $(e.target).closest('article').data('file'),
-                img = $(e.target).closest('.video').data('img'),
-                category = $(e.target).closest('.video').find('.category').text(),
-                date = $(e.target).closest('.video').find('.date').text(),
-                hour = $(e.target).closest('.video').find('.hour').text(),
-                name = $(this).find('.contain-txt strong a').data('title');
+                img = $(e.target).closest('.video').data('img') || $(e.target).closest('article').data('img'),
+                category = $(e.target).closest('.video').find('.category').text() || $(e.target).find('span.title-type-media').data('title'),
+                date = $(e.target).closest('.video').find('.date').text() || $(e.target).find('title-dates').text().substring(0, 8),
+                hour = $(e.target).closest('.video').find('.hour').text() || $(e.target).find('title-dates').text().substring(11, 5),
+                name = $(this).find('.contain-txt strong a').data('title') || $(e.target).find('title-media').data('title');
                 if(typeof name === 'undefined'){
                     name = $(this).find('.contain-txt strong a').text();
                 }
