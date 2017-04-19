b4b0ae963f940765bbed89132f1003d772a0d20c
Silvester56 <silvester@garage56.fr>
2017-04-19 15:20:04 +0200

    Industry Programs page

---
+++ _html/industry-programs.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <?php include('head.html'); ?>
+  </head>
+
+
+  <body>
+    <div id="prehome-container">
+      <div id="prehome"></div>
+    </div>
+    <?php include('header.html'); ?>
+    <div id="main" class="home loading">    
+          <div class="bigContainer">
+            
+            <div class="our-news newsPage servicesPage">
+              <h3>Industry programs</h3>
+              <p>Généralement, on utilise un texte en faux latin (le texte ne veut rien dire, il a été modifié), le Lorem ipsum ou Lipsum, qui permet donc de faire office de texte d'attente. L'avantage de le mettre en latin est que l'opérateur sait au premier coup d'oeil.</p>
+              <div class="innerWrapper">
+                <div class="articles">
+                  <div class="readMore">...</div>
+                  <div class="articles-img">
+                    <img class="fitImg" src="/img/slider/slider03.jpg">
+                    <div class="normalImg">
+                      <img src="/img/plus.png" alt="Marché du film">
+                    </div>
+                  </div>
+                  <div class="articles-content">
+                    <h6>Producers<br>Workshop</h6>
+                    <span>Les clés pour bien débuter sur le marché international</span>
+                    <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans...</p>
+                  </div>
+                </div>
+                <div class="articles">
+                  <div class="readMore">...</div>
+                  <div class="articles-img">
+                    <img class="fitImg" src="/img/slider/slider03.jpg">
+                    <div class="normalImg">
+                      <img src="/img/plus.png" alt="Marché du film">
+                    </div>
+                  </div>
+                  <div class="articles-content">
+                    <h6>Producers<br>Network</h6>
+                    <span>Un programme exclusif de networking pour les producteurs</span>
+                    <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans...</p>
+                  </div>
+                </div>
+                <div class="articles">
+                  <div class="readMore">...</div>
+                  <div class="articles-img">
+                    <img class="fitImg" src="/img/slider/slider03.jpg">
+                    <div class="normalImg">
+                      <img src="/img/plus.png" alt="Marché du film">
+                    </div>
+                  </div>
+                  <div class="articles-content">
+                    <h6>Doc Corner<br><br></h6>
+                    <span>Le cinéma documentaire au cœur du Marché du Film</span>
+                    <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans...</p>
+                  </div>
+                </div>
+                <div class="articles">
+                  <div class="readMore">...</div>
+                  <div class="articles-img">
+                    <img class="fitImg" src="/img/slider/slider03.jpg">
+                    <div class="normalImg">
+                      <img src="/img/plus.png" alt="Marché du film">
+                    </div>
+                  </div>
+                  <div class="articles-content">
+                    <h6>Next<br><br></h6>
+                    <span>L'espace dédié à l'innovation et à la Réalité Virtuelle</span>
+                    <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans...</p>
+                  </div>
+                </div>
+                <div class="articles">
+                  <div class="readMore">...</div>
+                  <div class="articles-img">
+                    <img class="fitImg" src="/img/slider/slider03.jpg">
+                    <div class="normalImg">
+                      <img src="/img/plus.png" alt="Marché du film">
+                    </div>
+                  </div>
+                  <div class="articles-content">
+                    <h6>Mixers<br><br></h6>
+                    <span>Rencontres ciblées et festives !</span>
+                    <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans contre des aliens ou dans...</p>
+                  </div>
+                </div>
+                <div class="articles">
+                  <div class="readMore">...</div>
+                  <div class="articles-img">
+                    <img class="fitImg" src="/img/slider/slider03.jpg">
+                    <div class="normalImg">
+                      <img src="/img/plus.png" alt="Marché du film">
+                    </div>
+                  </div>
+                  <div class="articles-content">
+                    <h6>Goes to Cannes<br><br></h6>
+                    <span>Works-in-Progress</span>
+                    <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans contre des aliens ou dans...</p>
+                  </div>
+                </div>
+                <div class="articles">
+                  <div class="readMore">...</div>
+                  <div class="articles-img">
+                    <img class="fitImg" src="/img/slider/slider03.jpg">
+                    <div class="normalImg">
+                      <img src="/img/plus.png" alt="Marché du film">
+                    </div>
+                  </div>
+                  <div class="articles-content">
+                    <h6>Frontières<br><br></h6>
+                    <span>Les clés pour bien débuter sur le marché international</span>
+                    <p>Vous entrez dans la peau d'un lapin bataillant contre des aliens ou dans contre des aliens ou dans...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div><!--CONTAINER-->
+        </div>
+
+    <?php include('box-news.php'); ?>
+    <?php include('box-accreditation.php'); ?>
+    <?php include('box-contact.php'); ?>
+    <?php include('footer.html'); ?>
+
+     <!-- cookie banner -->
+      <?php include('cookie-banner.php'); ?>
+    <!-- //// SCRIPTS \\\\ -->
+
+    <?php include('scripts.inc.php'); ?>
+  </body>
+</html>
