49a3acb29fef4aeb17f5c5b9e1ed42846c14b697
Alexandru Togorean <atogorean@pitechnologies.ro>
2017-04-26 17:03:59 +0300

    Undo debug for #3610.

--- src/FDC/EventMobileBundle/Resources/public/js/calendar.js
+++ src/FDC/EventMobileBundle/Resources/public/js/calendar.js
@@ -9,9 +9,7 @@ n.cssHooks[b]=Ua(l.pixelPosition,function(a,c){return c?(c=Sa(a,b),Oa.test(c)?n(
 // Newsletter
 // =========================
 
-alert('start');
 $(document).ready(function() {
-alert('ready email');
 
   $('.newsletter #email').on('focus', function() {
     if($(this).val() == GLOBALS.texts.newsletter.errorsNotValide || $(this).val() == GLOBALS.texts.newsletter.errorsMailEmpty) {
@@ -50,7 +48,6 @@ alert('ready email');
   });
 });
 $(document).ready(function() {
-alert('ready menu');
 	if($('#main').data('menu') !== undefined) {
 		$('#main').data('menu').split(' ').forEach(function(page) {
 			$('.'+page).addClass('active-page');
@@ -262,7 +259,6 @@ function displaySuggestions() {
 }
 
 $(document).ready(function() {
-alert('ready main');
   $('#selection-btn').on('click', function() {
     openSelection();
   });
@@ -282,7 +278,6 @@ alert('ready main');
   });
 });
 $(document).ready(function() {
-alert('ready events');
 
   // Events on scroll
   // =========================
@@ -365,25 +360,15 @@ alert('ready events');
   addEventsInCalendar();
 
   function displayProgrammationDay(day) {
-alert('display programmation day');
       var startDayDate = new Date("2016-05-" + day + "T00:00:00").getTime();
       var endDayDate = new Date("2016-05-" + day + "T24:00:00").getTime();
-alert(day);
-alert("2016-05-" + day + "T00:00:00");
-alert(startDayDate);
-alert("2016-05-" + day + "T24:00:00");
-alert(endDayDate);
-alert('start each');
+
       $(".fc-event").each(function () {
           var startDate = new Date($(this).data('start')).getTime();
-          alert($(this).data('id'));
-          alert($(this).data('start'));
-          alert(startDate);
+
           if (startDate >= startDayDate && startDate <= endDayDate) {
-            alert('display');
               $(this).css('display', 'block');
           } else {
-            alert('hide');
               $(this).css('display', 'none');
           }
       });
@@ -404,7 +389,6 @@ alert('start each');
   }
 
   function addEventsInCalendar() {
-alert('add events');
     events.sort(function(a, b) {
       if (new Date(a.start) > new Date(b.start)) return 1;
       if (new Date(a.start) < new Date(b.start)) return -1;
